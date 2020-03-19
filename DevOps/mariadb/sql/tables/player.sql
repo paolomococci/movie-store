451fe53061bf38b5a1c92347e5f87de91e84ee9d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-19 09:45:55 +0100

    Update player.sql

--- DevOps/mariadb/sql/tables/player.sql
+++ DevOps/mariadb/sql/tables/player.sql
@@ -1,1 +1,12 @@
-
+CREATE TABLE moviestoredb.player (
+	id BIGINT UNSIGNED auto_increment NOT NULL,
+	name varchar(100) NOT NULL,
+	nickname varchar(100) NOT NULL,
+	surname varchar(100) NOT NULL,
+	uptodate TIMESTAMP NOT NULL,
+	CONSTRAINT player_PK PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci
+COMMENT='players data table';
