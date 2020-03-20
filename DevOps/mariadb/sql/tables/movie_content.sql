a6f945c43343e505c09d9b223df6c54cfc500299
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-20 10:44:37 +0100

    Update movie_content.sql

--- DevOps/mariadb/sql/tables/movie_content.sql
+++ DevOps/mariadb/sql/tables/movie_content.sql
@@ -1,1 +1,11 @@
-
+CREATE TABLE moviestoredb.movie_content (
+	id BIGINT UNSIGNED auto_increment NOT NULL,
+	content TEXT NOT NULL,
+	updated TIMESTAMP NOT NULL,
+	CONSTRAINT movie_content_PK PRIMARY KEY (id),
+	CONSTRAINT movie_content_FK FOREIGN KEY (id) REFERENCES moviestoredb.movie(id) ON DELETE RESTRICT ON UPDATE CASCADE
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci
+COMMENT='movie content data table';
