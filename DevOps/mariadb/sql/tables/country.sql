9eb14226002e2a8395b78e22caeac05581f8d80a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-20 10:47:21 +0100

    Update country.sql

--- DevOps/mariadb/sql/tables/country.sql
+++ DevOps/mariadb/sql/tables/country.sql
@@ -1,1 +1,12 @@
+CREATE TABLE moviestoredb.country (
+	id BIGINT UNSIGNED auto_increment NOT NULL,
+	name varchar(100) NOT NULL,
+	code varchar(3) NOT NULL,
+	CONSTRAINT country_PK PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
 
+ALTER TABLE moviestoredb.country ADD CONSTRAINT country_FK FOREIGN KEY (id) REFERENCES moviestoredb.iso3166(id) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE moviestoredb.country ADD updated TIMESTAMP NOT NULL;
