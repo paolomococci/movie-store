facdef8a8f67fb68c21fd5840b1e527e49e38e98
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-20 10:48:11 +0100

    Update category.sql

--- DevOps/mariadb/sql/tables/category.sql
+++ DevOps/mariadb/sql/tables/category.sql
@@ -1,1 +1,11 @@
-
+CREATE TABLE moviestoredb.category (
+	id BIGINT UNSIGNED auto_increment NOT NULL,
+	name varchar(50) NOT NULL,
+	description varchar(200) NULL,
+	updated TIMESTAMP NOT NULL,
+	CONSTRAINT category_PK PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci
+COMMENT='category data table';
