4444006ea2b3793f5ffcd7029d47c80bf83e0da0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-20 10:44:08 +0100

    Update movie_player.sql

--- DevOps/mariadb/sql/tables/movie_player.sql
+++ DevOps/mariadb/sql/tables/movie_player.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE moviestoredb.movie_player (
+	id BIGINT UNSIGNED auto_increment NOT NULL,
+	updated TIMESTAMP NOT NULL,
+	CONSTRAINT movie_player_FK FOREIGN KEY (id) REFERENCES moviestoredb.movie(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT movie_player_FK_1 FOREIGN KEY (id) REFERENCES moviestoredb.player(id) ON DELETE RESTRICT ON UPDATE CASCADE
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci
+COMMENT='movie_player data table';
