2cdca060b4575aa7cbe61d5bf8b15623a208bf6e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-20 10:47:47 +0100

    Update city.sql

--- DevOps/mariadb/sql/tables/city.sql
+++ DevOps/mariadb/sql/tables/city.sql
@@ -1,1 +1,12 @@
-
+CREATE TABLE moviestoredb.city (
+	id BIGINT UNSIGNED auto_increment NOT NULL,
+	name varchar(100) NOT NULL,
+	country BIGINT UNSIGNED NOT NULL,
+	updated TIMESTAMP NOT NULL,
+	CONSTRAINT city_PK PRIMARY KEY (id),
+	CONSTRAINT city_FK FOREIGN KEY (id) REFERENCES moviestoredb.country(id) ON DELETE RESTRICT ON UPDATE CASCADE
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci
+COMMENT='city data table';
