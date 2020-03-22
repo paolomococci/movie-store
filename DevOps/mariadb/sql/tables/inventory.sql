581a03841ea2f0585d2db2045cc2868c82c84928
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-22 10:45:01 +0100

    Update inventory.sql

--- DevOps/mariadb/sql/tables/inventory.sql
+++ DevOps/mariadb/sql/tables/inventory.sql
@@ -1,11 +1,7 @@
-CREATE TABLE moviestoredb.inventory (
-	id BIGINT UNSIGNED auto_increment NOT NULL,
-	updated TIMESTAMP NOT NULL,
-	CONSTRAINT inventory_PK PRIMARY KEY (id),
-	CONSTRAINT inventory_FK FOREIGN KEY (id) REFERENCES moviestoredb.store(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-	CONSTRAINT inventory_FK_1 FOREIGN KEY (id) REFERENCES moviestoredb.movie(id) ON DELETE RESTRICT ON UPDATE CASCADE
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COLLATE=utf8_general_ci
-COMMENT='inventory data table';
+CREATE TABLE `inventory` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `inventory_FK` FOREIGN KEY (`id`) REFERENCES `store` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `inventory_FK_1` FOREIGN KEY (`id`) REFERENCES `movie` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='inventory data table';
