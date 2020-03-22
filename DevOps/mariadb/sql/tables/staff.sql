5f291642bf0d94d44d9974eda8661f423e5dcc4c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-22 10:51:23 +0100

    Update staff.sql

--- DevOps/mariadb/sql/tables/staff.sql
+++ DevOps/mariadb/sql/tables/staff.sql
@@ -1,21 +1,17 @@
-CREATE TABLE moviestoredb.staff (
-	id BIGINT UNSIGNED auto_increment NOT NULL,
-	name varchar(100) NOT NULL,
-	nickname varchar(100) NULL,
-	surname varchar(100) NOT NULL,
-	picture BLOB NULL,
-	phone varchar(20) NULL,
-	mobile varchar(20) NOT NULL,
-	email varchar(50) NOT NULL,
-	username varchar(100) NOT NULL,
-	password varchar(20) NOT NULL,
-	active BOOL DEFAULT true NOT NULL,
-	updated TIMESTAMP NOT NULL,
-	CONSTRAINT staff_PK PRIMARY KEY (id),
-	CONSTRAINT staff_FK FOREIGN KEY (id) REFERENCES moviestoredb.address(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-	CONSTRAINT staff_FK_1 FOREIGN KEY (id) REFERENCES moviestoredb.store(id) ON DELETE RESTRICT ON UPDATE CASCADE
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COLLATE=utf8_general_ci
-COMMENT='staff data table';
+CREATE TABLE `staff` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `nickname` varchar(100) DEFAULT NULL,
+  `surname` varchar(100) NOT NULL,
+  `picture` blob DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `mobile` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 1,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `staff_FK` FOREIGN KEY (`id`) REFERENCES `address` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `staff_FK_1` FOREIGN KEY (`id`) REFERENCES `store` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='staff data table';
