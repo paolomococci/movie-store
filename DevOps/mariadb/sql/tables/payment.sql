8f241b1a0f8e7fb38cb99424d22b2d4d1845c732
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-22 10:49:20 +0100

    Update payment.sql

--- DevOps/mariadb/sql/tables/payment.sql
+++ DevOps/mariadb/sql/tables/payment.sql
@@ -1,14 +1,10 @@
-CREATE TABLE moviestoredb.payment (
-	id BIGINT UNSIGNED auto_increment NOT NULL,
-	amount DECIMAL NOT NULL,
-	paid DATE NOT NULL,
-	updated TIMESTAMP NOT NULL,
-	CONSTRAINT payment_PK PRIMARY KEY (id),
-	CONSTRAINT payment_FK FOREIGN KEY (id) REFERENCES moviestoredb.rental(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-	CONSTRAINT payment_FK_1 FOREIGN KEY (id) REFERENCES moviestoredb.customer(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-	CONSTRAINT payment_FK_2 FOREIGN KEY (id) REFERENCES moviestoredb.staff(id) ON DELETE RESTRICT ON UPDATE CASCADE
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COLLATE=utf8_general_ci
-COMMENT='payment data table';
+CREATE TABLE `payment` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `amount` decimal(10,0) NOT NULL,
+  `payed` date NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `payment_FK` FOREIGN KEY (`id`) REFERENCES `rental` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `payment_FK_1` FOREIGN KEY (`id`) REFERENCES `customer` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `payment_FK_2` FOREIGN KEY (`id`) REFERENCES `staff` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='payment data table';
