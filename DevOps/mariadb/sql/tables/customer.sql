e2afb845207f4f64613c780fc5e682f6a69753f2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-22 10:44:31 +0100

    Update customer.sql

--- DevOps/mariadb/sql/tables/customer.sql
+++ DevOps/mariadb/sql/tables/customer.sql
@@ -1,19 +1,15 @@
-CREATE TABLE moviestoredb.customer (
-	id BIGINT UNSIGNED auto_increment NOT NULL,
-	name varchar(100) NOT NULL,
-	nickname varchar(100) NULL,
-	surname varchar(100) NOT NULL,
-	phone varchar(20) NULL,
-	mobile varchar(20) NOT NULL,
-	email varchar(50) NOT NULL,
-	created DATE NOT NULL,
-	active BOOL DEFAULT true NOT NULL,
-	updated TIMESTAMP NOT NULL,
-	CONSTRAINT customer_PK PRIMARY KEY (id),
-	CONSTRAINT customer_FK FOREIGN KEY (id) REFERENCES moviestoredb.address(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-	CONSTRAINT customer_FK_1 FOREIGN KEY (id) REFERENCES moviestoredb.store(id) ON DELETE RESTRICT ON UPDATE CASCADE
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COLLATE=utf8_general_ci
-COMMENT='customer data table';
+CREATE TABLE `customer` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `nickname` varchar(100) DEFAULT NULL,
+  `surname` varchar(100) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `mobile` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `created` date NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 1,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `customer_FK` FOREIGN KEY (`id`) REFERENCES `address` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `customer_FK_1` FOREIGN KEY (`id`) REFERENCES `store` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='customer data table';
