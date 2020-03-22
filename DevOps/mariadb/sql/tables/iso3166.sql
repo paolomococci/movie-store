8cae83b1a8cc0815719b7fe3e2dcf2b466804e39
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-22 10:45:43 +0100

    Update iso3166.sql

--- DevOps/mariadb/sql/tables/iso3166.sql
+++ DevOps/mariadb/sql/tables/iso3166.sql
@@ -1,13 +1,9 @@
-CREATE TABLE moviestoredb.iso3166 (
-	id BIGINT UNSIGNED auto_increment NOT NULL,
-	name varchar(100) NOT NULL,
-	code INT NOT NULL,
-	signature varchar(3) NOT NULL,
-	CONSTRAINT iso3166_PK PRIMARY KEY (id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COLLATE=utf8_general_ci;
-CREATE FULLTEXT INDEX iso3166_signature_IDX ON moviestoredb.iso3166 (signature);
-
-ALTER TABLE moviestoredb.iso3166 ADD updated TIMESTAMP NOT NULL;
+CREATE TABLE `iso3166` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `code` int(11) NOT NULL,
+  `signature` varchar(3) NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  FULLTEXT KEY `iso3166_signature_IDX` (`signature`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
