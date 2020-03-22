c59a1364e17038f405152cdb7a8d04908e3ce803
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-22 10:46:10 +0100

    Update language.sql

--- DevOps/mariadb/sql/tables/language.sql
+++ DevOps/mariadb/sql/tables/language.sql
@@ -1,10 +1,6 @@
-CREATE TABLE moviestoredb.`language` (
-	id BIGINT UNSIGNED auto_increment NOT NULL,
-	name varchar(30) NOT NULL,
-	updated TIMESTAMP NOT NULL,
-	CONSTRAINT language_PK PRIMARY KEY (id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COLLATE=utf8_general_ci
-COMMENT='language data table';
+CREATE TABLE `language` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='language data table';
