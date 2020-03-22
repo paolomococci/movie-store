8822c26051a53db49560910a849544593d7fac56
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-22 10:51:49 +0100

    Update store.sql

--- DevOps/mariadb/sql/tables/store.sql
+++ DevOps/mariadb/sql/tables/store.sql
@@ -1,12 +1,8 @@
-CREATE TABLE moviestoredb.store (
-	id BIGINT UNSIGNED auto_increment NOT NULL,
-	name varchar(50) NOT NULL,
-	updated TIMESTAMP NOT NULL,
-	CONSTRAINT store_PK PRIMARY KEY (id),
-	CONSTRAINT store_FK FOREIGN KEY (id) REFERENCES moviestoredb.address(id) ON DELETE RESTRICT ON UPDATE CASCADE
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COLLATE=utf8_general_ci
-COMMENT='store data table';
-CREATE INDEX store_name_IDX USING BTREE ON moviestoredb.store (name);
+CREATE TABLE `store` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `store_name_IDX` (`name`) USING BTREE,
+  CONSTRAINT `store_FK` FOREIGN KEY (`id`) REFERENCES `address` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='store data table';
