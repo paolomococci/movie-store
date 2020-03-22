0060bf1b0c28e7b7ade0736f833362a02b42a502
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-22 10:50:04 +0100

    Update player.sql

--- DevOps/mariadb/sql/tables/player.sql
+++ DevOps/mariadb/sql/tables/player.sql
@@ -1,14 +1,8 @@
-CREATE TABLE moviestoredb.player (
-	id BIGINT UNSIGNED auto_increment NOT NULL,
-	name varchar(100) NOT NULL,
-	nickname varchar(100) NOT NULL,
-	surname varchar(100) NOT NULL,
-	uptodate TIMESTAMP NOT NULL,
-	CONSTRAINT player_PK PRIMARY KEY (id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COLLATE=utf8_general_ci
-COMMENT='players data table';
-
-ALTER TABLE moviestoredb.player CHANGE uptodate updated timestamp DEFAULT current_timestamp() on update current_timestamp() NOT NULL;
+CREATE TABLE `player` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `nickname` varchar(100) NOT NULL,
+  `surname` varchar(100) NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='players data table';
