8ff31fecadd3770d4a649138f8e1ee0aa6a56983
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-24 00:19:36 +0100

    Create PlayerRepoMockMvcTests.java

---
+++ mariadb/crud-layer/src/test/java/local/example/data/repository/PlayerRepoMockMvcTests.java
@@ -0,0 +1,58 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PlayerRepoMockMvcTests {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Autowired
+	PlayerRestRepository playerRestRepository;
+
+	@Test
+	void contextTest() 
+			throws Exception {
+		assertThat(playerRestRepository).isNotNull();
+	}
+	
+	@Test
+	void existenceTest() 
+			throws Exception {
+		mockMvc
+			.perform(get("/"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$._links.players").exists());
+	}
+}
