94a521a4d8f3341951092d8f457367f347fe5e2a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-25 21:32:02 +0100

    Update MovieContent.java

--- mariadb/crud-layer/src/main/java/local/example/data/entity/MovieContent.java
+++ mariadb/crud-layer/src/main/java/local/example/data/entity/MovieContent.java
@@ -18,12 +18,16 @@
 
 package local.example.data.entity;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity(name = "movie_content")
 public class MovieContent {
@@ -32,4 +36,12 @@ public class MovieContent {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Getter
 	private long id;
+
+	@Getter
+	@Setter
+	@NotNull
+	private String content;
+
+	@Getter
+	private Timestamp updated;
 }
