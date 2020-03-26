56e56521911805987d1476d92a322f3da186d690
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-26 05:29:08 +0100

    Update City.java

--- mariadb/crud-layer/src/main/java/local/example/data/entity/City.java
+++ mariadb/crud-layer/src/main/java/local/example/data/entity/City.java
@@ -18,12 +18,16 @@
 
 package local.example.data.entity;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 public class City {
@@ -32,4 +36,12 @@ public class City {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Getter
 	private long id;
+
+	@Getter
+	@Setter
+	@NotNull
+	private String name;
+
+	@Getter
+	private Timestamp updated;
 }
