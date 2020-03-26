50743e370c04c848a1aadee6b73becddf62ca17c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-26 05:27:26 +0100

    Update Iso3166.java

--- mariadb/crud-layer/src/main/java/local/example/data/entity/Iso3166.java
+++ mariadb/crud-layer/src/main/java/local/example/data/entity/Iso3166.java
@@ -18,12 +18,16 @@
 
 package local.example.data.entity;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 public class Iso3166 {
@@ -32,4 +36,22 @@ public class Iso3166 {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Getter
 	private long id;
+
+	@Getter
+	@Setter
+	@NotNull
+	private String name;
+
+	@Getter
+	@Setter
+	@NotNull
+	private Integer code;
+
+	@Getter
+	@Setter
+	@NotNull
+	private String signature;
+
+	@Getter
+	private Timestamp updated;
 }
