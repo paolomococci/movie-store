687afdd3957cba155174438bf1c1b706392c793e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-27 23:38:54 +0100

    Update address.sql

--- DevOps/mariadb/sql/tables/address.sql
+++ DevOps/mariadb/sql/tables/address.sql
@@ -2,13 +2,9 @@ CREATE TABLE `address` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `civic` varchar(10) NOT NULL,
-  `district` varchar(100) NOT NULL,
-  `city` bigint(20) unsigned NOT NULL,
-  `code` varchar(10) NOT NULL,
-  `country` bigint(20) unsigned NOT NULL,
   `phone` varchar(25) DEFAULT NULL,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `address_city_IDX` (`city`) USING BTREE,
-  CONSTRAINT `address_FK` FOREIGN KEY (`id`) REFERENCES `city` (`id`) ON UPDATE CASCADE
+  CONSTRAINT `address_to_city_FK` FOREIGN KEY (`id`) REFERENCES `city` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `address_to_district_FK` FOREIGN KEY (`id`) REFERENCES `district` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='address data table';
