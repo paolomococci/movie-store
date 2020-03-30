2a2a69070e7c043b095f34d722987eace38093f5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-30 21:07:08 +0200

    Update ApplicationTests.java

--- mariadb/crud-layer/src/test/java/local/example/data/ApplicationTests.java
+++ mariadb/crud-layer/src/test/java/local/example/data/ApplicationTests.java
@@ -18,12 +18,69 @@
 
 package local.example.data;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
+	
+	@BeforeAll
+	static void init() 
+			throws Exception {
+		System.out.println("");
+	}
+	
+	@BeforeEach
+	void before() 
+			throws Exception {
+		System.out.println("");
+	}
 
 	@Test
-	void voidTest() {}
+	@DisplayName("sample test")
+	void sampleTest() 
+			throws Exception {
+		System.out.println("");
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1,2,3})
+	void sampleParametrizedTest(Integer index) 
+			throws Exception {
+		
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1,2,3})
+	void anotherParametrizedTest(Integer index) 
+			throws Exception {
+		
+	}
+
+	@Test
+	@Disabled
+	void sampleDisabledTest() 
+			throws Exception {
+		System.out.println("");
+	}
+	
+	@AfterEach
+	void after() 
+			throws Exception {
+		System.out.println("");
+	}
+	
+	@AfterAll
+	static void end() 
+			throws Exception {
+		System.out.println("");
+	}
 }
