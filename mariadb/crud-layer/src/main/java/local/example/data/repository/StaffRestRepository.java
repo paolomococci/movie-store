d5c7803a2bca280d7e6fc88812dcc338fe73e4fb
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-30 21:05:44 +0200

    Update StaffRestRepository.java

--- mariadb/crud-layer/src/main/java/local/example/data/repository/StaffRestRepository.java
+++ mariadb/crud-layer/src/main/java/local/example/data/repository/StaffRestRepository.java
@@ -23,7 +23,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import local.example.data.entity.Staff;
 
-@RepositoryRestResource(path = "staff")
+@RepositoryRestResource(path = "teams")
 public interface StaffRestRepository 
 	extends PagingAndSortingRepository<Staff, Long> {
 
