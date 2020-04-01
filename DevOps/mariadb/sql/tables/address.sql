2f221eaa7a71ef5a9732e6268081aad67fc1e744
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-01 16:13:26 +0200

    Update address.sql

--- DevOps/mariadb/sql/tables/address.sql
+++ DevOps/mariadb/sql/tables/address.sql
@@ -4,6 +4,7 @@ CREATE TABLE `address` (
   `civic` varchar(10) NOT NULL,
   `phone` varchar(25) DEFAULT NULL,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `address_to_city_FK` FOREIGN KEY (`id`) REFERENCES `city` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `address_to_district_FK` FOREIGN KEY (`id`) REFERENCES `district` (`id`) ON UPDATE CASCADE
