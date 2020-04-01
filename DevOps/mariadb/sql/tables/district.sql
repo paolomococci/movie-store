3b3a086348cb42c248f8b00b9a073c0af9da889b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-01 16:14:42 +0200

    Update district.sql

--- DevOps/mariadb/sql/tables/district.sql
+++ DevOps/mariadb/sql/tables/district.sql
@@ -2,5 +2,6 @@ CREATE TABLE `district` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `code` varchar(6) NOT NULL,
+  `updated` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='district data table';
