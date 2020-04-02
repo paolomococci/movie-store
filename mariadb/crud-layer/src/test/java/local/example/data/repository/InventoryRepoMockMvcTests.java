f7af1feb2324a7cbe14d0126ebab6225a3a5f7f7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-02 19:44:34 +0200

    Update InventoryRepoMockMvcTests.java

--- mariadb/crud-layer/src/test/java/local/example/data/repository/InventoryRepoMockMvcTests.java
+++ mariadb/crud-layer/src/test/java/local/example/data/repository/InventoryRepoMockMvcTests.java
@@ -34,8 +34,6 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class InventoryRepoMockMvcTests {
 
-	private static String INVENTORY_TEST_STRING = "{}";
-
 	@Autowired
 	MockMvc mockMvc;
 
