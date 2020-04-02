03da0f789a688fa5d304cc14af6b53e10745f5e6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-02 19:47:24 +0200

    Update PaymentRepoMockMvcTests.java

--- mariadb/crud-layer/src/test/java/local/example/data/repository/PaymentRepoMockMvcTests.java
+++ mariadb/crud-layer/src/test/java/local/example/data/repository/PaymentRepoMockMvcTests.java
@@ -34,8 +34,6 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class PaymentRepoMockMvcTests {
 
-	private static String PAYMENT_TEST_STRING = "{}";
-
 	@Autowired
 	MockMvc mockMvc;
 
