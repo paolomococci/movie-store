a502984de840150c454fb0e48fb31bc38d1c8626
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-02 19:50:47 +0200

    Update StoreRepoMockMvcTests.java

--- mariadb/crud-layer/src/test/java/local/example/data/repository/StoreRepoMockMvcTests.java
+++ mariadb/crud-layer/src/test/java/local/example/data/repository/StoreRepoMockMvcTests.java
@@ -34,8 +34,6 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class StoreRepoMockMvcTests {
 
-	private static String STORE_TEST_STRING = "{}";
-
 	@Autowired
 	MockMvc mockMvc;
 
