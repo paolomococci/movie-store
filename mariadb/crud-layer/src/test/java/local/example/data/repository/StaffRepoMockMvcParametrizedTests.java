66dedb0e55f6f27a19cd86d865c563997b3066d8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-04 19:10:24 +0200

    Create StaffRepoMockMvcParametrizedTests.java

---
+++ mariadb/crud-layer/src/test/java/local/example/data/repository/StaffRepoMockMvcParametrizedTests.java
@@ -0,0 +1,55 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.repository;
+
+import java.net.URI;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(OrderAnnotation.class)
+public class StaffRepoMockMvcParametrizedTests {
+
+	private static String STAFF_TEST_STRING = "{}"; // TODO
+	private static URI uri;
+
+	@Autowired
+	MockMvc mockMvc;
+	
+	// TODO
+
+	private static URI getUri() {
+		return uri;
+	}
+
+	private static void setUri(URI uri) {
+		StaffRepoMockMvcParametrizedTests.uri = uri;
+	}
+	
+	private static Stream<String> initUri() {
+		return Stream.of(StaffRepoMockMvcParametrizedTests.getUri().getPath());
+	}
+}
