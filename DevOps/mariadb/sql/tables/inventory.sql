89c4acc3a8c670fbc274d2bd32529de64565fdf6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-05 21:05:20 +0200

    Update inventory.sql

--- DevOps/mariadb/sql/tables/inventory.sql
+++ DevOps/mariadb/sql/tables/inventory.sql
@@ -2,6 +2,6 @@ CREATE TABLE `inventory` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
-  CONSTRAINT `inventory_FK` FOREIGN KEY (`id`) REFERENCES `store` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `inventory_FK_1` FOREIGN KEY (`id`) REFERENCES `movie` (`id`) ON UPDATE CASCADE
+  CONSTRAINT `inventory_to_movie_FK` FOREIGN KEY (`id`) REFERENCES `movie` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `inventory_to_store_FK` FOREIGN KEY (`id`) REFERENCES `store` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='inventory data table';
