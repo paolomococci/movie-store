f38ce0ce886ffc5ece5993dd518b07ee5a260ba0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-05 21:10:25 +0200

    Update store.sql

--- DevOps/mariadb/sql/tables/store.sql
+++ DevOps/mariadb/sql/tables/store.sql
@@ -4,5 +4,5 @@ CREATE TABLE `store` (
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `store_name_IDX` (`name`) USING BTREE,
-  CONSTRAINT `store_FK` FOREIGN KEY (`id`) REFERENCES `address` (`id`) ON UPDATE CASCADE
+  CONSTRAINT `store_to_address_FK` FOREIGN KEY (`id`) REFERENCES `address` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='store data table';
