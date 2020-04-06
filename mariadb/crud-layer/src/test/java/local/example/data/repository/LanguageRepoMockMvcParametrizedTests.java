344bae6d8e5c40f54e887835d6df31559a05966b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-06 09:00:41 +0200

    Update LanguageRepoMockMvcParametrizedTests.java

--- mariadb/crud-layer/src/test/java/local/example/data/repository/LanguageRepoMockMvcParametrizedTests.java
+++ mariadb/crud-layer/src/test/java/local/example/data/repository/LanguageRepoMockMvcParametrizedTests.java
@@ -18,28 +18,96 @@
 
 package local.example.data.repository;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.net.URI;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestMethodOrder(OrderAnnotation.class)
 public class LanguageRepoMockMvcParametrizedTests {
 
-	private static String LANGUAGE_TEST_STRING = "{}"; // TODO
+	private static String LANGUAGE_TEST_STRING = "{\"name\":\"italian\"}";
 	private static URI uri;
 
 	@Autowired
 	MockMvc mockMvc;
 	
-	// TODO
+	@Test
+	@Order(1)
+	@DisplayName("create a record into language's data table")
+	void createTest() 
+			throws Exception {
+		MvcResult mvcResult = mockMvc
+			.perform(post("/languages").content(LANGUAGE_TEST_STRING))
+			.andExpect(status().isCreated())
+			.andReturn();
+		setUri(new URI(mvcResult.getResponse().getHeader("Location")));
+	}
+
+	@Order(2)
+	@ParameterizedTest
+	@MethodSource("initUri")
+	@DisplayName("read a record from the language's data table identifying it from the uri")
+	void readTest(String uri) 
+			throws Exception {
+		mockMvc.perform(get(uri))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.name").value("italian"));
+	}
+
+	@Order(3)
+	@ParameterizedTest
+	@MethodSource("initUri")
+	@DisplayName("update a record into the language's data table identifying it from the uri")
+	void putTest(String uri) 
+			throws Exception {
+		mockMvc.perform(put(uri)
+			.content("{\"name\":\"IT_it\"}"))
+			.andExpect(status().isNoContent()); 
+		mockMvc.perform(get(uri))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.name").value("IT_it"));
+	}
+
+	@Order(4)
+	@ParameterizedTest
+	@MethodSource("initUri")
+	@DisplayName("delete a record from the language's data table identifying it from the uri")
+	void deleteTest(String uri) 
+			throws Exception {
+		mockMvc.perform(delete(uri))
+			.andExpect(status().isNoContent());
+	}
+
+	@Order(5)
+	@ParameterizedTest
+	@MethodSource("initUri")
+	@DisplayName("try to read a deleted record from the language's data table identifying it from the uri")
+	void notFoundTest(String uri) 
+			throws Exception {
+		mockMvc.perform(get(uri))
+			.andExpect(status().isNotFound());
+	}
 
 	private static URI getUri() {
 		return uri;
