58b6804d5bbfba49dcb0eff4365a0964352ed961
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-07 11:23:30 +0200

    Update Language.java

--- mariadb/crud-layer/src/main/java/local/example/data/entity/Language.java
+++ mariadb/crud-layer/src/main/java/local/example/data/entity/Language.java
@@ -25,6 +25,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -40,6 +41,7 @@ public class Language {
 	@Getter
 	@Setter
 	@NotNull
+	@Size(min = 1, max = 30)
 	private String name;
 
 	@Getter
