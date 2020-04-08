5ef28b19dc4c9261c249b9d0bdf9251adbe1325b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-08 12:06:23 +0200

    Update Language.java

--- mariadb/crud-layer/src/main/java/local/example/data/entity/Language.java
+++ mariadb/crud-layer/src/main/java/local/example/data/entity/Language.java
@@ -25,6 +25,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.Getter;
@@ -42,6 +43,7 @@ public class Language {
 	@Setter
 	@NotNull
 	@Size(min = 1, max = 30)
+	@Pattern(regexp = "[a-zA-Z_-]{1,30}$")
 	private String name;
 
 	@Getter
