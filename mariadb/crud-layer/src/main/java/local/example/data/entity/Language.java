6cf15365d71bfdeab97ada53f27eabf5fff99131
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-09 11:28:44 +0200

    Update Language.java

--- mariadb/crud-layer/src/main/java/local/example/data/entity/Language.java
+++ mariadb/crud-layer/src/main/java/local/example/data/entity/Language.java
@@ -44,7 +44,7 @@ public class Language {
 	@NotNull
 	@Size(min = 1, max = 30, message = "range of this value is 1 to 30 characters")
 	@Pattern(
-			regexp = "^[a-zA-Z_-]$", 
+			regexp = "^[a-zA-Z_-]*$", 
 			message = "only the characters `a-z`, `A-Z`, `_` and `-` are allowed"
 			)
 	private String name;
