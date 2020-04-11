40978bcd402272130fa4e152aecb1a37a0a90786
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-11 19:39:58 +0200

    Update rental.sql

--- DevOps/mariadb/sql/tables/rental.sql
+++ DevOps/mariadb/sql/tables/rental.sql
@@ -4,7 +4,7 @@ CREATE TABLE `rental` (
   `back` date NOT NULL,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
-  CONSTRAINT `rental_to_customer_FK` FOREIGN KEY (`id`) REFERENCES `customer` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `rental_to_inventory_FK` FOREIGN KEY (`id`) REFERENCES `inventory` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `rental_to_staff_FK` FOREIGN KEY (`id`) REFERENCES `staff` (`id`) ON UPDATE CASCADE
+  CONSTRAINT `rental_customer_fk` FOREIGN KEY (`id`) REFERENCES `customer` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `rental_inventory_fk` FOREIGN KEY (`id`) REFERENCES `inventory` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `rental_staff_fk` FOREIGN KEY (`id`) REFERENCES `staff` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='rental data table';
