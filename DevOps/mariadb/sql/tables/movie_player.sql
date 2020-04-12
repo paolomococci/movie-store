5bbc201c05979591b87c42c28aecc5b413b5fcc2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-12 10:38:19 +0200

    Update movie_player.sql

--- DevOps/mariadb/sql/tables/movie_player.sql
+++ DevOps/mariadb/sql/tables/movie_player.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `movie_player` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  KEY `movie_player_FK_1` (`id`),
+  PRIMARY KEY (`id`),
   CONSTRAINT `film_fk` FOREIGN KEY (`id`) REFERENCES `movie` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `player_fk` FOREIGN KEY (`id`) REFERENCES `player` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='movie_player data table';
