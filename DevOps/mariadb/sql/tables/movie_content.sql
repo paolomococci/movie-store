7a8a15ca7d12a94cfa9a87bb79c52f34934effad
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-12 10:37:56 +0200

    Update movie_content.sql

--- DevOps/mariadb/sql/tables/movie_content.sql
+++ DevOps/mariadb/sql/tables/movie_content.sql
@@ -1,6 +1,5 @@
 CREATE TABLE `movie_content` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `content` text NOT NULL,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   CONSTRAINT `content_table_fk` FOREIGN KEY (`id`) REFERENCES `content` (`id`) ON UPDATE CASCADE,
