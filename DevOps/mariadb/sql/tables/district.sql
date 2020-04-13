1bbb67a4cdaff9c88edccb9cf633f9f6b1bac29d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-13 20:05:35 +0200

    Update district.sql

--- DevOps/mariadb/sql/tables/district.sql
+++ DevOps/mariadb/sql/tables/district.sql
@@ -3,6 +3,5 @@ CREATE TABLE `district` (
   `name` varchar(50) NOT NULL,
   `code` varchar(6) NOT NULL,
   `updated` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `city_fk` FOREIGN KEY (`id`) REFERENCES `city` (`id`) ON UPDATE CASCADE
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='district data table';
