448a195944b7cd806061b0992bbfb20a6132a452
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-13 20:04:46 +0200

    Update city.sql

--- DevOps/mariadb/sql/tables/city.sql
+++ DevOps/mariadb/sql/tables/city.sql
@@ -2,6 +2,5 @@ CREATE TABLE `city` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`),
-  CONSTRAINT `country_fk` FOREIGN KEY (`id`) REFERENCES `country` (`id`) ON UPDATE CASCADE
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='city data table';
