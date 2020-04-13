17680658ca85b11b1e96d4901622e4a74ed9a64d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-13 20:05:09 +0200

    Update country.sql

--- DevOps/mariadb/sql/tables/country.sql
+++ DevOps/mariadb/sql/tables/country.sql
@@ -2,6 +2,6 @@ CREATE TABLE `country` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`),
-  CONSTRAINT `iso3166_fk` FOREIGN KEY (`id`) REFERENCES `iso3166` (`id`) ON UPDATE CASCADE
+  `iso3166_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
