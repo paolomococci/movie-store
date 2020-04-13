d048b5427776120ebc8699802a34ea48b65ba094
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-13 20:06:41 +0200

    Update Country.java

--- mariadb/crud-layer/src/main/java/local/example/data/entity/Country.java
+++ mariadb/crud-layer/src/main/java/local/example/data/entity/Country.java
@@ -23,18 +23,13 @@ import java.sql.Timestamp;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -62,8 +57,6 @@ public class Country {
 	
 	@Getter
 	@Setter
-	@ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
-	@JoinColumn(foreignKey = @ForeignKey(name = "iso3166_fk"), nullable = false)
-	@OnDelete(action = OnDeleteAction.CASCADE)
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	private Iso3166 iso3166;
 }
