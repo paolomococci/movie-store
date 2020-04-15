129b358ecdf1adc88e1df4ac427c96cdfe3188bc
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-16 00:07:47 +0200

    Update address.sql

--- DevOps/mariadb/sql/tables/address.sql
+++ DevOps/mariadb/sql/tables/address.sql
@@ -5,6 +5,8 @@ CREATE TABLE `address` (
   `phone` varchar(25) DEFAULT NULL,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `type` varchar(255) DEFAULT NULL,
-  `district_fk` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `district_fk` bigint(20) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `address_to_district_fk` (`district_fk`),
+  CONSTRAINT `address_to_district_fk` FOREIGN KEY (`district_fk`) REFERENCES `district` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='address data table';
