d23ce439bd240c01f2b31bc170d758acb237fcdb
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-17 00:44:02 +0200

    Update Customer.java

--- mariadb/crud-layer/src/main/java/local/example/data/entity/Customer.java
+++ mariadb/crud-layer/src/main/java/local/example/data/entity/Customer.java
@@ -25,6 +25,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -110,4 +112,16 @@ public class Customer {
 
 	@Getter
 	private Timestamp updated;
+	
+	@Getter
+	@Setter
+	@ManyToOne
+	@JoinColumn(name = "address_fk")
+	private Address customerAddress;
+	
+	@Getter
+	@Setter
+	@ManyToOne
+	@JoinColumn(name = "store_fk")
+	private Store customerStore;
 }
