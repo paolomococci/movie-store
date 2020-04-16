39f350f3e6facfd3477dc363e70c9db90ef22066
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-17 00:41:40 +0200

    Update moviestoredb-table-scheme-dump.sql

--- DevOps/mariadb/sql/moviestoredb-table-scheme-dump.sql
+++ DevOps/mariadb/sql/moviestoredb-table-scheme-dump.sql
@@ -87,9 +87,13 @@ CREATE TABLE `customer` (
   `birthday` date NOT NULL,
   `active` tinyint(1) NOT NULL DEFAULT 1,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `address_fk` bigint(20) unsigned DEFAULT NULL,
+  `store_fk` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `customer_address_fk` FOREIGN KEY (`id`) REFERENCES `address` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `customer_store_fk` FOREIGN KEY (`id`) REFERENCES `store` (`id`) ON UPDATE CASCADE
+  KEY `customer_to_address_fk` (`address_fk`),
+  KEY `customer_to_store_fk` (`store_fk`),
+  CONSTRAINT `customer_to_address_fk` FOREIGN KEY (`address_fk`) REFERENCES `address` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `customer_to_store_fk` FOREIGN KEY (`store_fk`) REFERENCES `store` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='customer data table';
 
 --
@@ -275,9 +279,13 @@ CREATE TABLE `staff` (
   `password` varchar(20) NOT NULL,
   `active` tinyint(1) NOT NULL DEFAULT 1,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `address_fk` bigint(20) unsigned DEFAULT NULL,
+  `store_fk` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `staff_address_fk` FOREIGN KEY (`id`) REFERENCES `address` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `staff_store_fk` FOREIGN KEY (`id`) REFERENCES `store` (`id`) ON UPDATE CASCADE
+  KEY `staff_to_address_fk` (`address_fk`),
+  KEY `staff_to_store_fk` (`store_fk`),
+  CONSTRAINT `staff_to_address_fk` FOREIGN KEY (`address_fk`) REFERENCES `address` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `staff_to_store_fk` FOREIGN KEY (`store_fk`) REFERENCES `store` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='staff data table';
 
 --
@@ -289,7 +297,9 @@ CREATE TABLE `store` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `address_fk` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `store_name_IDX` (`name`) USING BTREE,
-  CONSTRAINT `store_address_fk` FOREIGN KEY (`id`) REFERENCES `address` (`id`) ON UPDATE CASCADE
+  KEY `store_to_address_fk` (`address_fk`),
+  CONSTRAINT `store_to_address_fk` FOREIGN KEY (`address_fk`) REFERENCES `address` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='store data table';
