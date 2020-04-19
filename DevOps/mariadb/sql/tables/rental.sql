40f88b207209983b138697d36efd46bbf363f353
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-20 01:06:49 +0200

    Update rental.sql

--- DevOps/mariadb/sql/tables/rental.sql
+++ DevOps/mariadb/sql/tables/rental.sql
@@ -3,8 +3,14 @@ CREATE TABLE `rental` (
   `rent` date NOT NULL,
   `back` date NOT NULL,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `inventory_fk` bigint(20) unsigned DEFAULT NULL,
+  `customer_fk` bigint(20) unsigned DEFAULT NULL,
+  `staff_fk` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `rental_customer_fk` FOREIGN KEY (`id`) REFERENCES `customer` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `rental_inventory_fk` FOREIGN KEY (`id`) REFERENCES `inventory` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `rental_staff_fk` FOREIGN KEY (`id`) REFERENCES `staff` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='rental data table';
+  KEY `rental_to_inventory_fk` (`inventory_fk`),
+  KEY `rental_to_customer_fk` (`customer_fk`),
+  KEY `rental_to_staff_fk` (`staff_fk`),
+  CONSTRAINT `rental_to_customer_fk` FOREIGN KEY (`customer_fk`) REFERENCES `customer` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `rental_to_inventory_fk` FOREIGN KEY (`inventory_fk`) REFERENCES `inventory` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `rental_to_staff_fk` FOREIGN KEY (`staff_fk`) REFERENCES `staff` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='rental data table'
