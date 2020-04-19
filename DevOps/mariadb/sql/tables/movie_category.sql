bd8de3daa86e001415f457d117a0268b507f6251
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-19 17:39:52 +0200

    Update movie_category.sql

--- DevOps/mariadb/sql/tables/movie_category.sql
+++ DevOps/mariadb/sql/tables/movie_category.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `movie_category` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`),
-  CONSTRAINT `category_fk` FOREIGN KEY (`id`) REFERENCES `category` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `movie_fk` FOREIGN KEY (`id`) REFERENCES `movie` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='movie_category data table';
+  `movie_id` bigint(20) unsigned NOT NULL,
+  `category_id` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`movie_id`,`category_id`),
+  KEY `movie_category_to_category_fk` (`category_id`),
+  CONSTRAINT `movie_category_to_category_fk` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
+  CONSTRAINT `movie_category_to_movie_fk` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
