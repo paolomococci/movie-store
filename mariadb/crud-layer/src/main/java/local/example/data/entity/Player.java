4c185dfbe2754a4b2a4f1e00580fc43277e17454
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-19 17:44:20 +0200

    Update Player.java

--- mariadb/crud-layer/src/main/java/local/example/data/entity/Player.java
+++ mariadb/crud-layer/src/main/java/local/example/data/entity/Player.java
@@ -19,11 +19,13 @@
 package local.example.data.entity;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -71,4 +73,9 @@ public class Player {
 
 	@Getter
 	private Timestamp updated;
+	
+	@Getter
+	@Setter
+	@ManyToMany(mappedBy = "players")
+	private List<Movie> movies;
 }
