3e2caea918eb161e141406a1a0da3a0a888728ff
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-21 19:51:42 +0200

    Update content.sql

--- DevOps/mariadb/sql/tables/content.sql
+++ DevOps/mariadb/sql/tables/content.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `content` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `subject` text DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='content data table';
