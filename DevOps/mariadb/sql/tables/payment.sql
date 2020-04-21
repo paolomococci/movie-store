914bf36fb26a1e10a0472eb26ab3171ea7dd97ef
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-21 19:52:25 +0200

    Update payment.sql

--- DevOps/mariadb/sql/tables/payment.sql
+++ DevOps/mariadb/sql/tables/payment.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `payment` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `amount` decimal(10,0) NOT NULL,
+  `amount` double NOT NULL,
   `payed` date NOT NULL,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `customer_fk` bigint(20) unsigned DEFAULT NULL,
@@ -13,4 +13,4 @@ CREATE TABLE `payment` (
   CONSTRAINT `payment_to_customer_fk` FOREIGN KEY (`customer_fk`) REFERENCES `customer` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `payment_to_rental_fk` FOREIGN KEY (`rental_fk`) REFERENCES `rental` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `payment_to_staff_fk` FOREIGN KEY (`staff_fk`) REFERENCES `staff` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='payment data table'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='payment data table';
