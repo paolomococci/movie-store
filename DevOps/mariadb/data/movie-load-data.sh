02fd552fba39644348c50aace1a8acad4db1c7df
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-27 07:39:57 +0200

    Update movie-load-data.sh

--- DevOps/mariadb/data/movie-load-data.sh
+++ DevOps/mariadb/data/movie-load-data.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 
+curl -v -i -H "Content-Type:application/json" -d '{"title":"","subtitle":"","description":"","comeout":"","duration":"","cost":0.0,"rent":0.0,"rating":0.0}' http://127.0.0.1:8080/movies
