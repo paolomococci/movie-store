c819080e8bbdb964e6afe8d9f0779170533c1e53
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-27 07:43:11 +0200

    Update staff-load-data.sh

--- DevOps/mariadb/data/staff-load-data.sh
+++ DevOps/mariadb/data/staff-load-data.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 
+curl -v -i -H "Content-Type:application/json" -d '{"name":"","nickname":"","surname":"","phone":"","mobile":"","email":"","username":"password","":"","active":true}' http://127.0.0.1:8080/teams
