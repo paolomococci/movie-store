d650248223e01454eb5772198eb933624b836f6f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-27 07:38:15 +0200

    Update customer-load-data.sh

--- DevOps/mariadb/data/customer-load-data.sh
+++ DevOps/mariadb/data/customer-load-data.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 
+curl -v -i -H "Content-Type:application/json" -d '{"name":"","nickname":"","surname":"","phone":"","mobile":"","email":"","birthday":"","active":true}' http://127.0.0.1:8080/customers
