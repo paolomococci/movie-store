637cfc576ab8c4d2dc1184691320a65b4a871c76
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-04-28 11:04:05 +0200

    Update RentalRepoMockMvcParametrizedTests.java

--- mariadb/crud-layer/src/test/java/local/example/data/repository/RentalRepoMockMvcParametrizedTests.java
+++ mariadb/crud-layer/src/test/java/local/example/data/repository/RentalRepoMockMvcParametrizedTests.java
@@ -18,28 +18,68 @@
 
 package local.example.data.repository;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.net.URI;
 import java.util.stream.Stream;
 
+import javax.validation.ConstraintViolationException;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestMethodOrder(OrderAnnotation.class)
 public class RentalRepoMockMvcParametrizedTests {
 
-	private static String RENTAL_TEST_STRING = "{}"; // TODO
+	private static String RENTAL_TEST_STRING = "{\"rent\":\"\",\"back\":\"\"}";
 	private static URI uri;
 
 	@Autowired
 	MockMvc mockMvc;
 	
-	// TODO
+	@Test
+	@Order(1)
+	@DisplayName("create a record into rental's data table")
+	void createTest() throws 
+			Exception, 
+			DataIntegrityViolationException, 
+			ConstraintViolationException {
+		MvcResult mvcResult = mockMvc
+			.perform(post("/rentals").content(RENTAL_TEST_STRING))
+			.andExpect(status().isCreated())
+			.andReturn();
+		setUri(new URI(mvcResult.getResponse().getHeader("Location")));
+	}
+
+	@Order(2)
+	@Disabled
+	@ParameterizedTest
+	@MethodSource("initUri")
+	@DisplayName("read a record from the rental's data table identifying it from the uri")
+	void readTest(String uri) 
+			throws Exception {
+		mockMvc.perform(get(uri))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.rent").value(""))
+			.andExpect(jsonPath("$.back").value(""));
+	}
 
 	private static URI getUri() {
 		return uri;
