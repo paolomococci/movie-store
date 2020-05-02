d8161c2627d91d458505ada6d5b84ba4dba03bd9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-05-02 08:36:28 +0200

    Update Application.java

--- mariadb/crud-layer/src/main/java/local/example/data/Application.java
+++ mariadb/crud-layer/src/main/java/local/example/data/Application.java
@@ -18,13 +18,23 @@
 
 package local.example.data;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
+
