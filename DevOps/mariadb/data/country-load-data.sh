b1edf5814fc771f16edaf9f71f8ced21020ba824
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-05-03 18:00:06 +0200

    Update country-load-data.sh

--- DevOps/mariadb/data/country-load-data.sh
+++ DevOps/mariadb/data/country-load-data.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-curl -v -i -H "Content-Type:application/json" -d '{"name":""}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Abruzzo","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Basilicata","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Calabria","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Campania","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Emilia_Romagna","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Friuli_Venezia_Giulia","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Lazio","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Liguria","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Lombardia","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Marche","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Molise","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Piemonte","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Puglia","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Sardegna","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Sicilia","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Toscana","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Trentino_Alto_Adige","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Umbria","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Val_D_Aosta","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
+curl -v -i -H "Content-Type:application/json" -d '{"name":"Veneto","iso3166":"http://127.0.0.1:8080/countryCodes/20585"}' http://127.0.0.1:8080/countries
