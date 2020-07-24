0a779acb416a80564825ad8e837f2b083cbab2a9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-07-24 20:14:42 +0200

    Update Item.java

--- templates/app-layout-template/src/main/java/local/example/demo/model/Item.java
+++ templates/app-layout-template/src/main/java/local/example/demo/model/Item.java
@@ -18,6 +18,20 @@
 
 package local.example.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class Item {
 
+	private Long id;
+	private String code;
+	private String name;
 }
