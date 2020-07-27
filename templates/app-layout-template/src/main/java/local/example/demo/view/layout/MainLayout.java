61b7a1f14558f6230c9f2ee4603ffccd7681de53
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-07-27 08:17:52 +0200

    Update MainLayout.java

--- templates/app-layout-template/src/main/java/local/example/demo/view/layout/MainLayout.java
+++ templates/app-layout-template/src/main/java/local/example/demo/view/layout/MainLayout.java
@@ -19,10 +19,25 @@
 package local.example.demo.view.layout;
 
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.html.ListItem;
+import com.vaadin.flow.component.html.Nav;
+import com.vaadin.flow.component.html.UnorderedList;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.router.RouterLink;
+
+import local.example.demo.view.FifthView;
+import local.example.demo.view.FirstView;
+import local.example.demo.view.FourthView;
+import local.example.demo.view.ItemView;
+import local.example.demo.view.MainView;
+import local.example.demo.view.SecondView;
+import local.example.demo.view.ThirdView;
 
 @Push
 @CssImport(value = "style.css")
@@ -32,12 +47,68 @@ public class MainLayout
 
 	private static final long serialVersionUID = 8822844864605786754L;
 
+	private final H1 title;
+	private final RouterLink mainView;
+	private final RouterLink firstView;
+	private final RouterLink secondView;
+	private final RouterLink thirdView;
+	private final RouterLink fourthView;
+	private final RouterLink fifthView;
+	private final RouterLink itemView;
+	private final UnorderedList unorderedList;
+	private final Header header;
+	private final Nav nav;
+
 	public MainLayout() {
 		super();
+		
+		this.title = new H1("reactive RESTful web service data accessing");
+		
+		this.mainView = new RouterLink("main view", MainView.class);
+		this.firstView = new RouterLink("first view", FirstView.class);
+		this.secondView = new RouterLink("second view", SecondView.class);
+		this.thirdView = new RouterLink("third view", ThirdView.class);
+		this.fourthView = new RouterLink("fourth view", FourthView.class);
+		this.fifthView = new RouterLink("fifth view", FifthView.class);
+		this.itemView = new RouterLink("item view", ItemView.class);
+		
+		this.unorderedList = new UnorderedList(
+				new ListItem(this.mainView), 
+				new ListItem(this.firstView),  
+				new ListItem(this.secondView), 
+				new ListItem(this.thirdView), 
+				new ListItem(this.fourthView), 
+				new ListItem(this.fifthView),  
+				new ListItem(this.itemView)
+		);
+		
+		this.header = new Header(new DrawerToggle(), this.title);
+		this.nav = new Nav(this.unorderedList);
+		
+		this.addToNavbar(this.header);
+		this.addToDrawer(this.nav);
+		this.setPrimarySection(Section.DRAWER);
+		this.setDrawerOpened(false);
+	}
+
+	private RouterLink[] listLinks() {
+		return new RouterLink[] {
+				this.mainView, 
+				this.firstView,  
+				this.secondView, 
+				this.thirdView, 
+				this.fourthView, 
+				this.fifthView,  
+				this.itemView
+				};
 	}
 
 	@Override
 	public void afterNavigation(AfterNavigationEvent afterNavigationEvent) {
-		// TODO
+		for (final RouterLink link : this.listLinks()) {
+			if (link.getHighlightCondition().shouldHighlight(link, afterNavigationEvent)) {
+				this.title.setText(link.getText());
+			}
+		}
 	}
 }
