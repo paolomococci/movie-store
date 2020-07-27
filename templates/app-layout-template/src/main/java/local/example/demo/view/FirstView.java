ba2fdd292b401b26c8efe9754d7696ec32f37cbf
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-07-27 08:13:56 +0200

    Update FirstView.java

--- templates/app-layout-template/src/main/java/local/example/demo/view/FirstView.java
+++ templates/app-layout-template/src/main/java/local/example/demo/view/FirstView.java
@@ -18,14 +18,17 @@
 
 package local.example.demo.view;
 
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.Section;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 import local.example.demo.view.layout.MainLayout;
 
 @Route(value = "first", layout = MainLayout.class)
-@PageTitle(value = "first")
+@PageTitle(value = "first view")
 public class FirstView 
 		extends Main {
 	
@@ -34,7 +37,16 @@ public class FirstView
 	@SuppressWarnings("unused")
 	private static final String RESTFUL_URI = "";
 
+	private final Section section;
+	private final H2 subtitle;
+	private final Paragraph paragraph;
+
 	public FirstView() {
 		super();
+		this.paragraph = new Paragraph();
+		this.subtitle = new H2("sample of subtitle");
+		this.paragraph.add("sample of paragraph");
+		this.section = new Section(this.subtitle, this.paragraph);
+		this.add(this.section);
 	}
 }
