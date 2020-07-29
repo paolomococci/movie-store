28b638198cb7c436a3103db911f5b46ef39a8432
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-07-29 15:09:25 +0200

    Create Iso3166View.java

---
+++ mariadb/view-layer/src/main/java/local/example/data/view/Iso3166View.java
@@ -0,0 +1,52 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.view;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.Section;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+import local.example.data.view.layout.MainLayout;
+
+@Route(value = "iso3166", layout = MainLayout.class)
+@PageTitle(value = "iso3166 view")
+public class Iso3166View 
+		extends Main {
+
+	private static final long serialVersionUID = 9076892825614946696L;
+
+	@SuppressWarnings("unused")
+	private static final String RESTFUL_URI = "";
+
+	private final Section section;
+	private final H2 subtitle;
+	private final Paragraph paragraph;
+
+	public Iso3166View() {
+		super();
+		this.paragraph = new Paragraph();
+		this.subtitle = new H2("iso3166 view");
+		this.paragraph.add("sample of paragraph");
+		this.section = new Section(this.subtitle, this.paragraph);
+		this.add(this.section);
+	}
+}
