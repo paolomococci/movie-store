ce6ea44a9178d315c48963606c7181135faecbbd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-01 23:16:10 +0200

    Update DistrictView.java

--- mariadb/view-layer/src/main/java/local/example/data/view/DistrictView.java
+++ mariadb/view-layer/src/main/java/local/example/data/view/DistrictView.java
@@ -18,7 +18,10 @@
 
 package local.example.data.view;
 
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.Section;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -29,13 +32,14 @@ import local.example.data.view.layout.MainLayout;
 public class DistrictView 
 		extends Main {
 
-	private static final long serialVersionUID = 3048208635761330612L;
-
-	@SuppressWarnings("unused")
 	private static final String RESTFUL_URI = "";
 
 	public DistrictView() {
 		super();
-		// TODO
+		Paragraph paragraph = new Paragraph();
+		H2 subtitle = new H2("district view");
+		paragraph.add("sample of paragraph");
+		Section section = new Section(subtitle, paragraph);
+		this.add(section);
 	}
 }
