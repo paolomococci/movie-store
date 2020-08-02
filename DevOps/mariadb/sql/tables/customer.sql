0b1acf01c22e2baf4a51dabb4e26fc4b80825099
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-02 23:31:52 +0200

    Update customer.sql

--- DevOps/mariadb/sql/tables/customer.sql
+++ DevOps/mariadb/sql/tables/customer.sql
@@ -1,4 +1,5 @@
-CREATE TABLE `customer` (
+DROP TABLE IF EXISTS moviestoredb.customer;
+CREATE TABLE moviestoredb.customer (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `nickname` varchar(100) DEFAULT NULL,
