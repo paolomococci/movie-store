89cfb391de2689fa1de5e8c887029e2b0660d095
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-02 23:32:37 +0200

    Update district.sql

--- DevOps/mariadb/sql/tables/district.sql
+++ DevOps/mariadb/sql/tables/district.sql
@@ -1,4 +1,5 @@
-CREATE TABLE `district` (
+DROP TABLE IF EXISTS moviestoredb.district;
+CREATE TABLE moviestoredb.district (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `code` varchar(6) NOT NULL,
