f42cb5b71f780ae3148097a794e80d3e7cc3a2c0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-02 23:30:54 +0200

    Update country.sql

--- DevOps/mariadb/sql/tables/country.sql
+++ DevOps/mariadb/sql/tables/country.sql
@@ -1,4 +1,5 @@
-CREATE TABLE `country` (
+DROP TABLE IF EXISTS moviestoredb.country;
+CREATE TABLE moviestoredb.country (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -6,4 +7,4 @@ CREATE TABLE `country` (
   PRIMARY KEY (`id`),
   KEY `country_to_iso3166_fk` (`iso3166_id`),
   CONSTRAINT `country_to_iso3166_fk` FOREIGN KEY (`iso3166_id`) REFERENCES `iso3166` (`id`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='country data table';
