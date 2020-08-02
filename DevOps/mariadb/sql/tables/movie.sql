193882bd5cac43602d301b1a915ac5505fd0d7c5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-02 23:35:42 +0200

    Update movie.sql

--- DevOps/mariadb/sql/tables/movie.sql
+++ DevOps/mariadb/sql/tables/movie.sql
@@ -1,17 +1,19 @@
-CREATE TABLE `movie` (
+DROP TABLE IF EXISTS moviestoredb.movie;
+CREATE TABLE moviestoredb.movie (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
   `subtitle` varchar(100) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
+  `description` text NOT NULL,
   `comeout` date NOT NULL,
   `duration` time NOT NULL,
   `cost` double NOT NULL,
   `rent` double NOT NULL,
   `rating` double DEFAULT NULL,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `desciption` varchar(255) NOT NULL,
   `language_fk` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `movie_to_language_fk` (`language_fk`),
-  FULLTEXT KEY `movie_title_IDX` (`title`,`subtitle`),
+  FULLTEXT KEY `movie_title_IDX` (`title`,`subtitle`,`description`),
   CONSTRAINT `movie_to_language_fk` FOREIGN KEY (`language_fk`) REFERENCES `language` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='movie data table';
