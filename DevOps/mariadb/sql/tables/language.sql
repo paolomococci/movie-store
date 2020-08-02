5b9d2637de45e07e77c78d7fa3cc236fcb798935
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-02 23:34:08 +0200

    Update language.sql

--- DevOps/mariadb/sql/tables/language.sql
+++ DevOps/mariadb/sql/tables/language.sql
@@ -1,4 +1,5 @@
-CREATE TABLE `language` (
+DROP TABLE IF EXISTS moviestoredb.language;
+CREATE TABLE moviestoredb.language (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
