ff8e39f6ebdb4e1d67d448e620785cc4239850f5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-03 07:56:19 +0200

    Update movie_player.sql

--- DevOps/mariadb/sql/tables/movie_player.sql
+++ DevOps/mariadb/sql/tables/movie_player.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `movie_player` (
+DROP TABLE IF EXISTS moviestoredb.movie_player;
+CREATE TABLE moviestoredb.movie_player (
   `movie_id` bigint(20) unsigned NOT NULL,
   `player_id` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`movie_id`,`player_id`),
-  KEY `movie_player_to_player_fk` (`player_id`),
   CONSTRAINT `movie_player_to_movie_fk` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`),
   CONSTRAINT `movie_player_to_player_fk` FOREIGN KEY (`player_id`) REFERENCES `player` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
