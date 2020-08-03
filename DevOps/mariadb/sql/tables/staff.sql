9490efb219a0b746da44a8440a3df5463362c56b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-03 08:00:12 +0200

    Update staff.sql

--- DevOps/mariadb/sql/tables/staff.sql
+++ DevOps/mariadb/sql/tables/staff.sql
@@ -1,4 +1,5 @@
-CREATE TABLE `staff` (
+DROP TABLE IF EXISTS moviestoredb.staff;
+CREATE TABLE moviestoredb.staff (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `nickname` varchar(100) DEFAULT NULL,
@@ -14,8 +15,8 @@ CREATE TABLE `staff` (
   `address_fk` bigint(20) unsigned DEFAULT NULL,
   `store_fk` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `staff_to_store_fk` (`store_fk`),
   KEY `staff_to_address_fk` (`address_fk`),
+  KEY `staff_to_store_fk` (`store_fk`),
   CONSTRAINT `staff_to_address_fk` FOREIGN KEY (`address_fk`) REFERENCES `address` (`id`) ON DELETE SET NULL,
   CONSTRAINT `staff_to_store_fk` FOREIGN KEY (`store_fk`) REFERENCES `store` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='staff data table';
