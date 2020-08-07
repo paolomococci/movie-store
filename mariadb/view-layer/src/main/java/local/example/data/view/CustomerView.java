6c25665c4922772e217fd45a3d367dd687c88634
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-07 08:06:58 +0200

    Update CustomerView.java

--- mariadb/view-layer/src/main/java/local/example/data/view/CustomerView.java
+++ mariadb/view-layer/src/main/java/local/example/data/view/CustomerView.java
@@ -25,12 +25,17 @@ import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.html.Section;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 import local.example.data.model.Customer;
 import local.example.data.retrieve.RestfulRetriever;
 import local.example.data.view.layout.MainLayout;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.net.URI;
 
 @Route(value = "customer", layout = MainLayout.class)
 @PageTitle(value = "customer view")
@@ -46,11 +51,23 @@ public class CustomerView
 		super();
 		Paragraph paragraph = new Paragraph();
 		H2 subtitle = new H2("customer view");
-		paragraph.add("sample of paragraph");
+		paragraph.add("this is the list of registered customers");
 		Section section = new Section(subtitle, paragraph);
 		this.customerGrid = new Grid<>();
 		this.customerRestfulRetriever = new RestfulRetriever<>();
-		Button customerRetrieveButton = new Button();
+		Button customerRetrieveButton = new Button(
+				"recovers all customers",
+				VaadinIcon.ARROW_CIRCLE_DOWN_O.create(),
+				listener -> {
+					try {
+						this.customerGrid.setItems(
+								this.customerRestfulRetriever.getListOfItems(URI.create(RESTFUL_URI), "customers")
+						);
+					} catch (
+							ResponseStatusException | IOException exception) {
+						exception.printStackTrace();
+					}
+				});
 		customerRetrieveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 		this.add(section);
 	}
