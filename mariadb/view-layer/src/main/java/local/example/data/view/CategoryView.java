e9c1512e9f0273851126296ce7642e78212e8a14
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-07 08:09:10 +0200

    Update CategoryView.java

--- mariadb/view-layer/src/main/java/local/example/data/view/CategoryView.java
+++ mariadb/view-layer/src/main/java/local/example/data/view/CategoryView.java
@@ -20,17 +20,23 @@ package local.example.data.view;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.html.Section;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 import local.example.data.model.Category;
 import local.example.data.retrieve.RestfulRetriever;
 import local.example.data.view.layout.MainLayout;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.net.URI;
 
 @Route(value = "category", layout = MainLayout.class)
 @PageTitle(value = "category view")
@@ -46,11 +52,29 @@ public class CategoryView
 		super();
 		Paragraph paragraph = new Paragraph();
 		H2 subtitle = new H2("category view");
-		paragraph.add("sample of paragraph");
+		paragraph.add("this is the list of registered categories");
 		Section section = new Section(subtitle, paragraph);
 		this.categoryGrid = new Grid<>();
+		this.categoryGrid.addColumn(Category::getName)
+				.setHeader("name")
+				.setSortable(true)
+				.setTextAlign(ColumnTextAlign.START);
+		this.categoryGrid.addColumn(Category::getName)
+				.setHeader("description");
 		this.categoryRestfulRetriever = new RestfulRetriever<>();
-		Button categoryRetrieveButton = new Button();
+		Button categoryRetrieveButton = new Button(
+				"recovers all categories",
+				VaadinIcon.ARROW_CIRCLE_DOWN_O.create(),
+				listener -> {
+					try {
+						this.categoryGrid.setItems(
+								this.categoryRestfulRetriever.getListOfItems(URI.create(RESTFUL_URI), "categories")
+						);
+					} catch (
+							ResponseStatusException | IOException exception) {
+						exception.printStackTrace();
+					}
+				});
 		categoryRetrieveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 		this.add(section);
 	}
