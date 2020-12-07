1501bed7e1b27c2018e1f013fcaba8a7f653092d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-07 19:39:22 +0100

    Update AboutView.vue

--- mariadb/frontend-layer/src/views/AboutView.vue
+++ mariadb/frontend-layer/src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <h1>about view</h1>
+    <welcome msg="about view"/>
   </div>
 </template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
