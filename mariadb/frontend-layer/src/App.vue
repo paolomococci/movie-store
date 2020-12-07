3a25d8f1b156b42837d6e54c0c47b78a8fd72690
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-07 19:08:01 +0100

    Create App.vue

---
+++ mariadb/frontend-layer/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link class="routerLink" to="/">home</router-link> |
+      <router-link class="routerLink" to="/about">about</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+
+<style lang="scss">
+.routerLink {
+  margin: 5px;
+}
+</style>
