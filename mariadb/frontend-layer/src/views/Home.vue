bd476f054e39acc3ae561e094819a0cb0b76cad3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-07 19:10:35 +0100

    Create Home.vue

---
+++ mariadb/frontend-layer/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <hello msg="home view"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    hello: HelloWorld
+  }
+}
+</script>
