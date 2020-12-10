18724fe9e60588676d700e966313785197a7dfe3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:24:57 +0100

    Create Iso3166Component.vue

---
+++ mariadb/frontend-layer/src/components/Iso3166Component.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1><span v-text="msg"></span></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Iso3166Component',
+  props: {
+    msg: String
+  }
+}
+</script>
