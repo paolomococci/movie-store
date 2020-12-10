796f0b3a3ec470a8b2cde8c8d892048f3f5b4538
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:00:15 +0100

    Create StaffView.vue

---
+++ mariadb/frontend-layer/src/views/StaffView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <welcome msg="staff view"/>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'StaffView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
