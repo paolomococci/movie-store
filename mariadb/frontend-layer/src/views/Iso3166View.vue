f99ead7a8288d8b02f9b6518fd811ba24b5d0246
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:03:48 +0100

    Create Iso3166View.vue

---
+++ mariadb/frontend-layer/src/views/Iso3166View.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <welcome msg="iso3166 view"/>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'Iso3166View',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
