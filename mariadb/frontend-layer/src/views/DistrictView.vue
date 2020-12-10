e580bcb8b988e28565ed1b02ac749f8738f695a0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:04:50 +0100

    Create DistrictView.vue

---
+++ mariadb/frontend-layer/src/views/DistrictView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <welcome msg="district view"/>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'DistrictView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
