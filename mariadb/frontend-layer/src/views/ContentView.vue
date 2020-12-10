53ba7a9ca36d2083632d6c24c0ca1afef7bed716
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:07:32 +0100

    Create ContentView.vue

---
+++ mariadb/frontend-layer/src/views/ContentView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <welcome msg="content view"/>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'ContentView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
