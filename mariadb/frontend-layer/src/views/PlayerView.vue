3d47ec3d04fda6042164d2385ef325206060b38a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:01:42 +0100

    Create PlayerView.vue

---
+++ mariadb/frontend-layer/src/views/PlayerView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <welcome msg="player view"/>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'PlayerView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
