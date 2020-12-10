ef63fcd55c1d5ddbe154626fa02fe6bd1919e116
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:03:21 +0100

    Create LanguageView.vue

---
+++ mariadb/frontend-layer/src/views/LanguageView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <welcome msg="language view"/>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'LanguageView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
