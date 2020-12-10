7a59969dcfe2da103ea51a078cefbc0b8da61bf2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:22:38 +0100

    Create RentalComponent.vue

---
+++ mariadb/frontend-layer/src/components/RentalComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1><span v-text="msg"></span></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RentalComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
