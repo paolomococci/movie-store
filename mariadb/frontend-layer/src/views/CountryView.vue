634ed49797b3865c2a4e32ed649b26e0db1eb1f3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:06:56 +0100

    Create CountryView.vue

---
+++ mariadb/frontend-layer/src/views/CountryView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <welcome msg="country view"/>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'CountryView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
