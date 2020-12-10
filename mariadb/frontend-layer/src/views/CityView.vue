6b936d3691eabb10c8172be563dad068d57722b0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:08:15 +0100

    Create CityView.vue

---
+++ mariadb/frontend-layer/src/views/CityView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <welcome msg="city view"/>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'CityView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
