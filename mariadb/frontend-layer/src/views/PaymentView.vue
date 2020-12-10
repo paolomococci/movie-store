043cae825b6d408e2da1d374eed1f3029111ccea
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:02:17 +0100

    Create PaymentView.vue

---
+++ mariadb/frontend-layer/src/views/PaymentView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <welcome msg="payment view"/>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'PaymentView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
