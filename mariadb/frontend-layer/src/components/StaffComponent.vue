ec4779dd85a4331870dca4b11f62ab70e89adb49
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:21:51 +0100

    Create StaffComponent.vue

---
+++ mariadb/frontend-layer/src/components/StaffComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1><span v-text="msg"></span></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StaffComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
