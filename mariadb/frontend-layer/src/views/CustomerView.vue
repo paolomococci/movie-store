842349304cb84144deee12459f85154a4b3dc5f7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:06:30 +0100

    Create CustomerView.vue

---
+++ mariadb/frontend-layer/src/views/CustomerView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <welcome msg="customer view"/>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'CustomerView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
