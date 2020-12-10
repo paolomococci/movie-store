0322fea20bb029034d171221de959603c1690e2c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:28:44 +0100

    Create AddressComponent.vue

---
+++ mariadb/frontend-layer/src/components/AddressComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1><span v-text="msg"></span></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddressComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
