86ab18b129709c0b4686eddcd9e36412c68bc30b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:02:50 +0100

    Create MovieView.vue

---
+++ mariadb/frontend-layer/src/views/MovieView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <welcome msg="movie view"/>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'MovieView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
