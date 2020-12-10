6042a8a52094c8ef80f1c296c247b8ed8e7357bd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:08:45 +0100

    Create CategoryView.vue

---
+++ mariadb/frontend-layer/src/views/CategoryView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <welcome msg="category view"/>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'CategoryView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
