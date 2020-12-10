0c71c6f63bd9b7920c15de8ae0f26eaa2cbd3b4f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:04:14 +0100

    Create InventoryView.vue

---
+++ mariadb/frontend-layer/src/views/InventoryView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <welcome msg="inventory view"/>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'InventoryView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
