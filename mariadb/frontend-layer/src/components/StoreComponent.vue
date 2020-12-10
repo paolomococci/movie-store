0ec8954d5848095ec9235cd33dfda26a7a43a985
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:21:26 +0100

    Create StoreComponent.vue

---
+++ mariadb/frontend-layer/src/components/StoreComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1><span v-text="msg"></span></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StoreComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
