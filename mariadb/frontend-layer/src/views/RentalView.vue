0956df72fc70a32e340ba0968adb108fbc32400f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-10 23:00:54 +0100

    Create RentalView.vue

---
+++ mariadb/frontend-layer/src/views/RentalView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <welcome msg="rental view"/>
+  </div>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'RentalView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
