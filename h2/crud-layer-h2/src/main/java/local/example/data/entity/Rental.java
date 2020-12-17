9f406cc326eca94efd0dff577b6a8e2744c33208
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-17 18:07:01 +0100

    Create Rental.java

---
+++ h2/crud-layer-h2/src/main/java/local/example/data/entity/Rental.java
@@ -0,0 +1,88 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.entity;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Rental {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Getter
+	private long id;
+
+	@Getter
+	@Setter
+	@NotNull
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	@PastOrPresent(groups = java.sql.Date.class)
+	private Date rent;
+
+	@Getter
+	@Setter
+	@NotNull
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	@Future(groups = java.sql.Date.class)
+	private Date back;
+
+	@Getter
+	private Timestamp updated;
+	
+	@Getter
+	@Setter
+	@ManyToOne
+	@JoinColumn(name = "inventory_fk")
+	private Inventory inventory;
+	
+	@Getter
+	@Setter
+	@ManyToOne
+	@JoinColumn(name = "customer_fk")
+	private Customer customer;
+	
+	@Getter
+	@Setter
+	@ManyToOne
+	@JoinColumn(name = "staff_fk")
+	private Staff staff;
+	
+	@Getter
+	@Setter
+	@OneToMany(mappedBy = "rental")
+	private List<Payment> payments;
+}
