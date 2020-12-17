4a92928cc6259302f327d54fc536afa943e85b6c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-17 18:03:32 +0100

    Create Category.java

---
+++ h2/crud-layer-h2/src/main/java/local/example/data/entity/Category.java
@@ -0,0 +1,70 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.entity;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Getter
+	private long id;
+
+	@Getter
+	@Setter
+	@NotNull
+	@Size(min = 3, max = 50, message = "size range of this field is 3 to 50 characters")
+	@Pattern(
+			regexp = "^[a-zA-Z_-]*$", 
+			message = "only the characters `a-z`, `A-Z`, `_` and `-` are allowed"
+			)
+	private String name;
+
+	@Getter
+	@Setter
+	@Size(max = 200, message = "max field is 200 characters")
+	@Pattern(
+			regexp = "^[a-zA-Z]*[a-zA-Z0-9 -]*[a-zA-Z]*$", 
+			message = "only the characters `a-z`, `A-Z`, `_`, `-` and space are allowed"
+			)
+	private String description;
+
+	@Getter
+	private Timestamp updated;
+	
+	@Getter
+	@Setter
+	@ManyToMany(mappedBy = "categories")
+	private List<Movie> movies;
+}
