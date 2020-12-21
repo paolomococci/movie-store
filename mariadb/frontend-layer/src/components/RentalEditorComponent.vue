f874e663449212a90ea6ca3806e0f968504079a3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-21 21:32:47 +0100

    Create RentalEditorComponent.vue

---
+++ mariadb/frontend-layer/src/components/RentalEditorComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <!-- TODO -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'RentalEditorComponent',
+  props: {
+    uri: String
+  }
+}
+</script>
