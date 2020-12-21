8201f98b04af9dc47e8db73fefe6bd0cee329d25
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-21 21:13:04 +0100

    Create CategoryEditorComponent.vue

---
+++ mariadb/frontend-layer/src/components/CategoryEditorComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <!-- TODO -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CategoryEditorComponent',
+  props: {
+    uri: String
+  }
+}
+</script>
