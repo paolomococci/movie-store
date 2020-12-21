a0286e2db0e344347c7bd998cea4246167f8a864
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-21 21:31:07 +0100

    Create PaymentEditorComponent.vue

---
+++ mariadb/frontend-layer/src/components/PaymentEditorComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <!-- TODO -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PaymentEditorComponent',
+  props: {
+    uri: String
+  }
+}
+</script>
