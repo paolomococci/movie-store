a2e7a90a090c7c39e589c0968b52bd1e35c4f083
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-21 21:26:19 +0100

    Create DistrictEditorComponent.vue

---
+++ mariadb/frontend-layer/src/components/DistrictEditorComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <!-- TODO -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'DistrictEditorComponent',
+  props: {
+    uri: String
+  }
+}
+</script>
