08dd17a80bb0cf655b543f35f02e10639c246d41
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-21 21:27:17 +0100

    Create InventoryEditorComponent.vue

---
+++ mariadb/frontend-layer/src/components/InventoryEditorComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <!-- TODO -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'InventoryEditorComponent',
+  props: {
+    uri: String
+  }
+}
+</script>
