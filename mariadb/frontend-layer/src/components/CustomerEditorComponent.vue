6f46c23a9a177c3fc707d1b2af7e1ee4055e03bb
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-21 21:18:03 +0100

    Create CustomerEditorComponent.vue

---
+++ mariadb/frontend-layer/src/components/CustomerEditorComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <!-- TODO -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CustomerEditorComponent',
+  props: {
+    uri: String
+  }
+}
+</script>
