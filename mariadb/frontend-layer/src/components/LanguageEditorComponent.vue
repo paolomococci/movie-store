c01b185c190a12e58a86fac0c6a54ac4a6cfd717
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-21 21:29:10 +0100

    Create LanguageEditorComponent.vue

---
+++ mariadb/frontend-layer/src/components/LanguageEditorComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <!-- TODO -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'LanguageEditorComponent',
+  props: {
+    uri: String
+  }
+}
+</script>
