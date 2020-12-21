516bcdf673b4b411eb2051f118b81d00fed5b3d3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-21 21:16:55 +0100

    Create CountryEditorComponent.vue

---
+++ mariadb/frontend-layer/src/components/CountryEditorComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <!-- TODO -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CountryEditorComponent',
+  props: {
+    uri: String
+  }
+}
+</script>
