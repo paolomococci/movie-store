3405b7f5bc2750db6b061afc7b44690fa13146fd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-05 20:47:55 +0100

    Update CityRestController.java

--- h2/crud-layer-h2/src/main/java/local/example/data/controller/CityRestController.java
+++ h2/crud-layer-h2/src/main/java/local/example/data/controller/CityRestController.java
@@ -18,10 +18,38 @@
 
 package local.example.data.controller;
 
+import local.example.data.reference.CityReference;
+import local.example.data.repository.CityRestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/references")
+@RequestMapping(value = "/api/city/references")
 public class CityRestController {
+
+    @Autowired
+    CityRestRepository cityRestRepository;
+
+    @PatchMapping("/subscribe")
+    public ResponseEntity<?> subscribeUpdate(
+            @RequestBody CityReference cityReference
+    )
+            throws Exception {
+        // TODO
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @PatchMapping("/debar")
+    public ResponseEntity<?> debarUpdate(
+            @RequestBody CityReference cityReference
+    )
+            throws Exception {
+        // TODO
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
 }
