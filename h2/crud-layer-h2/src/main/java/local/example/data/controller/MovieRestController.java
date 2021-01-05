075d7644c37595888bb39c6e6e5a3106be0e56ef
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-05 20:18:44 +0100

    Update MovieRestController.java

--- h2/crud-layer-h2/src/main/java/local/example/data/controller/MovieRestController.java
+++ h2/crud-layer-h2/src/main/java/local/example/data/controller/MovieRestController.java
@@ -18,5 +18,10 @@
 
 package local.example.data.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/references")
 public class MovieRestController {
 }
