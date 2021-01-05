3758aa35691553dacefde45a5a210be06c9ee8d8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-05 20:44:58 +0100

    Update RentalRestController.java

--- h2/crud-layer-h2/src/main/java/local/example/data/controller/RentalRestController.java
+++ h2/crud-layer-h2/src/main/java/local/example/data/controller/RentalRestController.java
@@ -18,10 +18,38 @@
 
 package local.example.data.controller;
 
+import local.example.data.reference.RentalReference;
+import local.example.data.repository.RentalRestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/references")
+@RequestMapping(value = "/api/rental/references")
 public class RentalRestController {
+
+    @Autowired
+    RentalRestRepository rentalRestRepository;
+
+    @PatchMapping("/subscribe")
+    public ResponseEntity<?> subscribeUpdate(
+            @RequestBody RentalReference rentalReference
+    )
+            throws Exception {
+        // TODO
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @PatchMapping("/debar")
+    public ResponseEntity<?> debarUpdate(
+            @RequestBody RentalReference rentalReference
+    )
+            throws Exception {
+        // TODO
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
 }
