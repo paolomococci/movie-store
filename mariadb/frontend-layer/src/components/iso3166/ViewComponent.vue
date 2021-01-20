a1ed259f41810ca1e9a8b003a5bcd4d8de0305a5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-20 09:16:11 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/iso3166/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/iso3166/ViewComponent.vue
@@ -6,9 +6,21 @@
 
 <script>
 export default {
-  name: 'Iso3166Component',
+  name: 'ViewComponent',
   props: {
     msg: String
+  },
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
   }
 }
 </script>
