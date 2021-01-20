1516c9a4a3b041dda7db3e120e42f2b25bf71c69
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-20 09:18:42 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/language/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/language/ViewComponent.vue
@@ -6,9 +6,21 @@
 
 <script>
 export default {
-  name: 'LanguageComponent',
+  name: 'ViewComponent',
   props: {
     msg: String
+  },
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
   }
 }
 </script>
