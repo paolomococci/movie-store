2d32a48a71786328babbe0ebf153514e6f25f488
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-20 08:48:11 +0100

    Create HeaderComponent.vue

---
+++ mariadb/frontend-layer/src/components/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <!-- TODO-->
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'HeaderComponent',
+  components: {
+    // TODO
+  },
+  props: {
+    // TODO
+  },
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
+  }
+}
+</script>
