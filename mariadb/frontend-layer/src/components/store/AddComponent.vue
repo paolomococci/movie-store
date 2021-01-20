1eef8cb06fe44cc4d0d4aa5f002956eee62f69d8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-20 09:32:26 +0100

    Update AddComponent.vue

--- mariadb/frontend-layer/src/components/store/AddComponent.vue
+++ mariadb/frontend-layer/src/components/store/AddComponent.vue
@@ -6,9 +6,21 @@
 
 <script>
 export default {
-  name: 'StoreAddComponent',
+  name: 'AddComponent',
   props: {
     msg: String
+  },
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
   }
 }
 </script>
