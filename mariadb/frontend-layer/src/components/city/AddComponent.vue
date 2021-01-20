b5f8715014690c8aad8a1c3594657ad2ea38765b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-20 08:57:08 +0100

    Update AddComponent.vue

--- mariadb/frontend-layer/src/components/city/AddComponent.vue
+++ mariadb/frontend-layer/src/components/city/AddComponent.vue
@@ -6,9 +6,21 @@
 
 <script>
 export default {
-  name: 'CityAddComponent',
+  name: 'AddComponent',
   props: {
     msg: String
+  },
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
   }
 }
 </script>
