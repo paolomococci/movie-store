5f5874c23ac8f54004d9901115877386d0a8fd0f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-20 09:22:13 +0100

    Update EditorComponent.vue

--- mariadb/frontend-layer/src/components/movie/EditorComponent.vue
+++ mariadb/frontend-layer/src/components/movie/EditorComponent.vue
@@ -6,9 +6,21 @@
 
 <script>
 export default {
-  name: 'MovieEditorComponent',
+  name: 'EditorComponent',
   props: {
     uri: String
+  },
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
   }
 }
 </script>
