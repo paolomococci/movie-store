3c255dcf8ca65c0efabe29dcb8beb4d3af1b9502
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-20 08:52:24 +0100

    Update EditorComponent.vue

--- mariadb/frontend-layer/src/components/address/EditorComponent.vue
+++ mariadb/frontend-layer/src/components/address/EditorComponent.vue
@@ -6,9 +6,21 @@
 
 <script>
 export default {
-  name: 'AddressEditorComponent',
+  name: 'EditorComponent',
   props: {
     uri: String
+  },
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
   }
 }
 </script>
