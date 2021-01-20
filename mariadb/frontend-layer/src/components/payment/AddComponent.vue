1dbfb1205c00bf9ce8871233369c7c900cf60fed
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-20 09:22:52 +0100

    Update AddComponent.vue

--- mariadb/frontend-layer/src/components/payment/AddComponent.vue
+++ mariadb/frontend-layer/src/components/payment/AddComponent.vue
@@ -6,9 +6,21 @@
 
 <script>
 export default {
-  name: 'PaymentAddComponent',
+  name: 'AddComponent',
   props: {
     msg: String
+  },
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
   }
 }
 </script>
