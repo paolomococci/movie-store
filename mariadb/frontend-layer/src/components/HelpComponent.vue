cceb5c4e5d894faccc7540c935beb6d4587e41ff
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-20 08:48:49 +0100

    Create HelpComponent.vue

---
+++ mariadb/frontend-layer/src/components/HelpComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <!-- TODO-->
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'HelpComponent',
+  components: {
+    // TODO
+  },
+  props: {
+    // TODO
+  },
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
+  }
+}
+</script>
