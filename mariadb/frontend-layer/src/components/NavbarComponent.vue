fd27de0074cfcf6a06c901c92635726ce609db1a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-20 08:49:28 +0100

    Create NavbarComponent.vue

---
+++ mariadb/frontend-layer/src/components/NavbarComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <!-- TODO-->
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'NavbarComponent',
+  components: {
+    // TODO
+  },
+  props: {
+    // TODO
+  },
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
+  }
+}
+</script>
