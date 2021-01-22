07f860b8ea04f10cd2b53a379f40b03446f5f447
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-22 09:58:23 +0100

    Update CityRestfulApiService.js

--- mariadb/frontend-layer/src/services/CityRestfulApiService.js
+++ mariadb/frontend-layer/src/services/CityRestfulApiService.js
@@ -2,7 +2,30 @@ import axios from 'axios'
 import restful from '../../restful-config'
 
 class CityRestfulApiService {
-  // TODO
+
+    create(data) {
+        // TODO
+    }
+
+    read(uri) {
+        // TODO
+    }
+
+    readAll() {
+        // TODO
+    }
+
+    update(uri, data) {
+        // TODO
+    }
+
+    partialUpdate(uri, data) {
+        // TODO
+    }
+
+    delete(uri) {
+        // TODO
+    }
 }
 
 export default new CityRestfulApiService();
