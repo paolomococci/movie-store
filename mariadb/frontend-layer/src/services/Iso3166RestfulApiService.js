55d3242f9939b20b5fa8c236528235e530801227
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-22 09:50:11 +0100

    Update and rename Iso3166RestfulService.js to Iso3166RestfulApiService.js

---
+++ mariadb/frontend-layer/src/services/Iso3166RestfulApiService.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import restful from '../../restful-config'
+
+class Iso3166RestfulApiService {
+  // TODO
+}
+
+export default new Iso3166RestfulApiService();
--- mariadb/frontend-layer/src/services/Iso3166RestfulService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-import restful from '../../restful-config'
-
-class Iso3166RestfulService {
-}
-
-export default new Iso3166RestfulService();
