0d5e8fd57c422113c76123827b3af890be9bbaa1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-22 07:14:25 +0100

    Update AboutComponent.vue

--- mariadb/frontend-layer/src/components/AboutComponent.vue
+++ mariadb/frontend-layer/src/components/AboutComponent.vue
@@ -1,6 +1,10 @@
 <template>
   <section>
-    <!-- TODO-->
+    <b-jumbotron>
+      <template #header>web application for manage the data of some entities</template>
+      <template #lead><span v-text="msg"></span></template>
+      <hr class="my-4">
+    </b-jumbotron>
   </section>
 </template>
 
