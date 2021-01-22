71a446b6a8382965259f5352c8ccb967b6c6ebb9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-22 09:47:57 +0100

    Update and rename CountryRestfulService.js to CountryRestfulApiService.js

---
+++ mariadb/frontend-layer/src/services/CountryRestfulApiService.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import restful from '../../restful-config'
+
+class CountryRestfulApiService {
+  // TODO
+}
+
+export default new CountryRestfulApiService();
--- mariadb/frontend-layer/src/services/CountryRestfulService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-import restful from '../../restful-config'
-
-class CountryRestfulService {
-}
-
-export default new CountryRestfulService();
