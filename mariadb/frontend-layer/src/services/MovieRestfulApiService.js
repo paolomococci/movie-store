0bf2f9ab310a126ee250169e5a2046fb50806cd5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-22 09:51:10 +0100

    Update and rename MovieRestfulService.js to MovieRestfulApiService.js

---
+++ mariadb/frontend-layer/src/services/MovieRestfulApiService.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import restful from '../../restful-config'
+
+class MovieRestfulApiService {
+  // TODO
+}
+
+export default new MovieRestfulApiService();
--- mariadb/frontend-layer/src/services/MovieRestfulService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-import restful from '../../restful-config'
-
-class MovieRestfulService {
-}
-
-export default new MovieRestfulService();
