c39d26ee7c061da0eff61ba09dd2fb45d2bee0f4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-22 07:13:55 +0100

    Update HelpComponent.vue

--- mariadb/frontend-layer/src/components/HelpComponent.vue
+++ mariadb/frontend-layer/src/components/HelpComponent.vue
@@ -1,30 +1,33 @@
 <template>
-  <section>
-    <!-- TODO-->
-  </section>
+  <aside>
+    <b-button v-b-toggle.header-sidebar>help</b-button>
+    <b-sidebar id="header-sidebar" title="help" shadow>
+      <div class="px-3 py-2">
+        <b-card header="Edit"
+                title="how to modify an item">
+          <b-card-text>
+            To change an item just click on it, and select the values you want to change.
+          </b-card-text>
+        </b-card>
+        <b-card header="URI"
+                title="unique link of each element">
+          <b-card-text>
+            The URI is what actually identifies each item registered in the system.
+          </b-card-text>
+        </b-card>
+      </div>
+    </b-sidebar>
+  </aside>
 </template>
 
 <script>
-
 export default {
-  name: 'HelpComponent',
-  components: {
-    // TODO
-  },
-  props: {
-    // TODO
-  },
-  data: () => ({
-    // TODO
-  }),
-  methods: {
-    // TODO
-  },
-  computed: {
-    // TODO
-  },
-  mounted() {
-    // TODO
-  }
+  name: 'HelpComponent'
 }
 </script>
+
+<style>
+  aside {
+    margin: 1rem;
+  }
+</style>
