da1ccf77aff80d6dedd6fad8f64f317ae4fd74d1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-22 09:45:58 +0100

    Update and rename AddressRestfulService.js to AddressRestfulApiService.js

---
+++ mariadb/frontend-layer/src/services/AddressRestfulApiService.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import restful from '../../restful-config'
+
+class AddressRestfulApiService {
+  // TODO
+}
+
+export default new AddressRestfulApiService();
--- mariadb/frontend-layer/src/services/AddressRestfulService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-import restful from '../../restful-config'
-
-class AddressRestfulService {
-}
-
-export default new AddressRestfulService();
