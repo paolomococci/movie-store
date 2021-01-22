5340d817efd922a356d673ce5584711d536b383c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-22 07:13:22 +0100

    Update NavbarComponent.vue

--- mariadb/frontend-layer/src/components/NavbarComponent.vue
+++ mariadb/frontend-layer/src/components/NavbarComponent.vue
@@ -1,30 +1,32 @@
 <template>
   <section>
-    <!-- TODO-->
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">
+        <b-avatar size="6em" rounded>movie<br>store</b-avatar>
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">home</b-nav-item>
+          <b-nav-item to="/about">about</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <template #button-content>
+              <em>manage data</em>
+            </template>
+            <b-dropdown-item to="/address">company</b-dropdown-item>
+            <b-dropdown-item to="/category">contest</b-dropdown-item>
+            <b-dropdown-item to="/city">employee</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </section>
 </template>
 
 <script>
-
 export default {
-  name: 'NavbarComponent',
-  components: {
-    // TODO
-  },
-  props: {
-    // TODO
-  },
-  data: () => ({
-    // TODO
-  }),
-  methods: {
-    // TODO
-  },
-  computed: {
-    // TODO
-  },
-  mounted() {
-    // TODO
-  }
+  name: 'NavbarComponent'
 }
 </script>
