64ff32cb63d248164d229db11c9fc937146aa745
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-22 09:47:24 +0100

    Update and rename ContentRestfulService.js to ContentRestfulApiService.js

---
+++ mariadb/frontend-layer/src/services/ContentRestfulApiService.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import restful from '../../restful-config'
+
+class ContentRestfulApiService {
+  // TODO
+}
+
+export default new ContentRestfulApiService();
--- mariadb/frontend-layer/src/services/ContentRestfulService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-import restful from '../../restful-config'
-
-class ContentRestfulService {
-}
-
-export default new ContentRestfulService();
