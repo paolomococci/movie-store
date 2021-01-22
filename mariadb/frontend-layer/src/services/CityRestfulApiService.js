1f888eec13cca782928e3e916ea3457570f4423b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-22 09:46:57 +0100

    Update and rename CityRestfulService.js to CityRestfulApiService.js

---
+++ mariadb/frontend-layer/src/services/CityRestfulApiService.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import restful from '../../restful-config'
+
+class CityRestfulApiService {
+  // TODO
+}
+
+export default new CityRestfulApiService();
--- mariadb/frontend-layer/src/services/CityRestfulService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-import restful from '../../restful-config'
-
-class CityRestfulService {
-}
-
-export default new CityRestfulService();
