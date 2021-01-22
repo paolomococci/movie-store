1cd2d0bf0148aed767458a77d2d30de239436bb3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-22 09:53:22 +0100

    Update and rename StaffRestfulService.js to StaffRestfulApiService.js

---
+++ mariadb/frontend-layer/src/services/StaffRestfulApiService.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import restful from '../../restful-config'
+
+class StaffRestfulApiService {
+  // TODO
+}
+
+export default new StaffRestfulApiService();
--- mariadb/frontend-layer/src/services/StaffRestfulService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-import restful from '../../restful-config'
-
-class StaffRestfulService {
-}
-
-export default new StaffRestfulService();
