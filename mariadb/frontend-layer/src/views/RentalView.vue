611d51d5d9e3854720d15259870e3e0f130d90e3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-23 23:01:46 +0100

    Update RentalView.vue

--- mariadb/frontend-layer/src/views/RentalView.vue
+++ mariadb/frontend-layer/src/views/RentalView.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
+  <main>
     <welcome msg="rental view"/>
-  </div>
+    <rental-view-component/>
+  </main>
 </template>
 
 <script>
 import WelcomeComponent from '@/components/WelcomeComponent.vue'
+import ViewComponent from '@/components/rental/ViewComponent.vue'
 
 export default {
   name: 'RentalView',
   components: {
-    welcome: WelcomeComponent
+    welcome: WelcomeComponent,
+    'rental-view-component': ViewComponent
   }
 }
 </script>
