9775a181afa821e742a99ef77290dcaf7887f9a8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-23 22:56:46 +0100

    Update CityView.vue

--- mariadb/frontend-layer/src/views/CityView.vue
+++ mariadb/frontend-layer/src/views/CityView.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
+  <main>
     <welcome msg="city view"/>
-  </div>
+    <city-view-component/>
+  </main>
 </template>
 
 <script>
 import WelcomeComponent from '@/components/WelcomeComponent.vue'
+import ViewComponent from '@/components/city/ViewComponent.vue'
 
 export default {
   name: 'CityView',
   components: {
-    welcome: WelcomeComponent
+    welcome: WelcomeComponent,
+    'city-view-component': ViewComponent
   }
 }
 </script>
