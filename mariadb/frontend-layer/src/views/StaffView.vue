63a193ac06f356a40ff9380159c542cc7fe52866
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-23 23:02:11 +0100

    Update StaffView.vue

--- mariadb/frontend-layer/src/views/StaffView.vue
+++ mariadb/frontend-layer/src/views/StaffView.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
+  <main>
     <welcome msg="staff view"/>
-  </div>
+    <staff-view-component/>
+  </main>
 </template>
 
 <script>
 import WelcomeComponent from '@/components/WelcomeComponent.vue'
+import ViewComponent from '@/components/staff/ViewComponent.vue'
 
 export default {
   name: 'StaffView',
   components: {
-    welcome: WelcomeComponent
+    welcome: WelcomeComponent,
+    'staff-view-component': ViewComponent
   }
 }
 </script>
