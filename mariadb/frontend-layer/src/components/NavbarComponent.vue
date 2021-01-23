a219c5f53fd532d0c1ee59c4201fec254e0f1c39
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-23 22:03:44 +0100

    Update NavbarComponent.vue

--- mariadb/frontend-layer/src/components/NavbarComponent.vue
+++ mariadb/frontend-layer/src/components/NavbarComponent.vue
@@ -1,13 +1,12 @@
 <template>
   <section>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">
+      <b-navbar-brand to="/">
         <b-avatar size="6em" rounded>movie<br>store</b-avatar>
       </b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item to="/">home</b-nav-item>
           <b-nav-item to="/about">about</b-nav-item>
         </b-navbar-nav>
         <b-navbar-nav class="ml-auto">
@@ -15,9 +14,9 @@
             <template #button-content>
               <em>manage data</em>
             </template>
-            <b-dropdown-item to="/address">company</b-dropdown-item>
-            <b-dropdown-item to="/category">contest</b-dropdown-item>
-            <b-dropdown-item to="/city">employee</b-dropdown-item>
+            <b-dropdown-item to="/address">address</b-dropdown-item>
+            <b-dropdown-item to="/category">category</b-dropdown-item>
+            <b-dropdown-item to="/city">city</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
