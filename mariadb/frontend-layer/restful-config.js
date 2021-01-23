34a4a441af66300c6fe3501a3b6bff40c5b25f9b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-23 13:55:05 +0100

    Update restful-config.js

--- mariadb/frontend-layer/restful-config.js
+++ mariadb/frontend-layer/restful-config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://127.0.0.1:8080/',
+    baseURL: 'http://127.0.0.1:8090',
     headers: {
         "Content-Type": "application/hal+json"
     }
