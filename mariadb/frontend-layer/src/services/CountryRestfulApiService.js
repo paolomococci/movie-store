e6071c4985fcc67253774668f2e95ca67c85ec87
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-23 13:47:58 +0100

    Update CountryRestfulApiService.js

--- mariadb/frontend-layer/src/services/CountryRestfulApiService.js
+++ mariadb/frontend-layer/src/services/CountryRestfulApiService.js
@@ -4,27 +4,27 @@ import restful from '../../restful-config'
 class CountryRestfulApiService {
 
     create(data) {
-        // TODO
+        return restful.post('/countries', data);
     }
 
     read(uri) {
-        // TODO
+        return axios.get(uri);
     }
 
     readAll() {
-        // TODO
+        return restful.get('/countries');
     }
 
     update(uri, data) {
-        // TODO
+        return axios.put(uri, data);
     }
 
     partialUpdate(uri, data) {
-        // TODO
+        return axios.patch(uri, data);
     }
 
     delete(uri) {
-        // TODO
+        return axios.delete(uri);
     }
 }
 
