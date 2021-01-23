de31516dc4c8c2ac8759769e43cdaef0a175f0d7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-23 23:02:48 +0100

    Update StoreView.vue

--- mariadb/frontend-layer/src/views/StoreView.vue
+++ mariadb/frontend-layer/src/views/StoreView.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
-    <welcome msg="store view"/>
-  </div>
+  <main>
+    <welcome msg="staff view"/>
+    <staff-view-component/>
+  </main>
 </template>
 
 <script>
 import WelcomeComponent from '@/components/WelcomeComponent.vue'
+import ViewComponent from '@/components/staff/ViewComponent.vue'
 
 export default {
-  name: 'StoreView',
+  name: 'StaffView',
   components: {
-    welcome: WelcomeComponent
+    welcome: WelcomeComponent,
+    'staff-view-component': ViewComponent
   }
 }
 </script>
