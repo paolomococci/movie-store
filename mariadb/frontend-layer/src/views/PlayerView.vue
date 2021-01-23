873f2ae71f05ae996631acbacbfa8463d9fe6610
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-23 23:01:23 +0100

    Update PlayerView.vue

--- mariadb/frontend-layer/src/views/PlayerView.vue
+++ mariadb/frontend-layer/src/views/PlayerView.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
+  <main>
     <welcome msg="player view"/>
-  </div>
+    <player-view-component/>
+  </main>
 </template>
 
 <script>
 import WelcomeComponent from '@/components/WelcomeComponent.vue'
+import ViewComponent from '@/components/player/ViewComponent.vue'
 
 export default {
   name: 'PlayerView',
   components: {
-    welcome: WelcomeComponent
+    welcome: WelcomeComponent,
+    'player-view-component': ViewComponent
   }
 }
 </script>
