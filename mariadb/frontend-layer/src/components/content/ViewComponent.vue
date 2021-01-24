3610f241308ab81bb55416aadd79f66ecf81d46b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-24 19:10:26 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/content/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/content/ViewComponent.vue
@@ -8,6 +8,8 @@
 </template>
 
 <script>
+import ContentRestfulApiService from '../../services/ContentRestfulApiService'
+
 export default {
   name: 'ViewComponent',
   props: {
@@ -17,6 +19,16 @@ export default {
     // TODO
   }),
   methods: {
+    retrieveContents() {
+      ContentRestfulApiService.readAll()
+        .then(response => {
+          this.contents = response.data._embedded.contents;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
     // TODO
   },
   computed: {
