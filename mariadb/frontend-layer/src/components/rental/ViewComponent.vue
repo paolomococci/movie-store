a59b115373dce79e92525c6ea206d34053165905
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-25 03:41:42 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/rental/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/rental/ViewComponent.vue
@@ -16,9 +16,24 @@ export default {
     msg: String
   },
   data: () => ({
+    rentals: []
     // TODO
   }),
   methods: {
+    async updateView() {
+      this.$bvToast.toast('data of rentals being updated', {
+        title: 'update view',
+        toaster: 'b-toaster-bottom-center',
+        variant: 'info',
+        solid: true,
+        appendToast: true,
+        autoHideDelay: 1000
+      });
+      await setTimeout(() => {
+          this.rentals = null;
+          this.retrieveRentals();
+        }, 1000);
+    },
     retrieveRentals() {
       RentalRestfulApiService.readAll()
         .then(response => {
