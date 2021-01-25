4f792177ac5c724069168a38bbfd80915760d3ed
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-25 04:02:48 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/content/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/content/ViewComponent.vue
@@ -43,6 +43,13 @@ export default {
         .catch(e => {
           console.log(e);
         });
+    },
+    deleteItem(uri) {
+      ContentRestfulApiService.delete(uri)
+        .catch(e => {
+          console.log(e);
+        });
+      this.updateView();
     }
     // TODO
   },
