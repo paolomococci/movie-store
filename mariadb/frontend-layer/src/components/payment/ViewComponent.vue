f3caa91b8f9bd32b58ac72e7035004128b77d270
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-25 03:42:47 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/payment/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/payment/ViewComponent.vue
@@ -16,9 +16,24 @@ export default {
     msg: String
   },
   data: () => ({
+    payments: []
     // TODO
   }),
   methods: {
+    async updateView() {
+      this.$bvToast.toast('data of payments being updated', {
+        title: 'update view',
+        toaster: 'b-toaster-bottom-center',
+        variant: 'info',
+        solid: true,
+        appendToast: true,
+        autoHideDelay: 1000
+      });
+      await setTimeout(() => {
+          this.payments = null;
+          this.retrievePayments();
+        }, 1000);
+    },
     retrievePayments() {
       PaymentRestfulApiService.readAll()
         .then(response => {
