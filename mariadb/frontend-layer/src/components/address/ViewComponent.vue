bb8e9d5e9baa9d07091d15a22ae70ce6577669d5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-25 04:04:34 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/address/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/address/ViewComponent.vue
@@ -43,6 +43,13 @@ export default {
         .catch(e => {
           console.log(e);
         });
+    },
+    deleteItem(uri) {
+      AddressRestfulApiService.delete(uri)
+        .catch(e => {
+          console.log(e);
+        });
+      this.updateView();
     }
     // TODO
   },
