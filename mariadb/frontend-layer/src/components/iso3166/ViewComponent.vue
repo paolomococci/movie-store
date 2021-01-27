7a50d05d2f271c6ba904230a5bcb0e5261364e39
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-27 19:43:43 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/iso3166/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/iso3166/ViewComponent.vue
@@ -19,6 +19,11 @@ export default {
     iso3166s: [],
     fields: [
       {
+        key: 'name',
+        label: 'names',
+        sortable: true
+      },
+      {
         key: 'showDetails',
         label: 'details'
       }
