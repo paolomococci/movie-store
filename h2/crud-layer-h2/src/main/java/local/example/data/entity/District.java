f0af249bd8665428b8de6b05b197fa5f2225096e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-27 20:47:52 +0100

    Update District.java

--- h2/crud-layer-h2/src/main/java/local/example/data/entity/District.java
+++ h2/crud-layer-h2/src/main/java/local/example/data/entity/District.java
@@ -19,20 +19,15 @@
 package local.example.data.entity;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -66,7 +61,10 @@ public class District {
 	private String code;
 
 	@Getter
-	private Timestamp updated;
+	@Setter
+	@Column(name = "UPDATED")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate updated;
 	
 	@Getter
 	@Setter
