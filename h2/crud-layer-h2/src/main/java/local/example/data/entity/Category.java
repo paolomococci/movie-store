cb0b6793508b09b87845d7db105a81a4e61a70ca
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-27 20:46:07 +0100

    Update Category.java

--- h2/crud-layer-h2/src/main/java/local/example/data/entity/Category.java
+++ h2/crud-layer-h2/src/main/java/local/example/data/entity/Category.java
@@ -19,17 +19,15 @@
 package local.example.data.entity;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -61,7 +59,10 @@ public class Category {
 	private String description;
 
 	@Getter
-	private Timestamp updated;
+	@Setter
+	@Column(name = "UPDATED")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate updated;
 	
 	@Getter
 	@Setter
