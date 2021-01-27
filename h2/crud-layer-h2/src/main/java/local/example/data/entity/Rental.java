61a10851d428d214067a806cf82ef69e906f3194
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-27 20:52:32 +0100

    Update Rental.java

--- h2/crud-layer-h2/src/main/java/local/example/data/entity/Rental.java
+++ h2/crud-layer-h2/src/main/java/local/example/data/entity/Rental.java
@@ -20,15 +20,10 @@ package local.example.data.entity;
 
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
@@ -61,7 +56,10 @@ public class Rental {
 	private Date back;
 
 	@Getter
-	private Timestamp updated;
+	@Setter
+	@Column(name = "UPDATED")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate updated;
 	
 	@Getter
 	@Setter
