fbfc4dd7b91331abd6c2d7fa0c931db93b236559
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-27 19:24:41 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/country/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/country/ViewComponent.vue
@@ -17,7 +17,12 @@ export default {
   },
   data: () => ({
     countries: [],
-    fields: []
+    fields: [
+      {
+        key: 'showDetails',
+        label: 'details'
+      }
+    ]
   }),
   methods: {
     async updateView() {
