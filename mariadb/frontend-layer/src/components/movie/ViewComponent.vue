baeea949b8daee92b8503f8459840f5b79bba5bc
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-27 19:26:44 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/movie/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/movie/ViewComponent.vue
@@ -17,7 +17,12 @@ export default {
   },
   data: () => ({
     movies: [],
-    fields: []
+    fields: [
+      {
+        key: 'showDetails',
+        label: 'details'
+      }
+    ]
   }),
   methods: {
     async updateView() {
