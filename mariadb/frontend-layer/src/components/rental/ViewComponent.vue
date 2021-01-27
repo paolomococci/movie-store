85dac3373866aa01d86483bf178a5b3192781ba6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-27 19:27:46 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/rental/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/rental/ViewComponent.vue
@@ -17,7 +17,12 @@ export default {
   },
   data: () => ({
     rentals: [],
-    fields: []
+    fields: [
+      {
+        key: 'showDetails',
+        label: 'details'
+      }
+    ]
   }),
   methods: {
     async updateView() {
