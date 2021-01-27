f2901b896677b85cfd1936065b6f56bddc543d5c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-27 19:25:43 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/inventory/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/inventory/ViewComponent.vue
@@ -17,7 +17,12 @@ export default {
   },
   data: () => ({
     inventories: [],
-    fields: []
+    fields: [
+      {
+        key: 'showDetails',
+        label: 'details'
+      }
+    ]
   }),
   methods: {
     async updateView() {
