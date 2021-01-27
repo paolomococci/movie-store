fdb648fbf7fe405c6cec7ea1edf69ceeb64ea3ee
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-27 20:53:43 +0100

    Update Store.java

--- h2/crud-layer-h2/src/main/java/local/example/data/entity/Store.java
+++ h2/crud-layer-h2/src/main/java/local/example/data/entity/Store.java
@@ -19,19 +19,15 @@
 package local.example.data.entity;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -58,7 +54,10 @@ public class Store {
 	private String name;
 
 	@Getter
-	private Timestamp updated;
+	@Setter
+	@Column(name = "UPDATED")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate updated;
 	
 	@Getter
 	@Setter
