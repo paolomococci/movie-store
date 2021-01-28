4c2ede714ebd2cb700c64d620818c9fc1d766a24
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-28 18:16:27 +0100

    Update Language.java

--- h2/crud-layer-h2/src/main/java/local/example/data/entity/Language.java
+++ h2/crud-layer-h2/src/main/java/local/example/data/entity/Language.java
@@ -23,7 +23,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
@@ -43,12 +42,12 @@ public class Language {
 
 	@Getter
 	@Setter
-	@NotNull
 	@Size(min = 1, max = 30, message = "size range of this field is 1 to 30 characters")
 	@Pattern(
 			regexp = "^[a-zA-Z_-]*$", 
 			message = "only the characters `a-z`, `A-Z`, `_` and `-` are allowed"
 			)
+	@Column(name = "NAME", unique = true, nullable = false, columnDefinition = "VARCHAR(30)")
 	private String name;
 
 	@Getter
