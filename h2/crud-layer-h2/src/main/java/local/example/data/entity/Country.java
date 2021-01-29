4f2997a433522d321c9ee7e5b0a091cd35d4a364
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-29 23:53:22 +0100

    Update Country.java

--- h2/crud-layer-h2/src/main/java/local/example/data/entity/Country.java
+++ h2/crud-layer-h2/src/main/java/local/example/data/entity/Country.java
@@ -23,7 +23,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
@@ -40,12 +39,12 @@ public class Country {
 
 	@Getter
 	@Setter
-	@NotNull
 	@Size(min = 1, max = 100, message = "size range of this field is 1 to 100 characters")
 	@Pattern(
 			regexp = "^[a-zA-Z_-]*$", 
 			message = "only the characters `a-z`, `A-Z`, `_` and `-` are allowed"
 			)
+	@Column(name = "NAME", nullable = false, columnDefinition = "VARCHAR(100)")
 	private String name;
 
 	@Getter
