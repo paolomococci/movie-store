46da15936bc2d776a4c591d0f5d3e239754ea6d8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-31 20:15:44 +0100

    Update TableComponent.vue

--- mariadb/frontend-layer/src/components/language/TableComponent.vue
+++ mariadb/frontend-layer/src/components/language/TableComponent.vue
@@ -14,7 +14,7 @@
           </b-row>
           <b-row>
             <b-button-group>
-              <!-- edit language component -->
+              <!-- language editor component -->
               <!-- view references component -->
               <b-dropdown>
                 <b-dropdown-item></b-dropdown-item>
