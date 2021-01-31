82026280f1362dbaf226ad38321c4a11f67ba2c6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-31 20:15:16 +0100

    Update TableComponent.vue

--- mariadb/frontend-layer/src/components/iso3166/TableComponent.vue
+++ mariadb/frontend-layer/src/components/iso3166/TableComponent.vue
@@ -14,7 +14,7 @@
           </b-row>
           <b-row>
             <b-button-group>
-              <!-- edit iso3166 component -->
+              <!-- iso3166 editor component -->
               <!-- view references component -->
               <b-dropdown>
                 <b-dropdown-item></b-dropdown-item>
