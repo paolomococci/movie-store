8bb2657de05de37a83ce0d8617c036404e4c627f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-31 20:16:17 +0100

    Update TableComponent.vue

--- mariadb/frontend-layer/src/components/movie/TableComponent.vue
+++ mariadb/frontend-layer/src/components/movie/TableComponent.vue
@@ -14,7 +14,7 @@
           </b-row>
           <b-row>
             <b-button-group>
-              <!-- edit movie component -->
+              <!-- movie editor component -->
               <!-- view references component -->
               <b-dropdown>
                 <b-dropdown-item></b-dropdown-item>
