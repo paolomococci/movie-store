f8b95d2b9cb88d32b0f9ed09ea02f6e21c6ab6b9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-31 20:17:10 +0100

    Update TableComponent.vue

--- mariadb/frontend-layer/src/components/player/TableComponent.vue
+++ mariadb/frontend-layer/src/components/player/TableComponent.vue
@@ -14,7 +14,7 @@
           </b-row>
           <b-row>
             <b-button-group>
-              <!-- edit player component -->
+              <!-- player editor component -->
               <!-- view references component -->
               <b-dropdown>
                 <b-dropdown-item></b-dropdown-item>
