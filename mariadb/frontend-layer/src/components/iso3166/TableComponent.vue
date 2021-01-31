dd62283cacba9710488380a8624a0f82b3b11a3c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-31 08:41:38 +0100

    Create TableComponent.vue

---
+++ mariadb/frontend-layer/src/components/iso3166/TableComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <b-table>
+      <template>
+        <b-form-checkbox>
+          country code details
+        </b-form-checkbox>
+      </template>
+      <template>
+        <b-card>
+          <b-row>
+            <b-col></b-col>
+            <b-col></b-col>
+          </b-row>
+          <b-row>
+            <b-button-group>
+              <!-- edit iso3166 component -->
+              <!-- view references component -->
+              <b-dropdown>
+                <b-dropdown-item></b-dropdown-item>
+                <b-dropdown-divider/>
+                <b-dropdown-item></b-dropdown-item>
+              </b-dropdown>
+            </b-button-group>
+          </b-row>
+        </b-card>
+      </template>
+    </b-table>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'TableComponent',
+  props: {
+    // TODO
+  }
+}
+</script>
