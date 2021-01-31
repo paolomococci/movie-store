7792cb86c2496f37899fa64d6449c85b24a38fe5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-31 20:13:51 +0100

    Update TableComponent.vue

--- mariadb/frontend-layer/src/components/customer/TableComponent.vue
+++ mariadb/frontend-layer/src/components/customer/TableComponent.vue
@@ -14,7 +14,7 @@
           </b-row>
           <b-row>
             <b-button-group>
-              <!-- edit customer component -->
+              <!-- customer editor component -->
               <!-- view references component -->
               <b-dropdown>
                 <b-dropdown-item></b-dropdown-item>
