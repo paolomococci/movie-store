2dc2b7fded4533e9d5c1a34cf1030ef84544caff
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-31 20:13:27 +0100

    Update TableComponent.vue

--- mariadb/frontend-layer/src/components/city/TableComponent.vue
+++ mariadb/frontend-layer/src/components/city/TableComponent.vue
@@ -14,7 +14,7 @@
           </b-row>
           <b-row>
             <b-button-group>
-              <!-- edit city component -->
+              <!-- city editor component -->
               <!-- view references component -->
               <b-dropdown>
                 <b-dropdown-item></b-dropdown-item>
