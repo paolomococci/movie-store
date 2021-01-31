3fa8f5fea235024019217e23c97c0effd100b30e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-31 08:48:18 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/category/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/category/ViewComponent.vue
@@ -4,14 +4,19 @@
       <!-- todo aside to show add button -->
     </aside>
     <!-- todo sub-section to show the table list -->
+    <category-table-component/>
   </section>
 </template>
 
 <script>
 import CategoryRestfulApiService from '../../services/CategoryRestfulApiService'
+import TableComponent from './TableComponent'
 
 export default {
   name: 'ViewComponent',
+  components: {
+    'category-table-component': TableComponent
+  },
   props: {
     msg: String
   },
