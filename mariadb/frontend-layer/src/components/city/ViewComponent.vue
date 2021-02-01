c7c66548cd029bed2c5f8f97f4e2835b51abdba8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-01 08:01:25 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/city/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/city/ViewComponent.vue
@@ -9,7 +9,6 @@
 </template>
 
 <script>
-import CityRestfulApiService from '../../services/CityRestfulApiService'
 import TableComponent from './TableComponent'
 
 export default {
@@ -19,77 +18,6 @@ export default {
   },
   props: {
     msg: String
-  },
-  data: () => ({
-    cities: [],
-    fields: [
-      {
-        key: 'name',
-        label: 'names',
-        sortable: true
-      },
-      {
-        key: 'showDetails',
-        label: 'details'
-      }
-    ]
-  }),
-  methods: {
-    async updateView() {
-      this.$bvToast.toast('data of cities being updated', {
-        title: 'update view',
-        toaster: 'b-toaster-bottom-center',
-        variant: 'info',
-        solid: true,
-        appendToast: true,
-        autoHideDelay: 1000
-      });
-      await setTimeout(() => {
-          this.cities = null;
-          this.retrieveCities();
-        }, 1000);
-    },
-    retrieveCities() {
-      CityRestfulApiService.readAll()
-        .then(response => {
-          this.cities = response.data._embedded.cities;
-          console.log(response.data);
-        })
-        .catch(e => {
-          console.log(e);
-        });
-    },
-    deleteItemConfirm(uri) {
-      this.messageBoxToConfirmDeletion = '';
-      this.$bvModal.msgBoxConfirm('are you sure you want to delete this item', {
-        title: 'please confirm',
-        size: 'md',
-        buttonSize: 'md',
-        okVariant: 'danger',
-        okTitle: 'yes',
-        cancelTitle: 'no',
-        footerClass: 'p-2',
-        hideHeaderClose: false,
-        centered: true
-      }).then(value => {
-        if(value) {
-          this.deleteItem(uri);
-        }
-      });
-    },
-    deleteItem(uri) {
-      CityRestfulApiService.delete(uri)
-        .catch(e => {
-          console.log(e);
-        });
-      this.updateView();
-    }
-  },
-  computed: {
-    // TODO
-  },
-  mounted() {
-    // TODO
   }
 }
 </script>
