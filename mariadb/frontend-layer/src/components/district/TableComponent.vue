a5a506cb4ffb3f22aef97f52edcb59e473ea0330
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-01 08:06:33 +0100

    Update TableComponent.vue

--- mariadb/frontend-layer/src/components/district/TableComponent.vue
+++ mariadb/frontend-layer/src/components/district/TableComponent.vue
@@ -30,10 +30,79 @@
 </template>
 
 <script>
+import DistrictRestfulApiService from '../../services/DistrictRestfulApiService'
 
 export default {
   name: 'TableComponent',
-  props: {
+  data: () => ({
+    districts: [],
+    fields: [
+      {
+        key: 'name',
+        label: 'names',
+        sortable: true
+      },
+      {
+        key: 'showDetails',
+        label: 'details'
+      }
+    ]
+  }),
+  methods: {
+    async updateView() {
+      this.$bvToast.toast('data of districts being updated', {
+        title: 'update view',
+        toaster: 'b-toaster-bottom-center',
+        variant: 'info',
+        solid: true,
+        appendToast: true,
+        autoHideDelay: 1000
+      });
+      await setTimeout(() => {
+          this.districts = null;
+          this.retrieveDistricts();
+        }, 1000);
+    },
+    retrieveDistricts() {
+      DistrictRestfulApiService.readAll()
+        .then(response => {
+          this.districts = response.data._embedded.districts;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    deleteItemConfirm(uri) {
+      this.messageBoxToConfirmDeletion = '';
+      this.$bvModal.msgBoxConfirm('are you sure you want to delete this item', {
+        title: 'please confirm',
+        size: 'md',
+        buttonSize: 'md',
+        okVariant: 'danger',
+        okTitle: 'yes',
+        cancelTitle: 'no',
+        footerClass: 'p-2',
+        hideHeaderClose: false,
+        centered: true
+      }).then(value => {
+        if(value) {
+          this.deleteItem(uri);
+        }
+      });
+    },
+    deleteItem(uri) {
+      DistrictRestfulApiService.delete(uri)
+        .catch(e => {
+          console.log(e);
+        });
+      this.updateView();
+    }
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
     // TODO
   }
 }
