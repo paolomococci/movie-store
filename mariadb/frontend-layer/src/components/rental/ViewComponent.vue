eb58aeda4797e4cbae12a1ac362327e4fbd0fe1b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-01 08:14:36 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/rental/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/rental/ViewComponent.vue
@@ -9,7 +9,6 @@
 </template>
 
 <script>
-import RentalRestfulApiService from '../../services/RentalRestfulApiService'
 import TableComponent from './TableComponent'
 
 export default {
@@ -19,77 +18,6 @@ export default {
   },
   props: {
     msg: String
-  },
-  data: () => ({
-    rentals: [],
-    fields: [
-      {
-        key: 'rent',
-        label: 'rent',
-        sortable: true
-      },
-      {
-        key: 'showDetails',
-        label: 'details'
-      }
-    ]
-  }),
-  methods: {
-    async updateView() {
-      this.$bvToast.toast('data of rentals being updated', {
-        title: 'update view',
-        toaster: 'b-toaster-bottom-center',
-        variant: 'info',
-        solid: true,
-        appendToast: true,
-        autoHideDelay: 1000
-      });
-      await setTimeout(() => {
-          this.rentals = null;
-          this.retrieveRentals();
-        }, 1000);
-    },
-    retrieveRentals() {
-      RentalRestfulApiService.readAll()
-        .then(response => {
-          this.rentals = response.data._embedded.rentals;
-          console.log(response.data);
-        })
-        .catch(e => {
-          console.log(e);
-        });
-    },
-    deleteItemConfirm(uri) {
-      this.messageBoxToConfirmDeletion = '';
-      this.$bvModal.msgBoxConfirm('are you sure you want to delete this item', {
-        title: 'please confirm',
-        size: 'md',
-        buttonSize: 'md',
-        okVariant: 'danger',
-        okTitle: 'yes',
-        cancelTitle: 'no',
-        footerClass: 'p-2',
-        hideHeaderClose: false,
-        centered: true
-      }).then(value => {
-        if(value) {
-          this.deleteItem(uri);
-        }
-      });
-    },
-    deleteItem(uri) {
-      RentalRestfulApiService.delete(uri)
-        .catch(e => {
-          console.log(e);
-        });
-      this.updateView();
-    }
-  },
-  computed: {
-    // TODO
-  },
-  mounted() {
-    // TODO
   }
 }
 </script>
