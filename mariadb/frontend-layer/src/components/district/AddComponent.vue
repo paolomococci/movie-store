bd6495d5127cb7a49a676278cc2bdabd93d0a1ef
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-02 18:57:39 +0100

    Update AddComponent.vue

--- mariadb/frontend-layer/src/components/district/AddComponent.vue
+++ mariadb/frontend-layer/src/components/district/AddComponent.vue
@@ -1,6 +1,45 @@
 <template>
   <section>
-    <!-- todo -->
+    <b-button
+      variant="outline-primary"
+      @click="showModalDetail">add</b-button>
+    <b-button
+      variant="outline-secondary"
+      @click="updateView">refresh</b-button>
+    <b-modal
+      ref="modal-add"
+      hide-footer
+      title="enter the details of a new item">
+      <div class="d-block text-center">
+        <h3>fields</h3>
+        <b-form  @submit.stop.prevent="onSubmitForm">
+          <!-- todo fields -->
+          <b-form-group>
+            <b-form-input></b-form-input>
+            <b-form-invalid-feedback>
+              description of the error
+            </b-form-invalid-feedback>
+            <b-form-valid-feedback>all right</b-form-valid-feedback>
+          </b-form-group>
+          <!-- buttons -->
+          <b-button
+            class="mt-3"
+            variant="outline-secondary"
+            block
+            @click="hideModalDetail">cancel</b-button>
+          <b-button
+            class="mt-3"
+            variant="outline-warning"
+            block
+            @click="onResetForm()">reset</b-button>
+          <b-button
+            class="mt-3"
+            variant="outline-primary"
+            block
+            type="submit">save</b-button>
+        </b-form>
+      </div>
+    </b-modal>
   </section>
 </template>
 
