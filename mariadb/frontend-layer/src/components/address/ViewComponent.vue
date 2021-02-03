b042ca729a858e44fbc053794ad48f853b1306f7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-03 20:40:56 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/address/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/address/ViewComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <aside>
-      <!-- todo aside to show add button -->
+      <address-add-component/>
     </aside>
     <!-- todo sub-section to show the table list -->
     <address-table-component/>
@@ -9,11 +9,13 @@
 </template>
 
 <script>
+import AddComponent from './AddComponent'
 import TableComponent from './TableComponent'
 
 export default {
   name: 'ViewComponent',
   components: {
+    'address-add-component': AddComponent,
     'address-table-component': TableComponent
   },
   props: {
