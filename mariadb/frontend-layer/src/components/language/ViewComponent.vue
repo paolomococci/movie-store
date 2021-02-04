397541007508a430c6a865a25a699f02acd8aefb
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-04 17:02:21 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/language/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/language/ViewComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <aside>
-      <!-- todo aside to show add button -->
+      <language-add-component/>
     </aside>
     <!-- todo sub-section to show the table list -->
     <language-table-component/>
@@ -9,11 +9,13 @@
 </template>
 
 <script>
+import AddComponent from './AddComponent'
 import TableComponent from './TableComponent'
 
 export default {
   name: 'ViewComponent',
   components: {
+    'language-add-component': AddComponent,
     'language-table-component': TableComponent
   },
   props: {
