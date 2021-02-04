11683ef55c068bd3ee00c432485144019324a051
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-04 16:58:02 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/district/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/district/ViewComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <aside>
-      <!-- todo aside to show add button -->
+      <district-add-component/>
     </aside>
     <!-- todo sub-section to show the table list -->
     <district-table-component/>
@@ -9,11 +9,13 @@
 </template>
 
 <script>
+import AddComponent from './AddComponent'
 import TableComponent from './TableComponent'
 
 export default {
   name: 'ViewComponent',
   components: {
+    'district-add-component': AddComponent,
     'district-table-component': TableComponent
   },
   props: {
