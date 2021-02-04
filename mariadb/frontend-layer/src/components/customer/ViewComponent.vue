02e56094dbeef14794329e9a1451d1ee6896c8d4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-04 16:56:28 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/customer/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/customer/ViewComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <aside>
-      <!-- todo aside to show add button -->
+      <customer-add-component/>
     </aside>
     <!-- todo sub-section to show the table list -->
     <customer-table-component/>
@@ -9,11 +9,13 @@
 </template>
 
 <script>
+import AddComponent from './AddComponent'
 import TableComponent from './TableComponent'
 
 export default {
   name: 'ViewComponent',
   components: {
+    'customer-add-component': AddComponent,
     'customer-table-component': TableComponent
   },
   props: {
