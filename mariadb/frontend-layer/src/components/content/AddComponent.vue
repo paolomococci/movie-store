f29cf780a0fd114c58dc5ba5be466f5cfdcae512
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-07 21:04:33 +0100

    Update AddComponent.vue

--- mariadb/frontend-layer/src/components/content/AddComponent.vue
+++ mariadb/frontend-layer/src/components/content/AddComponent.vue
@@ -52,7 +52,8 @@ export default {
   mixins: [validationMixin],
   data: () => ({
     form: {
-      // TODO initialize the fields
+      contentSubject: '',
+      contentUpdated: ''
     }
   }),
   validations: {
@@ -66,7 +67,8 @@ export default {
     },
     onResetForm() {
       this.form = {
-        // TODO deletes any values assigned to the fields
+        contentSubject: '',
+        contentUpdated: ''
       };
       this.$nextTick(() => {
         this.$v.$reset();
