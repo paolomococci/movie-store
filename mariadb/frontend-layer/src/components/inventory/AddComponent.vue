a4eac789f39559f1a8b6fee95f30c553957a9b6d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-08 08:24:23 +0100

    Update AddComponent.vue

--- mariadb/frontend-layer/src/components/inventory/AddComponent.vue
+++ mariadb/frontend-layer/src/components/inventory/AddComponent.vue
@@ -44,7 +44,9 @@
 </template>
 
 <script>
+import moment from 'moment'
 import { validationMixin } from 'vuelidate'
+import { required, minLength, maxLength, helpers } from 'vuelidate/lib/validators'
 import InventoryRestfulApiService from '../../services/InventoryRestfulApiService'
 
 export default {
