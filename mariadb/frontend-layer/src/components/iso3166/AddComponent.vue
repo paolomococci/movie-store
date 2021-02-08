ba04ac2ebdece09e179009715892cab4c93c37ab
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-08 08:24:49 +0100

    Update AddComponent.vue

--- mariadb/frontend-layer/src/components/iso3166/AddComponent.vue
+++ mariadb/frontend-layer/src/components/iso3166/AddComponent.vue
@@ -44,7 +44,9 @@
 </template>
 
 <script>
+import moment from 'moment'
 import { validationMixin } from 'vuelidate'
+import { required, minLength, maxLength, helpers } from 'vuelidate/lib/validators'
 import Iso3166RestfulApiService from '../../services/Iso3166RestfulApiService'
 
 export default {
