b4629ad1e9ef6f45fb3876786fdc20639eae4566
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-08 08:27:04 +0100

    Update AddComponent.vue

--- mariadb/frontend-layer/src/components/rental/AddComponent.vue
+++ mariadb/frontend-layer/src/components/rental/AddComponent.vue
@@ -44,7 +44,9 @@
 </template>
 
 <script>
+import moment from 'moment'
 import { validationMixin } from 'vuelidate'
+import { required, minLength, maxLength, helpers } from 'vuelidate/lib/validators'
 import RentalRestfulApiService from '../../services/RentalRestfulApiService'
 
 export default {
