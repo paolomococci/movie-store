0bfb64cc56e4296458312b9d988bd3d5bcb17ae8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-08 19:58:24 +0100

    Update AddComponent.vue

--- mariadb/frontend-layer/src/components/category/AddComponent.vue
+++ mariadb/frontend-layer/src/components/category/AddComponent.vue
@@ -13,13 +13,20 @@
       <div class="d-block text-center">
         <h3>fields</h3>
         <b-form  @submit.stop.prevent="onSubmitForm">
-          <!-- todo fields -->
-          <b-form-group>
-            <b-form-input></b-form-input>
-            <b-form-invalid-feedback>
-              description of the error
+          <!-- name field -->
+          <b-form-group id="input-name-group" label="name" label-for="input-name">
+            <b-form-input
+              id="input-name"
+              name="input-name"
+              v-model="$v.form.categoryName.$model"
+              :state="onValidateCategoryNameField('categoryName')"
+              aria-describedby="input-name-feedback-invalid"></b-form-input>
+            <b-form-invalid-feedback id="input-name-feedback-invalid">
+              size range of this field is 3 to 50 characters, only the characters `a-z`, `A-Z`, `_` and `-` are allowed
             </b-form-invalid-feedback>
-            <b-form-valid-feedback>all right</b-form-valid-feedback>
+            <b-form-valid-feedback id="input-name-feedback-valid">
+              the category name has a correct format
+            </b-form-valid-feedback>
           </b-form-group>
           <!-- buttons -->
           <b-button
