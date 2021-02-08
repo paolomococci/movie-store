3cc3cf9e3f14aa4c949e2b332ed3b25594669b4f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-08 19:57:22 +0100

    Update AddComponent.vue

--- mariadb/frontend-layer/src/components/content/AddComponent.vue
+++ mariadb/frontend-layer/src/components/content/AddComponent.vue
@@ -13,13 +13,20 @@
       <div class="d-block text-center">
         <h3>fields</h3>
         <b-form  @submit.stop.prevent="onSubmitForm">
-          <!-- todo fields -->
-          <b-form-group>
-            <b-form-input></b-form-input>
-            <b-form-invalid-feedback>
-              description of the error
+          <!-- subject field -->
+          <b-form-group id="input-subject-group" label="subject" label-for="input-subject">
+            <b-form-input
+              id="input-subject"
+              name="input-subject"
+              v-model="$v.form.contentSubject.$model"
+              :state="onValidateContentSubjectField('contentSubject')"
+              aria-describedby="input-subject-feedback-invalid"></b-form-input>
+            <b-form-invalid-feedback id="input-subject-feedback-invalid">
+              max of this field is 255 characters, only the characters `a-z`, `A-Z`, `_`, `-` and space are allowed
             </b-form-invalid-feedback>
-            <b-form-valid-feedback>all right</b-form-valid-feedback>
+            <b-form-valid-feedback id="input-subject-feedback-valid">
+              the content subject has a correct format
+            </b-form-valid-feedback>
           </b-form-group>
           <!-- buttons -->
           <b-button
