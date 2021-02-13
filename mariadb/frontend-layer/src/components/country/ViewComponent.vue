e2ba6dd98dcda198d27dacafa7fca5c7a0d9a089
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-13 23:03:39 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/country/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/country/ViewComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <aside>
-      <country-add-component/>
+      <country-add-component @updateView="updateView"/>
     </aside>
     <!-- todo sub-section to show the table list -->
     <country-table-component/>
