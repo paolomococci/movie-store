41c419fee80f7ae642528cbcec033b786e7a97fb
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-13 22:55:52 +0100

    Update ViewComponent.vue

--- mariadb/frontend-layer/src/components/address/ViewComponent.vue
+++ mariadb/frontend-layer/src/components/address/ViewComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <aside>
-      <address-add-component/>
+      <address-add-component @updateView="updateView"/>
     </aside>
     <!-- todo sub-section to show the table list -->
     <address-table-component/>
