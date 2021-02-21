fa786f26dba60c2ca57e52f86a885882dbefa554
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-21 22:13:12 +0100

    Update urls.py

--- h2/django/movie_store_spa/movie_store_spa/urls.py
+++ h2/django/movie_store_spa/movie_store_spa/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
+#from django.urls import include, path
+#from django.conf.urls import url, include
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    #path(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls)
 ]
