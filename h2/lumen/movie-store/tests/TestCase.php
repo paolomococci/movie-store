5c08a74b45676fc2173cfed06447d314b1daec19
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-25 23:38:53 +0100

    Create TestCase.php

---
+++ h2/lumen/movie-store/tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+use Laravel\Lumen\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    public function createApplication()
+    {
+        return require __DIR__.'/../bootstrap/app.php';
+    }
+}
