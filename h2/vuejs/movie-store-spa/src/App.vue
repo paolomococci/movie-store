5d8d3c76a9ac093f734098c3d2ff481bfeb10ac1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-26 21:24:04 +0100

    Create App.vue

---
+++ h2/vuejs/movie-store-spa/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <header-component/>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '@/components/HeaderComponent'
+
+export default {
+  components: {
+    'header-component': HeaderComponent
+  }
+}
+</script>
