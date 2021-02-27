4d6f620c964ee74e9637a690e1b72d93a6f6c54f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-27 23:39:12 +0100

    Create PlayerView.vue

---
+++ h2/vuejs/movie-store-spa/src/views/PlayerView.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <welcome msg="player view"/>
+    <player-view-component/>
+  </main>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+import ViewComponent from '@/components/player/ViewComponent.vue'
+
+export default {
+  name: 'PlayerView',
+  components: {
+    welcome: WelcomeComponent,
+    'player-view-component': ViewComponent
+  }
+}
+</script>
