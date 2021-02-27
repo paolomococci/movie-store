86fbaba3e4ffa8c11a6283f52b255c0b9143942c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-27 23:26:53 +0100

    Create AddressView.vue

---
+++ h2/vuejs/movie-store-spa/src/views/AddressView.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <welcome msg="address view"/>
+    <address-view-component/>
+  </main>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+import ViewComponent from '@/components/address/ViewComponent.vue'
+
+export default {
+  name: 'AddressView',
+  components: {
+    welcome: WelcomeComponent,
+    'address-view-component': ViewComponent
+  }
+}
+</script>
