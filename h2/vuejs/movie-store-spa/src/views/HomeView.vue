2465e8fe0704dba4ee74c4730add0e75086cebba
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-27 23:36:14 +0100

    Create HomeView.vue

---
+++ h2/vuejs/movie-store-spa/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <home-component msg="you can view and edit this type of:"/>
+  </main>
+</template>
+
+<script>
+import HomeComponent from '@/components/HomeComponent.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    'home-component': HomeComponent
+  }
+}
+</script>
