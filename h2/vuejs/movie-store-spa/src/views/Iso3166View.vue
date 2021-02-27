fdf24e0296a59329a2eba78bc74f421c53565537
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-27 23:37:19 +0100

    Create Iso3166View.vue

---
+++ h2/vuejs/movie-store-spa/src/views/Iso3166View.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <welcome msg="iso3166 view"/>
+    <iso3166-view-component/>
+  </main>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+import ViewComponent from '@/components/iso3166/ViewComponent.vue'
+
+export default {
+  name: 'Iso3166View',
+  components: {
+    welcome: WelcomeComponent,
+    'iso3166-view-component': ViewComponent
+  }
+}
+</script>
