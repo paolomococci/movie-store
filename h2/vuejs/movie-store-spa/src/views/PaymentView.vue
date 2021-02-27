92f06e753407e9e0e7366c5c4bc916b49ca821d2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-27 23:38:45 +0100

    Create PaymentView.vue

---
+++ h2/vuejs/movie-store-spa/src/views/PaymentView.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <welcome msg="payment view"/>
+    <payment-view-component/>
+  </main>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+import ViewComponent from '@/components/payment/ViewComponent.vue'
+
+export default {
+  name: 'PaymentView',
+  components: {
+    welcome: WelcomeComponent,
+    'payment-view-component': ViewComponent
+  }
+}
+</script>
