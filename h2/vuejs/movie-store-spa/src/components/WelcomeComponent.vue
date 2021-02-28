d279759241a1eb6dabbeebfbe1a032970fce355f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-28 19:17:24 +0100

    Create WelcomeComponent.vue

---
+++ h2/vuejs/movie-store-spa/src/components/WelcomeComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <b-jumbotron>
+      <template #header><span v-text="msg"></span></template>
+      <hr class="my-4">
+    </b-jumbotron>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'WelcomeComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
