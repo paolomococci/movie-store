4b8b19cd63b2869d81ec00fd9eeb325e6c74ca9c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-28 19:14:11 +0100

    Create HeaderComponent.vue

---
+++ h2/vuejs/movie-store-spa/src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <nav>
+      <navbar-component/>
+    </nav>
+    <help/>
+  </header>
+</template>
+
+<script>
+import NavbarComponent from '@/components/NavbarComponent'
+import HelpComponent from '@/components/HelpComponent'
+
+export default {
+  name: 'HeaderComponent',
+  components: {
+    'navbar-component': NavbarComponent,
+    help: HelpComponent
+  }
+}
+</script>
