1fa2fae5c8188c552b3bdc2f7e300068c7a8bff5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-02-28 19:14:43 +0100

    Create HelpComponent.vue

---
+++ h2/vuejs/movie-store-spa/src/components/HelpComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <aside>
+    <b-button v-b-toggle.header-sidebar>help</b-button>
+    <b-sidebar id="header-sidebar" title="help" shadow>
+      <div class="px-3 py-2">
+        <b-card header="Edit"
+                title="how to modify an item">
+          <b-card-text>
+            To change an item just click on it, and select the values you want to change.
+          </b-card-text>
+        </b-card>
+        <b-card header="URI"
+                title="unique link of each element">
+          <b-card-text>
+            The URI is what actually identifies each item registered in the system.
+          </b-card-text>
+        </b-card>
+      </div>
+    </b-sidebar>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'HelpComponent'
+}
+</script>
+
+<style>
+  aside {
+    margin: 1rem;
+  }
+</style>
