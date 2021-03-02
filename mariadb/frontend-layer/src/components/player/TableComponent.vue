70025bac7a15102dcbc59a96c2d207729048e5aa
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-02 16:40:08 +0100

    Update TableComponent.vue

--- mariadb/frontend-layer/src/components/player/TableComponent.vue
+++ mariadb/frontend-layer/src/components/player/TableComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <b-table :items="players" :fields="fields" striped resposive="sm">
+    <b-table :items="players" :fields="fields" striped responsive="sm">
       <template #cell(showDetails)="row">
         <b-form-checkbox v-model="row.detailsShowing" @change="row.toggleDetails">
           player details
