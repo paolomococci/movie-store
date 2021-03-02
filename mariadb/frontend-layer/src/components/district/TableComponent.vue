49c642acee52cee933a5969fe2b46afa4772f806
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-02 16:37:58 +0100

    Update TableComponent.vue

--- mariadb/frontend-layer/src/components/district/TableComponent.vue
+++ mariadb/frontend-layer/src/components/district/TableComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <b-table :items="districts" :fields="fields" striped resposive="sm">
+    <b-table :items="districts" :fields="fields" striped responsive="sm">
       <template #cell(showDetails)="row">
         <b-form-checkbox v-model="row.detailsShowing" @change="row.toggleDetails">
           district details
