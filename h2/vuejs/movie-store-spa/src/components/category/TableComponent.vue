98b5640bf4a67872ce2f0fe05672f533d9f5dcf5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-02 16:02:29 +0100

    Create TableComponent.vue

---
+++ h2/vuejs/movie-store-spa/src/components/category/TableComponent.vue
@@ -0,0 +1,108 @@
+<template>
+  <section>
+    <b-table :items="categories" :fields="fields" striped resposive="sm">
+      <template #cell(showDetails)="row">
+        <b-form-checkbox v-model="row.detailsShowing" @change="row.toggleDetails">
+          category details
+        </b-form-checkbox>
+      </template>
+      <template #row-details="row">
+        <b-card>
+          <b-row class="mb-2">
+            <b-col sm="3" class="text-sm-right"><em>URI:</em></b-col>
+            <b-col><output v-text="row.item._links.self.href"></output></b-col>
+          </b-row>
+          <b-row>
+            <b-button-group>
+              <!-- category editor component -->
+              <!-- view references component -->
+              <b-dropdown>
+                <b-dropdown-item @click="row.toggleDetails">toggle</b-dropdown-item>
+                <b-dropdown-divider/>
+                <b-dropdown-item @click="deleteItemConfirm(row.item._links.self.href)">
+                  delete item
+                </b-dropdown-item>
+              </b-dropdown>
+            </b-button-group>
+          </b-row>
+        </b-card>
+      </template>
+    </b-table>
+  </section>
+</template>
+
+<script>
+import CategoryRestfulApiService from '../../services/CategoryRestfulApiService'
+
+export default {
+  name: 'TableComponent',
+  data: () => ({
+    categories: [],
+    fields: [
+      {
+        key: 'name',
+        label: 'names',
+        sortable: true
+      },
+      {
+        key: 'showDetails',
+        label: 'details'
+      }
+    ]
+  }),
+  methods: {
+    async updateView() {
+      this.$bvToast.toast('data of categories being updated', {
+        title: 'update view',
+        toaster: 'b-toaster-bottom-center',
+        variant: 'info',
+        solid: true,
+        appendToast: true,
+        autoHideDelay: 1000
+      });
+      await setTimeout(() => {
+          this.categories = null;
+          this.retrieveCategories();
+        }, 1000);
+    },
+    retrieveCategories() {
+      CategoryRestfulApiService.readAll()
+        .then(response => {
+          this.categories = response.data._embedded.categories;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    deleteItemConfirm(uri) {
+      this.messageBoxToConfirmDeletion = '';
+      this.$bvModal.msgBoxConfirm('are you sure you want to delete this item', {
+        title: 'please confirm',
+        size: 'md',
+        buttonSize: 'md',
+        okVariant: 'danger',
+        okTitle: 'yes',
+        cancelTitle: 'no',
+        footerClass: 'p-2',
+        hideHeaderClose: false,
+        centered: true
+      }).then(value => {
+        if(value) {
+          this.deleteItem(uri);
+        }
+      });
+    },
+    deleteItem(uri) {
+      CategoryRestfulApiService.delete(uri)
+        .catch(e => {
+          console.log(e);
+        });
+      this.updateView();
+    }
+  },
+  mounted() {
+    this.retrieveCategories();
+  }
+}
+</script>
