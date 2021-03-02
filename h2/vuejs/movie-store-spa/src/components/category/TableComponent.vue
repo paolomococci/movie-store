b06c4c85f2f59adec7ae8127d7862f1305b30eb6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-02 16:30:29 +0100

    Update TableComponent.vue

--- h2/vuejs/movie-store-spa/src/components/category/TableComponent.vue
+++ h2/vuejs/movie-store-spa/src/components/category/TableComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <b-table :items="categories" :fields="fields" striped resposive="sm">
+    <b-table :items="categories" :fields="fields" striped responsive="sm">
       <template #cell(showDetails)="row">
         <b-form-checkbox v-model="row.detailsShowing" @change="row.toggleDetails">
           category details
