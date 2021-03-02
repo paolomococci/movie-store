23aa490ad7739d69f47437e88a78b9d4e79b9961
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-02 17:04:17 +0100

    Create ViewComponent.vue

---
+++ h2/vuejs/movie-store-spa/src/components/district/ViewComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <aside>
+      <district-add-component @updateView="updateView"/>
+    </aside>
+    <!-- todo sub-section to show the table list -->
+    <district-table-component/>
+  </section>
+</template>
+
+<script>
+import AddComponent from './AddComponent'
+import TableComponent from './TableComponent'
+
+export default {
+  name: 'ViewComponent',
+  components: {
+    'district-add-component': AddComponent,
+    'district-table-component': TableComponent
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
