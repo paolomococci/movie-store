f85234df3f4da489ee8b9135262563c0f0a59946
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-03 17:57:18 +0100

    Create ViewComponent.vue

---
+++ h2/vuejs/movie-store-spa/src/components/inventory/ViewComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <aside>
+      <inventory-add-component @updateView="updateView"/>
+    </aside>
+    <!-- todo sub-section to show the table list -->
+    <inventory-table-component/>
+  </section>
+</template>
+
+<script>
+import AddComponent from './AddComponent'
+import TableComponent from './TableComponent'
+
+export default {
+  name: 'ViewComponent',
+  components: {
+    'inventory-add-component': AddComponent,
+    'inventory-table-component': TableComponent
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
