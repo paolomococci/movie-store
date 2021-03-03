9fdf75498c9cd5d114cea0ac9572cf70ee80f458
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-03 18:03:40 +0100

    Create ViewComponent.vue

---
+++ h2/vuejs/movie-store-spa/src/components/iso3155/ViewComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <aside>
+      <iso3166-add-component @updateView="updateView"/>
+    </aside>
+    <!-- todo sub-section to show the table list -->
+    <iso3166-table-component/>
+  </section>
+</template>
+
+<script>
+import AddComponent from './AddComponent'
+import TableComponent from './TableComponent'
+
+export default {
+  name: 'ViewComponent',
+  components: {
+    'iso3166-add-component': AddComponent,
+    'iso3166-table-component': TableComponent
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
