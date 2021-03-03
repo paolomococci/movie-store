fee42c22ca547658a7069a986af94f96ea9ae9e7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-03 17:59:47 +0100

    Create EditorComponent.vue

---
+++ h2/vuejs/movie-store-spa/src/components/iso3155/EditorComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <!-- TODO -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'EditorComponent',
+  props: {
+    uri: String
+  },
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
+  }
+}
+</script>
