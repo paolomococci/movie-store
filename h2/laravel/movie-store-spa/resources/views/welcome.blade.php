dc582ce51d0ce4a770a3d00f992511a52a92ed9a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-21 06:43:40 +0100

    Update welcome.blade.php

--- h2/laravel/movie-store-spa/resources/views/welcome.blade.php
+++ h2/laravel/movie-store-spa/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Movie Store</title>
+        <title>movie store</title>
 
         <style>
             html, body {
