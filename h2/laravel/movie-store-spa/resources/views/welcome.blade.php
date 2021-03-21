d37d1047d2901aae2f4785e2e220b789690bc2c1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-21 06:51:37 +0100

    Update welcome.blade.php

--- h2/laravel/movie-store-spa/resources/views/welcome.blade.php
+++ h2/laravel/movie-store-spa/resources/views/welcome.blade.php
@@ -5,60 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>movie store</title>
-
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #666;
-                font-family: sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #666;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-
     </head>
     <body>
         <div class="flex-center position-ref full-height">
