210c0058aa6c72cb825929b138c852450c0ab1ad
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-21 06:52:34 +0100

    Update welcome.blade.php

--- h2/laravel/movie-store-spa/resources/views/welcome.blade.php
+++ h2/laravel/movie-store-spa/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="{{ url('css/welcome.css')}}" type="text/css" media="screen"/>
         <title>movie store</title>
     </head>
     <body>
