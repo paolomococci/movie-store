2e1fc08d9b5b3cddbd3f4c9df59b3a4449304c22
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 00:49:21 +0200

    Update RentalController.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/controller/RentalController.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/controller/RentalController.java
@@ -18,9 +18,14 @@
 
 package local.moviestore.publisher.data.controller;
 
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheRepositoryResource;
 import io.quarkus.rest.data.panache.ResourceProperties;
 
+import local.moviestore.publisher.data.model.Rental;
+import local.moviestore.publisher.data.repository.RentalRepository;
+
 @ResourceProperties(path = "rental")
-public interface RentalController {
+public interface RentalController 
+		extends PanacheRepositoryResource<RentalRepository, Rental, Long> {
 
 }
