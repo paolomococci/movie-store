f9956a092085e42c4767c615f1c77d7bc9cbf806
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 00:14:19 +0200

    Update InventoryRepository.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/repository/InventoryRepository.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/repository/InventoryRepository.java
@@ -20,7 +20,11 @@ package local.moviestore.publisher.data.repository;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import local.moviestore.publisher.data.model.Inventory;
+
 @ApplicationScoped
-public class InventoryRepository {
+public class InventoryRepository 
+		implements PanacheRepository<Inventory> {
 
 }
