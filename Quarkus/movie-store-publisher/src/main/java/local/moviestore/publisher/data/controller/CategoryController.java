d082cd83ed3873a20d0be3e171cd0cd787f89f53
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 00:55:47 +0200

    Update CategoryController.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/controller/CategoryController.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/controller/CategoryController.java
@@ -18,9 +18,14 @@
 
 package local.moviestore.publisher.data.controller;
 
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheRepositoryResource;
 import io.quarkus.rest.data.panache.ResourceProperties;
 
+import local.moviestore.publisher.data.model.Category;
+import local.moviestore.publisher.data.repository.CategoryRepository;
+
 @ResourceProperties(path = "category")
-public interface CategoryController {
+public interface CategoryController 
+		extends PanacheRepositoryResource<CategoryRepository, Category, Long> {
 
 }
