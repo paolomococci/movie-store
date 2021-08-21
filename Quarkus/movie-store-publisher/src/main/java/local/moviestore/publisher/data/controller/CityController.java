608872c28f49e89dbaad66f6038983c369b22e67
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 00:55:17 +0200

    Update CityController.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/controller/CityController.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/controller/CityController.java
@@ -18,9 +18,14 @@
 
 package local.moviestore.publisher.data.controller;
 
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheRepositoryResource;
 import io.quarkus.rest.data.panache.ResourceProperties;
 
+import local.moviestore.publisher.data.model.City;
+import local.moviestore.publisher.data.repository.CityRepository;
+
 @ResourceProperties(path = "city")
-public interface CityController {
+public interface CityController 
+		extends PanacheRepositoryResource<CityRepository, City, Long> {
 
 }
