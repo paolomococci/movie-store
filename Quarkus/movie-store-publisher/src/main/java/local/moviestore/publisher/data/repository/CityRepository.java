969ae7ff3f919fdacf5df3418a3295a09b6871a4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 00:16:24 +0200

    Create CityRepository.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/repository/CityRepository.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/repository/CityRepository.java
@@ -20,7 +20,11 @@ package local.moviestore.publisher.data.repository;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import local.moviestore.publisher.data.model.City;
+
 @ApplicationScoped
-public class CityRepository {
+public class CityRepository 
+		implements PanacheRepository<City> {
 
 }
