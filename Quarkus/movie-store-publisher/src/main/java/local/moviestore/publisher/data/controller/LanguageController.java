ea0c30e65020718ddd0e7d1dd0d6df5b40ecd070
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 00:51:25 +0200

    Update LanguageController.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/controller/LanguageController.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/controller/LanguageController.java
@@ -18,9 +18,14 @@
 
 package local.moviestore.publisher.data.controller;
 
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheRepositoryResource;
 import io.quarkus.rest.data.panache.ResourceProperties;
 
+import local.moviestore.publisher.data.model.Language;
+import local.moviestore.publisher.data.repository.LanguageRepository;
+
 @ResourceProperties(path = "language")
-public interface LanguageController {
+public interface LanguageController 
+		extends PanacheRepositoryResource<LanguageRepository, Language, Long> {
 
 }
