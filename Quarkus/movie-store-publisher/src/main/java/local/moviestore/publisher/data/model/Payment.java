515fb414d95873ad6dfe0ddb0f21115ff9d33f07
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-21 23:31:32 +0200

    Update Payment.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/Payment.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/Payment.java
@@ -19,10 +19,15 @@
 package local.moviestore.publisher.data.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "PAYMENT")
 public class Payment {
 
+	@Id
+	@GeneratedValue
+	private Long id;
 }
