828a56d2c8d6d02879a6477446158f3137606ea1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 00:02:58 +0200

    Update DistrictController.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/controller/DistrictController.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/controller/DistrictController.java
@@ -21,6 +21,6 @@ package local.moviestore.publisher.data.controller;
 import io.quarkus.rest.data.panache.ResourceProperties;
 
 @ResourceProperties(path = "district")
-public class DistrictController {
+public interface DistrictController {
 
 }
