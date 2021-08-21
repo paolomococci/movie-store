66093bf12204ffeeaca4dd15d8198c11d32bc71f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-21 23:30:27 +0200

    Update Staff.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/Staff.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/Staff.java
@@ -19,10 +19,15 @@
 package local.moviestore.publisher.data.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "STAFF")
 public class Staff {
 
+	@Id
+	@GeneratedValue
+	private Long id;
 }
