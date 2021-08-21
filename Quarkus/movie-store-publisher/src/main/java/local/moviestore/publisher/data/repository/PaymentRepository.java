d67cdd1057af2732fe0904da906d19819e795686
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 00:12:01 +0200

    Update PaymentRepository.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/repository/PaymentRepository.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/repository/PaymentRepository.java
@@ -20,7 +20,11 @@ package local.moviestore.publisher.data.repository;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import local.moviestore.publisher.data.model.Payment;
+
 @ApplicationScoped
-public class PaymentRepository {
+public class PaymentRepository 
+		implements PanacheRepository<Payment> {
 
 }
