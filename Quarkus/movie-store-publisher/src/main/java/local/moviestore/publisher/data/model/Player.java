b58cfb408adef218ee35b01a2f33bc1b6b96ab70
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-21 23:31:10 +0200

    Update Player.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/Player.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/Player.java
@@ -19,10 +19,15 @@
 package local.moviestore.publisher.data.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "PLAYER")
 public class Player {
 
+	@Id
+	@GeneratedValue
+	private Long id;
 }
