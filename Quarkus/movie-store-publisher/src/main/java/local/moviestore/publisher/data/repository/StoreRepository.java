40c941af1ae397154415e4dcbc7ae697441ed971
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 00:10:33 +0200

    Update StoreRepository.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/repository/StoreRepository.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/repository/StoreRepository.java
@@ -20,7 +20,11 @@ package local.moviestore.publisher.data.repository;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import local.moviestore.publisher.data.model.Store;
+
 @ApplicationScoped
-public class StoreRepository {
+public class StoreRepository 
+		implements PanacheRepository<Store> {
 
 }
