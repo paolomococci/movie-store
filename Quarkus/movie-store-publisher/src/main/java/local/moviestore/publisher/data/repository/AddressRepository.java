918b4858dd1cd5a31602b765a13f5d3810543646
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 00:17:10 +0200

    Update AddressRepository.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/repository/AddressRepository.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/repository/AddressRepository.java
@@ -20,7 +20,11 @@ package local.moviestore.publisher.data.repository;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import local.moviestore.publisher.data.model.Address;
+
 @ApplicationScoped
-public class AddressRepository {
+public class AddressRepository 
+		implements PanacheRepository<Address> {
 
 }
