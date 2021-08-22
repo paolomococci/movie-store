1fd40a30a6bbf382b5ff271df38eaa7118c70d6c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 15:34:50 +0200

    Update Language.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/Language.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/Language.java
@@ -35,4 +35,20 @@ public class Language {
 
 	private String name;
 	private Timestamp updated;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Timestamp getUpdated() {
+		return updated;
+	}
 }
