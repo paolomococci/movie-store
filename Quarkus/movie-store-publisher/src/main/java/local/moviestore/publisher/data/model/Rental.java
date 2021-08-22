0631b2fc5d5bb6a41ed4ff6e6e1bb20a451e0360
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 16:13:34 +0200

    Update Rental.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/Rental.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/Rental.java
@@ -23,6 +23,7 @@ import java.sql.Timestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -33,7 +34,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 public class Rental {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@JsonFormat(pattern = "yyyy-MM-dd")
