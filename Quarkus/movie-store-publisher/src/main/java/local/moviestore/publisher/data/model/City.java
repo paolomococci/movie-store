4ef52f334e9c02626c7dcaf72805a50422ee4173
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 16:44:50 +0200

    Update City.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/City.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/City.java
@@ -19,11 +19,15 @@
 package local.moviestore.publisher.data.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -36,6 +40,13 @@ public class City {
 
 	private String name;
 	private Timestamp updated;
+
+	@ManyToOne
+	@JoinColumn(name = "country_fk")
+	private Country country;
+
+	@OneToMany(mappedBy = "city")
+	private List<District> districts;
 
 	public Long getId() {
 		return id;
