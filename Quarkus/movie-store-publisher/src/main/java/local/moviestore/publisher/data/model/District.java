dc1939a256a974e2c0afd72710156ed05e8c5b6d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 16:10:34 +0200

    Update District.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/District.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/District.java
@@ -22,6 +22,7 @@ import java.sql.Timestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -30,7 +31,7 @@ import javax.persistence.Table;
 public class District {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String name;
@@ -40,19 +41,24 @@ public class District {
 	public Long getId() {
 		return id;
 	}
+
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
+
 	public String getCode() {
 		return code;
 	}
-	public void setCode(String code) {
-		this.code = code;
-	}
+
 	public Timestamp getUpdated() {
 		return updated;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
 	}
 }
