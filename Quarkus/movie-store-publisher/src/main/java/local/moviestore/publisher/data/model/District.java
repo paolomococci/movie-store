ba448acb9b14b040f57bc8c290cd6740f07d4917
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 10:18:07 +0200

    Update District.java

--- Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/District.java
+++ Quarkus/movie-store-publisher/src/main/java/local/moviestore/publisher/data/model/District.java
@@ -36,4 +36,23 @@ public class District {
 	private String name;
 	private String code;
 	private Timestamp updated;
+
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public Timestamp getUpdated() {
+		return updated;
+	}
 }
