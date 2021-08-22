4b557ea1b34d0eeea9b18dccec278f504dabd88a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-22 15:50:41 +0200

    Create store.sql

---
+++ Quarkus/movie-store-publisher/src/main/resources/sql/store.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `STORE` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `updated` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
