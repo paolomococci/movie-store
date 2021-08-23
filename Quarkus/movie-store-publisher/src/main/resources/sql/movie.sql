423dfd972134ffd649ec2af2502a58386e03e8dd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-23 05:55:38 +0200

    Update movie.sql

--- Quarkus/movie-store-publisher/src/main/resources/sql/movie.sql
+++ Quarkus/movie-store-publisher/src/main/resources/sql/movie.sql
@@ -1,5 +1,5 @@
-CREATE TABLE `MOVIE` (
-  `id` bigint(20) NOT NULL,
+CREATE TABLE moviestore_db.movie (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `comeout` date DEFAULT NULL,
   `cost` double DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
@@ -9,5 +9,8 @@ CREATE TABLE `MOVIE` (
   `subtitle` varchar(255) DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
   `updated` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `language_fk` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKec85gtqm6ya7kbd5e16fxr5lp` (`language_fk`),
+  CONSTRAINT `FKec85gtqm6ya7kbd5e16fxr5lp` FOREIGN KEY (`language_fk`) REFERENCES `language` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
