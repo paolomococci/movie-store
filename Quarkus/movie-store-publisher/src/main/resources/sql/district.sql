09daf293066eff0200eab76a5a1d352d536b2508
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-23 05:59:06 +0200

    Update district.sql

--- Quarkus/movie-store-publisher/src/main/resources/sql/district.sql
+++ Quarkus/movie-store-publisher/src/main/resources/sql/district.sql
@@ -1,7 +1,10 @@
-CREATE TABLE `DISTRICT` (
-  `id` bigint(20) NOT NULL,
+CREATE TABLE moviestore_db.district (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `code` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `updated` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `city_fk` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKssu7out7o2kj77x6q2i109e6` (`city_fk`),
+  CONSTRAINT `FKssu7out7o2kj77x6q2i109e6` FOREIGN KEY (`city_fk`) REFERENCES `city` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
