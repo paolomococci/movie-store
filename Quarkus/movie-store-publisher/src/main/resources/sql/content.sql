fe6e34dafd491461a9384787a7b529f0542f2f7e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-23 06:01:03 +0200

    Update content.sql

--- Quarkus/movie-store-publisher/src/main/resources/sql/content.sql
+++ Quarkus/movie-store-publisher/src/main/resources/sql/content.sql
@@ -1,5 +1,5 @@
-CREATE TABLE `CONTENT` (
-  `id` bigint(20) NOT NULL,
+CREATE TABLE moviestore_db.content (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `subject` varchar(255) DEFAULT NULL,
   `updated` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`)
