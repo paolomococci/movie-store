c93b22a93e0e2bb5112a7d84b5aa4c1a0433b836
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-23 06:01:49 +0200

    Update city.sql

--- Quarkus/movie-store-publisher/src/main/resources/sql/city.sql
+++ Quarkus/movie-store-publisher/src/main/resources/sql/city.sql
@@ -1,6 +1,9 @@
-CREATE TABLE `CITY` (
-  `id` bigint(20) NOT NULL,
+CREATE TABLE moviestore_db.city (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `updated` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `country_fk` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK8mipuflnidcgu8cvvk595f9ot` (`country_fk`),
+  CONSTRAINT `FK8mipuflnidcgu8cvvk595f9ot` FOREIGN KEY (`country_fk`) REFERENCES `country` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
