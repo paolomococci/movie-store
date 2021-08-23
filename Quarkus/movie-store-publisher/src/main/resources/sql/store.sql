19604a5e7b482db80aae858167615cfd64ac7ac9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-23 05:48:34 +0200

    Update store.sql

--- Quarkus/movie-store-publisher/src/main/resources/sql/store.sql
+++ Quarkus/movie-store-publisher/src/main/resources/sql/store.sql
@@ -1,6 +1,9 @@
-CREATE TABLE `STORE` (
-  `id` bigint(20) NOT NULL,
+CREATE TABLE moviestore_db.store (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `updated` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `address_fk` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK9iciaukn8x9vpvqpfvwfde2fu` (`address_fk`),
+  CONSTRAINT `FK9iciaukn8x9vpvqpfvwfde2fu` FOREIGN KEY (`address_fk`) REFERENCES `address` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
