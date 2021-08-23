783235d64df73133c58f73061e2560695791db52
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-23 05:52:05 +0200

    Update payment.sql

--- Quarkus/movie-store-publisher/src/main/resources/sql/payment.sql
+++ Quarkus/movie-store-publisher/src/main/resources/sql/payment.sql
@@ -1,7 +1,16 @@
-CREATE TABLE `PAYMENT` (
-  `id` bigint(20) NOT NULL,
+CREATE TABLE moviestore_db.payment (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `amount` double DEFAULT NULL,
   `payed` date DEFAULT NULL,
   `updated` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `customer_fk` bigint(20) DEFAULT NULL,
+  `staff_fk` bigint(20) DEFAULT NULL,
+  `rental_fk` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK5j2uj2gqovn84ffi1nx3q29io` (`customer_fk`),
+  KEY `FK5gqbi37x0oolfhk5naiutpf68` (`staff_fk`),
+  KEY `FKjm17ljmts2eavkoxqv1384mvx` (`rental_fk`),
+  CONSTRAINT `FK5gqbi37x0oolfhk5naiutpf68` FOREIGN KEY (`staff_fk`) REFERENCES `staff` (`id`),
+  CONSTRAINT `FK5j2uj2gqovn84ffi1nx3q29io` FOREIGN KEY (`customer_fk`) REFERENCES `customer` (`id`),
+  CONSTRAINT `FKjm17ljmts2eavkoxqv1384mvx` FOREIGN KEY (`rental_fk`) REFERENCES `rental` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
