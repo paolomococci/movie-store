d6d042edf0d287fcd93020b69834749cd79d9ebf
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-23 06:04:06 +0200

    Update address.sql

--- Quarkus/movie-store-publisher/src/main/resources/sql/address.sql
+++ Quarkus/movie-store-publisher/src/main/resources/sql/address.sql
@@ -1,9 +1,12 @@
-CREATE TABLE `ADDRESS` (
-  `id` bigint(20) NOT NULL,
+CREATE TABLE moviestore_db.address (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `civic` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
   `type` varchar(255) DEFAULT NULL,
   `updated` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `district_fk` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKfx5yqnw5eihcfaubq9sqq0ca1` (`district_fk`),
+  CONSTRAINT `FKfx5yqnw5eihcfaubq9sqq0ca1` FOREIGN KEY (`district_fk`) REFERENCES `district` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
