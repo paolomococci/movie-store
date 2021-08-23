539f66e8b83bd96e44a62899082e718da27d1729
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-23 05:59:48 +0200

    Update customer.sql

--- Quarkus/movie-store-publisher/src/main/resources/sql/customer.sql
+++ Quarkus/movie-store-publisher/src/main/resources/sql/customer.sql
@@ -1,5 +1,5 @@
-CREATE TABLE `CUSTOMER` (
-  `id` bigint(20) NOT NULL,
+CREATE TABLE moviestore_db.customer (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `active` bit(1) DEFAULT NULL,
   `birthday` date DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
@@ -9,5 +9,11 @@ CREATE TABLE `CUSTOMER` (
   `phone` varchar(255) DEFAULT NULL,
   `surname` varchar(255) DEFAULT NULL,
   `updated` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `address_fk` bigint(20) DEFAULT NULL,
+  `store_fk` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK28dg7c4ivpo97omo2dp3hfckj` (`address_fk`),
+  KEY `FK3j55ur00kg6vqo5y3vv746w0n` (`store_fk`),
+  CONSTRAINT `FK28dg7c4ivpo97omo2dp3hfckj` FOREIGN KEY (`address_fk`) REFERENCES `address` (`id`),
+  CONSTRAINT `FK3j55ur00kg6vqo5y3vv746w0n` FOREIGN KEY (`store_fk`) REFERENCES `store` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
