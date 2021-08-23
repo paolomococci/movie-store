c43faa7e83a5325b00f5d3dd8a545a3864b6a206
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-23 06:03:29 +0200

    Update category.sql

--- Quarkus/movie-store-publisher/src/main/resources/sql/category.sql
+++ Quarkus/movie-store-publisher/src/main/resources/sql/category.sql
@@ -1,5 +1,5 @@
-CREATE TABLE `CATEGORY` (
-  `id` bigint(20) NOT NULL,
+CREATE TABLE moviestore_db.category (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `description` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `updated` datetime(6) DEFAULT NULL,
