ecc01de9bc070866a3e703125b26b71273341b6b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-23 05:58:28 +0200

    Update inventory.sql

--- Quarkus/movie-store-publisher/src/main/resources/sql/inventory.sql
+++ Quarkus/movie-store-publisher/src/main/resources/sql/inventory.sql
@@ -1,5 +1,11 @@
-CREATE TABLE `INVENTORY` (
-  `id` bigint(20) NOT NULL,
+CREATE TABLE moviestore_db.inventory (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `updated` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `store_fk` bigint(20) DEFAULT NULL,
+  `movie_fk` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKmt509f7cu666uym7sud2mtw2r` (`store_fk`),
+  KEY `FK9e1xmu2xtghox18wyrlr4rog2` (`movie_fk`),
+  CONSTRAINT `FK9e1xmu2xtghox18wyrlr4rog2` FOREIGN KEY (`movie_fk`) REFERENCES `movie` (`id`),
+  CONSTRAINT `FKmt509f7cu666uym7sud2mtw2r` FOREIGN KEY (`store_fk`) REFERENCES `store` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
