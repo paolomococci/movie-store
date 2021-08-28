cbbb5bf547987de699610f4cd4b59863ea38da41
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-28 10:55:42 +0200

    Update InventoryControllerIT.java

--- Quarkus/movie-store-publisher/src/test/java/local/moviestore/publisher/data/controller/it/InventoryControllerIT.java
+++ Quarkus/movie-store-publisher/src/test/java/local/moviestore/publisher/data/controller/it/InventoryControllerIT.java
@@ -20,7 +20,10 @@ package local.moviestore.publisher.data.controller.it;
 
 import io.quarkus.test.junit.NativeImageTest;
 
+import local.moviestore.publisher.data.controller.InventoryControllerTests;
+
 @NativeImageTest
-public class InventoryControllerIT {
+public class InventoryControllerIT 
+		extends InventoryControllerTests {
 
 }
