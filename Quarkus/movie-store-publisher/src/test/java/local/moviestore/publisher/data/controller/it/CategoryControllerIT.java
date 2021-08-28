13afcf4cca7caf49e4a4eabcc9ef7cbb2a1d2b81
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-28 10:53:39 +0200

    Update CategoryControllerIT.java

--- Quarkus/movie-store-publisher/src/test/java/local/moviestore/publisher/data/controller/it/CategoryControllerIT.java
+++ Quarkus/movie-store-publisher/src/test/java/local/moviestore/publisher/data/controller/it/CategoryControllerIT.java
@@ -20,7 +20,10 @@ package local.moviestore.publisher.data.controller.it;
 
 import io.quarkus.test.junit.NativeImageTest;
 
+import local.moviestore.publisher.data.controller.CategoryControllerTests;
+
 @NativeImageTest
-public class CategoryControllerIT {
+public class CategoryControllerIT 
+		extends CategoryControllerTests {
 
 }
