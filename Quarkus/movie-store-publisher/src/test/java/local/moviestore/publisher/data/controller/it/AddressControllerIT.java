bf3193962ce9b3ba6843329815e45306f4a1c8a8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-28 10:53:22 +0200

    Update AddressControllerIT.java

--- Quarkus/movie-store-publisher/src/test/java/local/moviestore/publisher/data/controller/it/AddressControllerIT.java
+++ Quarkus/movie-store-publisher/src/test/java/local/moviestore/publisher/data/controller/it/AddressControllerIT.java
@@ -20,7 +20,10 @@ package local.moviestore.publisher.data.controller.it;
 
 import io.quarkus.test.junit.NativeImageTest;
 
+import local.moviestore.publisher.data.controller.AddressControllerTests;
+
 @NativeImageTest
-public class AddressControllerIT {
+public class AddressControllerIT 
+		extends AddressControllerTests {
 
 }
