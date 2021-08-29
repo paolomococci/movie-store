dbbe8e8e5ff38580eceed817a2a9f3ab4e355700
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-29 11:26:23 +0200

    Update CustomerControllerTests.java

--- Quarkus/movie-store-publisher/src/test/java/local/moviestore/publisher/data/controller/CustomerControllerTests.java
+++ Quarkus/movie-store-publisher/src/test/java/local/moviestore/publisher/data/controller/CustomerControllerTests.java
@@ -88,7 +88,11 @@ public class CustomerControllerTests {
 	@Test
 	@Order(6)
 	public void deleteTest() {
-		
+		RestAssured.given()
+				.when()
+				.delete("/customer/{id}", CustomerControllerTests.getCustomer().getId())
+				.then()
+				.statusCode(204);
 	}
 
 	public static Customer getCustomer() {
