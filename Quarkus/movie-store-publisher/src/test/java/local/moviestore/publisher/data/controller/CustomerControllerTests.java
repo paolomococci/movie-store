cfbcdbbae9f7eca90248385fb396c5c1aa9b44a6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-29 05:43:14 +0200

    Update CustomerControllerTests.java

--- Quarkus/movie-store-publisher/src/test/java/local/moviestore/publisher/data/controller/CustomerControllerTests.java
+++ Quarkus/movie-store-publisher/src/test/java/local/moviestore/publisher/data/controller/CustomerControllerTests.java
@@ -22,6 +22,9 @@ import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
@@ -29,9 +32,11 @@ import io.restassured.RestAssured;
 import local.moviestore.publisher.data.model.Customer;
 
 @QuarkusTest
+@TestMethodOrder(OrderAnnotation.class)
 public class CustomerControllerTests {
 
 	@Test
+	@Order(1)
 	public void readAllEmptyTest() {
 		List<Customer> customers = RestAssured.given()
 				.when().get("/customer")
