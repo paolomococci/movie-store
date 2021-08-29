d4abfd09dd69056beff16fc30cfb14eb270b65c5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-29 05:43:42 +0200

    Update CountryControllerTests.java

--- Quarkus/movie-store-publisher/src/test/java/local/moviestore/publisher/data/controller/CountryControllerTests.java
+++ Quarkus/movie-store-publisher/src/test/java/local/moviestore/publisher/data/controller/CountryControllerTests.java
@@ -22,6 +22,9 @@ import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
@@ -29,9 +32,11 @@ import io.restassured.RestAssured;
 import local.moviestore.publisher.data.model.Country;
 
 @QuarkusTest
+@TestMethodOrder(OrderAnnotation.class)
 public class CountryControllerTests {
 
 	@Test
+	@Order(1)
 	public void readAllEmptyTest() {
 		List<Country> countries = RestAssured.given()
 				.when().get("/country")
