10172b3ae509555049ba1d111b2ed31587cb64f8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-29 05:52:14 +0200

    Update CityControllerTests.java

--- Quarkus/movie-store-publisher/src/test/java/local/moviestore/publisher/data/controller/CityControllerTests.java
+++ Quarkus/movie-store-publisher/src/test/java/local/moviestore/publisher/data/controller/CityControllerTests.java
@@ -48,4 +48,34 @@ public class CityControllerTests {
 				.getList(".", City.class);
 		Assertions.assertTrue(cities.isEmpty());
 	}
+
+	@Test
+	@Order(2)
+	public void createTest() {
+		
+	}
+
+	@Test
+	@Order(3)
+	public void readTest() {
+		
+	}
+
+	@Test
+	@Order(4)
+	public void readAllTest() {
+		
+	}
+
+	@Test
+	@Order(5)
+	public void updateTest() {
+		
+	}
+
+	@Test
+	@Order(6)
+	public void deleteTest() {
+		
+	}
 }
