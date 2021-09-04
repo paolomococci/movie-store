2d51007c5c42c222195852c7405f2c7d042e1a69
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-04 23:18:11 +0200

    Update Content.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Content.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Content.java
@@ -18,5 +18,55 @@
 
 package local.moviestore.data.model;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "content")
 public class Content {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String subject;
+    private Timestamp updated;
+
+    /* reference to movie entity */
+
+    @ManyToMany(mappedBy = "contents")
+    private List<Movie> movies;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public Timestamp getUpdated() {
+        return updated;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    /* getter and setter to reference of movie entity */
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
 }
