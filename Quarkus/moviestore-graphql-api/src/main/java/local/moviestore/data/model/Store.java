6d5f574ebaaa0ecc3d93f0b52755254dd012da00
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-04 23:25:03 +0200

    Update Store.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Store.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Store.java
@@ -18,5 +18,95 @@
 
 package local.moviestore.data.model;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "store")
 public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Timestamp updated;
+
+    /* references to other entities */
+
+    @ManyToOne
+    @JoinColumn(name = "address_fk")
+    private Address storeAddress;
+
+    @OneToMany(mappedBy = "staffStore")
+    private List<Staff> teams;
+
+    @OneToMany(mappedBy = "customerStore")
+    private List<Customer> customers;
+
+    @OneToMany(mappedBy = "inventoryStore")
+    private List<Inventory> inventories;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Timestamp getUpdated() {
+        return updated;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /* getter and setter to references of other entities */
+
+    public Address getStoreAddress() {
+        return storeAddress;
+    }
+
+    public List<Staff> getTeams() {
+        return teams;
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public List<Inventory> getInventories() {
+        return inventories;
+    }
+
+    public void setUpdated(Timestamp updated) {
+        this.updated = updated;
+    }
+
+    public void setStoreAddress(Address storeAddress) {
+        this.storeAddress = storeAddress;
+    }
+
+    public void setTeams(List<Staff> teams) {
+        this.teams = teams;
+    }
+
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
+    }
+
+    public void setInventories(List<Inventory> inventories) {
+        this.inventories = inventories;
+    }
 }
