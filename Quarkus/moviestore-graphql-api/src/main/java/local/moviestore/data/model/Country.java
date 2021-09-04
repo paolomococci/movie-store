db3ba0e0e9161fb45f79ac2bbf9b6e49bac25c27
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-04 23:18:50 +0200

    Update Country.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Country.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Country.java
@@ -18,5 +18,67 @@
 
 package local.moviestore.data.model;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "country")
 public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Timestamp updated;
+
+    /* references to other entities */
+
+    @ManyToOne
+    private Iso3166 iso3166;
+
+    @OneToMany(mappedBy = "country")
+    private List<City> cities;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Timestamp getUpdated() {
+        return updated;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /* getter and setter to references of other entities */
+
+    public Iso3166 getIso3166() {
+        return iso3166;
+    }
+
+    public List<City> getCities() {
+        return cities;
+    }
+
+    public void setIso3166(Iso3166 iso3166) {
+        this.iso3166 = iso3166;
+    }
+
+    public void setCities(List<City> cities) {
+        this.cities = cities;
+    }
 }
