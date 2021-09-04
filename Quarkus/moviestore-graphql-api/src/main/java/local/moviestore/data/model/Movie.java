fa01707a68a6165dd6151bf716645bd33a36f263
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-04 23:22:50 +0200

    Update Movie.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Movie.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Movie.java
@@ -18,5 +18,192 @@
 
 package local.moviestore.data.model;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "movie")
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String subtitle;
+    private String description;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date comeout;
+
+    @JsonFormat(pattern = "HH:mm:ss")
+    private Time duration;
+
+    private Double cost;
+    private Double rent;
+    private Double rating;
+    private Timestamp updated;
+
+    /* references to other entities */
+
+    @ManyToOne
+    @JoinColumn(name = "language_fk")
+    private Language language;
+
+    @OneToMany(mappedBy = "movie")
+    private List<Inventory> inventories;
+
+    @ManyToMany
+    @JoinTable(
+            name = "movie_category",
+            joinColumns = {@JoinColumn(name = "movie_id")},
+            inverseJoinColumns = {@JoinColumn(name = "category_id")})
+    private List<Category> categories;
+
+    @ManyToMany
+    @JoinTable(
+            name = "movie_content",
+            joinColumns = {@JoinColumn(name = "movie_id")},
+            inverseJoinColumns = {@JoinColumn(name = "content_id")})
+    private List<Category> contents;
+
+    @ManyToMany
+    @JoinTable(
+            name = "movie_player",
+            joinColumns = {@JoinColumn(name = "movie_id")},
+            inverseJoinColumns = {@JoinColumn(name = "player_id")})
+    private List<Category> players;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getComeout() {
+        return comeout;
+    }
+
+    public Time getDuration() {
+        return duration;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public Double getRent() {
+        return rent;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public Timestamp getUpdated() {
+        return updated;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setComeout(Date comeout) {
+        this.comeout = comeout;
+    }
+
+    public void setDuration(Time duration) {
+        this.duration = duration;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public void setRent(Double rent) {
+        this.rent = rent;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    /* getter and setter to references of other entities */
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public List<Inventory> getInventories() {
+        return inventories;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public List<Category> getContents() {
+        return contents;
+    }
+
+    public List<Category> getPlayers() {
+        return players;
+    }
+
+    public void setUpdated(Timestamp updated) {
+        this.updated = updated;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public void setInventories(List<Inventory> inventories) {
+        this.inventories = inventories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public void setContents(List<Category> contents) {
+        this.contents = contents;
+    }
+
+    public void setPlayers(List<Category> players) {
+        this.players = players;
+    }
 }
