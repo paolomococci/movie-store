a2318a5f4bc5b99905154012367ca827f0864e89
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-04 21:10:12 +0200

    Update NativeDistrictResourceIT.java

--- Quarkus/moviestore-graphql-api/src/native-test/java/local/moviestore/data/resource/NativeDistrictResourceIT.java
+++ Quarkus/moviestore-graphql-api/src/native-test/java/local/moviestore/data/resource/NativeDistrictResourceIT.java
@@ -18,5 +18,9 @@
 
 package local.moviestore.data.resource;
 
-public class NativeDistrictResourceIT {
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeDistrictResourceIT
+        extends DistrictResourceTests {
 }
