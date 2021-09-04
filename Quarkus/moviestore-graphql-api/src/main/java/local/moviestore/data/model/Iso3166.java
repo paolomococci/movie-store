412a6f8915c341f43d3c3f3bffce5803a517c7ef
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-04 23:21:48 +0200

    Update Iso3166.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Iso3166.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Iso3166.java
@@ -18,5 +18,73 @@
 
 package local.moviestore.data.model;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "iso3166")
 public class Iso3166 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Integer code;
+    private String signature;
+    private Timestamp updated;
+
+    /* reference to country entity */
+
+    @OneToMany(mappedBy = "iso3166")
+    private List<Country> countries;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public Timestamp getUpdated() {
+        return updated;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    /* getter and setter to reference of country entity */
+
+    public List<Country> getCountries() {
+        return countries;
+    }
+
+    public void setCountries(List<Country> countries) {
+        this.countries = countries;
+    }
 }
