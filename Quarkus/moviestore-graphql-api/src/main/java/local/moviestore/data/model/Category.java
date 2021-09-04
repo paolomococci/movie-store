1ca4e6cc84e8b7cea0bd573317d46cff0b09963e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-04 23:16:57 +0200

    Update Category.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Category.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Category.java
@@ -18,5 +18,64 @@
 
 package local.moviestore.data.model;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Timestamp updated;
+
+    /* reference to movie entity */
+
+    @ManyToMany(mappedBy = "categories")
+    private List<Movie> movies;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Timestamp getUpdated() {
+        return updated;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /* getter and setter to reference of movie entity */
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
 }
