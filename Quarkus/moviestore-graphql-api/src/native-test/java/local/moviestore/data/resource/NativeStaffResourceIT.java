8f492ecfbe3fa9f02033d5eb9cb7505342f66d24
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-04 21:06:09 +0200

    Update NativeStaffResourceIT.java

--- Quarkus/moviestore-graphql-api/src/native-test/java/local/moviestore/data/resource/NativeStaffResourceIT.java
+++ Quarkus/moviestore-graphql-api/src/native-test/java/local/moviestore/data/resource/NativeStaffResourceIT.java
@@ -18,5 +18,9 @@
 
 package local.moviestore.data.resource;
 
-public class NativeStaffResourceIT {
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeStaffResourceIT
+        extends StaffResourceTests {
 }
