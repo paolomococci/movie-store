4c3c41d7d4489fbd489bee548d11e3124bee8d13
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-04 23:22:22 +0200

    Update Language.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Language.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Language.java
@@ -18,5 +18,55 @@
 
 package local.moviestore.data.model;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "language")
 public class Language {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Timestamp updated;
+
+    /* reference to movie entity */
+
+    @OneToMany(mappedBy = "language")
+    private List<Movie> movies;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Timestamp getUpdated() {
+        return updated;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /* getter and setter to reference of movie entity */
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
 }
