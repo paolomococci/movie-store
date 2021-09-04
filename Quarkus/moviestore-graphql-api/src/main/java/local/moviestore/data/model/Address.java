1f044a9dc84a60fa8a5578035fe4880641a6cf8d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-04 23:16:10 +0200

    Update Address.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Address.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Address.java
@@ -18,5 +18,118 @@
 
 package local.moviestore.data.model;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "address")
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+    private String name;
+    private String civic;
+    private String phone;
+    private Timestamp updated;
+
+    /* references to other entities */
+
+    @ManyToOne
+    @JoinColumn(name = "district_fk")
+    private District district;
+
+    @OneToMany(mappedBy = "storeAddress")
+    private List<Store> stores;
+
+    @OneToMany(mappedBy = "staffAddress")
+    private List<Staff> staff;
+
+    @OneToMany(mappedBy = "customerAddress")
+    private List<Customer> residents;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCivic() {
+        return civic;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public Timestamp getUpdated() {
+        return updated;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCivic(String civic) {
+        this.civic = civic;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /* getter and setter to references of other entities */
+
+    public District getDistrict() {
+        return district;
+    }
+
+    public List<Store> getStores() {
+        return stores;
+    }
+
+    public List<Staff> getStaff() {
+        return staff;
+    }
+
+    public List<Customer> getResidents() {
+        return residents;
+    }
+
+    public void setDistrict(District district) {
+        this.district = district;
+    }
+
+    public void setStores(List<Store> stores) {
+        this.stores = stores;
+    }
+
+    public void setStaff(List<Staff> staff) {
+        this.staff = staff;
+    }
+
+    public void setResidents(List<Customer> residents) {
+        this.residents = residents;
+    }
 }
