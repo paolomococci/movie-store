a2f9963d04c441a96c518ec2eae3815f3c1a698b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-05 16:41:01 +0200

    Update CategoryRepository.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/repository/CategoryRepository.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/repository/CategoryRepository.java
@@ -18,8 +18,13 @@
 
 package local.moviestore.data.repository;
 
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import local.moviestore.data.model.Category;
+
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class CategoryRepository {
+public class CategoryRepository
+        implements PanacheRepository<Category> {
 }
