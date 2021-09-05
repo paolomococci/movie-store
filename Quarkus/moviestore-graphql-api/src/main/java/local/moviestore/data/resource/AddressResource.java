a0c1f69c49e14341121a333cd7024c96308351b5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-05 09:52:45 +0200

    Update AddressResource.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/resource/AddressResource.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/resource/AddressResource.java
@@ -18,8 +18,15 @@
 
 package local.moviestore.data.resource;
 
+import local.moviestore.data.service.AddressService;
+
 import org.eclipse.microprofile.graphql.GraphQLApi;
+
+import javax.inject.Inject;
 
 @GraphQLApi
 public class AddressResource {
+
+    @Inject
+    AddressService addressService;
 }
