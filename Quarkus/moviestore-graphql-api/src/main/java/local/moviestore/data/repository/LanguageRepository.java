197528696d27e2701d6cc71f98cc4a9ba554e33a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-05 16:36:29 +0200

    Update LanguageRepository.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/repository/LanguageRepository.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/repository/LanguageRepository.java
@@ -18,8 +18,13 @@
 
 package local.moviestore.data.repository;
 
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import local.moviestore.data.model.Language;
+
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class LanguageRepository {
+public class LanguageRepository
+        implements PanacheRepository<Language> {
 }
