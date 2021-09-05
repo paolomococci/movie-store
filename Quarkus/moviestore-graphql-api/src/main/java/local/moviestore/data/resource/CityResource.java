c86125299c382582be170b5d6e4196a5bafe7ee9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-05 15:12:24 +0200

    Update CityResource.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/resource/CityResource.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/resource/CityResource.java
@@ -18,15 +18,26 @@
 
 package local.moviestore.data.resource;
 
+import local.moviestore.data.model.City;
 import local.moviestore.data.service.CityService;
 
+import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Query;
 
 import javax.inject.Inject;
+import java.util.List;
 
 @GraphQLApi
 public class CityResource {
 
     @Inject
     CityService cityService;
+
+    @Query(value = "allCities")
+    @Description(value = "returns a list of all cities registered in the system")
+    public List<City> allCities() {
+        // TODO and, in the future, replace List with CompletionStage
+        return null;
+    }
 }
