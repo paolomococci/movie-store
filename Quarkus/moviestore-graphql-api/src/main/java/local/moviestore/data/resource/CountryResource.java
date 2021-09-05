174601401abccf59f40113d2d53d382465424d71
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-05 09:51:22 +0200

    Update CountryResource.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/resource/CountryResource.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/resource/CountryResource.java
@@ -18,8 +18,15 @@
 
 package local.moviestore.data.resource;
 
+import local.moviestore.data.service.CountryService;
+
 import org.eclipse.microprofile.graphql.GraphQLApi;
+
+import javax.inject.Inject;
 
 @GraphQLApi
 public class CountryResource {
+
+    @Inject
+    CountryService countryService;
 }
