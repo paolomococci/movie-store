97d612eaf377f2c43626fdf681a9ce9f9d8ad8ce
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-05 16:40:43 +0200

    Update CityRepository.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/repository/CityRepository.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/repository/CityRepository.java
@@ -18,8 +18,13 @@
 
 package local.moviestore.data.repository;
 
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import local.moviestore.data.model.City;
+
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class CityRepository {
+public class CityRepository
+        implements PanacheRepository<City> {
 }
