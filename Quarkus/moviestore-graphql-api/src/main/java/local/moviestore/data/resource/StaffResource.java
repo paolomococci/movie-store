0bac32dfb2ac408a7806cec2f1eaacb047acb2d9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-05 09:47:36 +0200

    Update StaffResource.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/resource/StaffResource.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/resource/StaffResource.java
@@ -18,8 +18,15 @@
 
 package local.moviestore.data.resource;
 
+import local.moviestore.data.service.StaffService;
+
 import org.eclipse.microprofile.graphql.GraphQLApi;
+
+import javax.inject.Inject;
 
 @GraphQLApi
 public class StaffResource {
+
+    @Inject
+    StaffService staffService;
 }
