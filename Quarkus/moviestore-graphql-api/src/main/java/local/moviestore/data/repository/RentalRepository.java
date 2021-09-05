2cc643b2b98843b2863d9f11b45f9b0675cc1d1b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-05 16:27:51 +0200

    Update RentalRepository.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/repository/RentalRepository.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/repository/RentalRepository.java
@@ -18,8 +18,13 @@
 
 package local.moviestore.data.repository;
 
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import local.moviestore.data.model.Rental;
+
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class RentalRepository {
+public class RentalRepository
+        implements PanacheRepository<Rental> {
 }
