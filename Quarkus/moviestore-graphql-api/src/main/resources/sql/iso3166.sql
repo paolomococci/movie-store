285729889c9b55f531ca9b47c38ccd64cf98b46a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-06 05:41:38 +0200

    Create iso3166.sql

---
+++ Quarkus/moviestore-graphql-api/src/main/resources/sql/iso3166.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `iso3166` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `code` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `signature` varchar(255) DEFAULT NULL,
+  `updated` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
