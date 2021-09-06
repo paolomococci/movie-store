70d87c6500c6e628fc8ec20c6e4bc849e5e31686
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-06 05:47:38 +0200

    Create store.sql

---
+++ Quarkus/moviestore-graphql-api/src/main/resources/sql/store.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `store` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `updated` datetime(6) DEFAULT NULL,
+  `address_fk` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK9iciaukn8x9vpvqpfvwfde2fu` (`address_fk`),
+  CONSTRAINT `FK9iciaukn8x9vpvqpfvwfde2fu` FOREIGN KEY (`address_fk`) REFERENCES `address` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
