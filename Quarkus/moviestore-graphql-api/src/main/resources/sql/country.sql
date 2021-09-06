b4682257bb1f939019a887a8adc71e05344f21c8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-06 05:39:29 +0200

    Create country.sql

---
+++ Quarkus/moviestore-graphql-api/src/main/resources/sql/country.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `country` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `updated` datetime(6) DEFAULT NULL,
+  `iso3166_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK4nwmqywexxigox0ml7vnbalfk` (`iso3166_id`),
+  CONSTRAINT `FK4nwmqywexxigox0ml7vnbalfk` FOREIGN KEY (`iso3166_id`) REFERENCES `iso3166` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
