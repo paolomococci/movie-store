6563692c727bb99ebcc00e30ade2eecc6177f73d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-06 05:38:49 +0200

    Create content.sql

---
+++ Quarkus/moviestore-graphql-api/src/main/resources/sql/content.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `content` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `subject` varchar(255) DEFAULT NULL,
+  `updated` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
