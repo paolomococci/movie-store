249e45faf22153fb8c0b4b1ad7f87384f406f0cd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-07 15:35:23 +0200

    Update StaffRepository.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/repository/StaffRepository.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/repository/StaffRepository.java
@@ -29,7 +29,7 @@ import java.util.List;
 public class StaffRepository
         implements PanacheRepository<Staff> {
 
-    public List<Staff> allStaff() {
+    public List<Staff> allStaffs() {
         return this.listAll();
     }
 }
