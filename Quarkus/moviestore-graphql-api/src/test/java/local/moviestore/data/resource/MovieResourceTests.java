f476ff98ae74a7b557ba39ae9edb12f0cae32043
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-07 16:25:42 +0200

    Update MovieResourceTests.java

--- Quarkus/moviestore-graphql-api/src/test/java/local/moviestore/data/resource/MovieResourceTests.java
+++ Quarkus/moviestore-graphql-api/src/test/java/local/moviestore/data/resource/MovieResourceTests.java
@@ -24,6 +24,12 @@ import org.junit.jupiter.api.Test;
 @QuarkusTest
 public class MovieResourceTests {
 
+    private static final String QUERY_ALL_MOVIES;
+
+    static {
+        QUERY_ALL_MOVIES = "";
+    }
+
     @Test
     public void emptyTest() {
 
