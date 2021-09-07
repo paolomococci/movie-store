e9cd391285d82ed38041eeabcebf5e909554161d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-07 15:08:20 +0200

    Update CustomerRepository.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/repository/CustomerRepository.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/repository/CustomerRepository.java
@@ -23,8 +23,13 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import local.moviestore.data.model.Customer;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 @ApplicationScoped
 public class CustomerRepository
         implements PanacheRepository<Customer> {
+
+    public List<Customer> allCustomers() {
+        return this.listAll();
+    }
 }
