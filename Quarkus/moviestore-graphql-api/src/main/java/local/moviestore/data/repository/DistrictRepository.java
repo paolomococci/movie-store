e30eb08d0808bfd0e1f41db308b5727de5c6f8fe
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-07 15:07:43 +0200

    Update DistrictRepository.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/repository/DistrictRepository.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/repository/DistrictRepository.java
@@ -23,8 +23,13 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import local.moviestore.data.model.District;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 @ApplicationScoped
 public class DistrictRepository
         implements PanacheRepository<District> {
+
+    public List<District> allDistricts() {
+        return this.listAll();
+    }
 }
