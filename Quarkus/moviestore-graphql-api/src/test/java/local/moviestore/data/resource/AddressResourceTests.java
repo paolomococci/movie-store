d6d7bf58aff3ab6e12627dacdc1feec70c278010
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-08 14:53:49 +0200

    Update AddressResourceTests.java

--- Quarkus/moviestore-graphql-api/src/test/java/local/moviestore/data/resource/AddressResourceTests.java
+++ Quarkus/moviestore-graphql-api/src/test/java/local/moviestore/data/resource/AddressResourceTests.java
@@ -21,24 +21,28 @@ package local.moviestore.data.resource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 public class AddressResourceTests {
 
+    private static final String PATH_SIZE;
     private static final String QUERY_ALL_ADDRESSES_TYPE_NAME_CIVIC_PHONE;
 
     static {
+        PATH_SIZE = "allAddresses.size()";
         QUERY_ALL_ADDRESSES_TYPE_NAME_CIVIC_PHONE = "{\"query\":\"{ allAddresses { type name civic phone}}\"}";
     }
 
     @Test
-    @Disabled
+    //@Disabled
     public void emptyTest() {
-        RestAssured.given().body(QUERY_ALL_ADDRESSES_TYPE_NAME_CIVIC_PHONE)
+        RestAssured.given().body(this.QUERY_ALL_ADDRESSES_TYPE_NAME_CIVIC_PHONE)
                 .then().contentType(ContentType.JSON)
-                .body("data.allAddresses.size()", Matchers.is(0))
+                .body(this.PATH_SIZE, Matchers.is(0))
                 .statusCode(200);
     }
 }
