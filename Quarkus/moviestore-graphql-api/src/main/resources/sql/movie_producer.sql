5b8361a90eefbcd91232e491cfcc7b7acb708f63
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-09 12:55:59 +0200

    Create movie_producer.sql

---
+++ Quarkus/moviestore-graphql-api/src/main/resources/sql/movie_producer.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `movie_producer` (
+  `movie_id` bigint(20) NOT NULL,
+  `producer_id` bigint(20) NOT NULL,
+  KEY `FK36mw05cutwof3spqh2i1wdrku` (`producer_id`),
+  KEY `FKe04q3b68c56s40c2nqdrw8hcx` (`movie_id`),
+  CONSTRAINT `FK36mw05cutwof3spqh2i1wdrku` FOREIGN KEY (`producer_id`) REFERENCES `producer` (`id`),
+  CONSTRAINT `FKe04q3b68c56s40c2nqdrw8hcx` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
