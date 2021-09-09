15ca6a5fea219be4029823c0addf3ec9ef9cd91b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-09 15:57:51 +0200

    Create movie_director.sql

---
+++ Quarkus/movie-store-publisher/src/main/resources/sql/movie_director.sql
@@ -0,0 +1,8 @@
+CREATE TABLE moviestore_db.movie_director (
+  `movie_id` bigint(20) NOT NULL,
+  `director_id` bigint(20) NOT NULL,
+  KEY `FKgn1rkfh7ioiax467kc9dcrcrr` (`director_id`),
+  KEY `FKbay4b2v2db4yfaww2oocpld9m` (`movie_id`),
+  CONSTRAINT `FKbay4b2v2db4yfaww2oocpld9m` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`),
+  CONSTRAINT `FKgn1rkfh7ioiax467kc9dcrcrr` FOREIGN KEY (`director_id`) REFERENCES `director` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
