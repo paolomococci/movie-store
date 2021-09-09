9d9d91f945b387bc2cda22c9a0369a34f4b8fb35
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-09 13:04:20 +0200

    Update movie_player.sql

--- Quarkus/movie-store-publisher/src/main/resources/sql/movie_player.sql
+++ Quarkus/movie-store-publisher/src/main/resources/sql/movie_player.sql
@@ -1,8 +1,2 @@
 CREATE TABLE moviestore_db.movie_player (
-  `movie_id` bigint(20) NOT NULL,
-  `player_id` bigint(20) NOT NULL,
-  KEY `FKpiw9mlbjj8i7hwuyokuumnqsv` (`player_id`),
-  KEY `FKhhyh6b04umdfgow640dsqttpl` (`movie_id`),
-  CONSTRAINT `FKhhyh6b04umdfgow640dsqttpl` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`),
-  CONSTRAINT `FKpiw9mlbjj8i7hwuyokuumnqsv` FOREIGN KEY (`player_id`) REFERENCES `category` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
