d2e58f1252d54550f43d5e8b2254b471ac68bb15
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-09 12:24:56 +0200

    Update director.sql

--- Quarkus/moviestore-graphql-api/src/main/resources/sql/director.sql
+++ Quarkus/moviestore-graphql-api/src/main/resources/sql/director.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE `director` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
+  `updated` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
