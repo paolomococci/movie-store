243c660267f085f77e44dcd2fb9cabbe6b535bc6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-09 11:31:41 +0200

    Update Director.java

--- Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Director.java
+++ Quarkus/moviestore-graphql-api/src/main/java/local/moviestore/data/model/Director.java
@@ -18,5 +18,19 @@
 
 package local.moviestore.data.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
 public class Director {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String nickname;
+    private String surname;
+    private Timestamp updated;
 }
