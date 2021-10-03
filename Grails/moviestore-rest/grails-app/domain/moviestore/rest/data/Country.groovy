60925b8080cdfbbe7e2e1d2149d8b5f7b95be76c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-10-04 00:22:50 +0200

    Update Country.groovy

--- Grails/moviestore-rest/grails-app/domain/moviestore/rest/data/Country.groovy
+++ Grails/moviestore-rest/grails-app/domain/moviestore/rest/data/Country.groovy
@@ -42,7 +42,7 @@ class Country {
     ]
 
     static constraints = {
-        code size: 8..12, blank: false, unique: true
+        code size: 8..15, blank: false, unique: true, matches: "^[0-9]*"
         name size: 1..127, matches: "^[a-zA-Z_-]*"
     }
 
