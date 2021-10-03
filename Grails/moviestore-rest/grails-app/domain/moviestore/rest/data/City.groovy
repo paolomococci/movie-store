3236a5ab964367d584c5376955a8accb92e7fcfe
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-10-03 17:09:58 +0200

    Update City.groovy

--- Grails/moviestore-rest/grails-app/domain/moviestore/rest/data/City.groovy
+++ Grails/moviestore-rest/grails-app/domain/moviestore/rest/data/City.groovy
@@ -20,6 +20,8 @@ package moviestore.rest.data
 
 import grails.rest.*
 
+import java.sql.Timestamp
+
 @Resource(
         readOnly = false,
         formats = ['json', 'xml'],
@@ -28,6 +30,8 @@ import grails.rest.*
 class City {
 
     String code
+    String name
+    Timestamp updated
 
     static constraints = {
         code size: 8..12, blank: false, unique: true
