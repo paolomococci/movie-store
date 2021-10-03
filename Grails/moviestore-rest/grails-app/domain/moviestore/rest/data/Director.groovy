822aa9f47c89d85cdfb2f43d22ebd9c76361cdaf
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-10-03 15:30:29 +0200

    Update Director.groovy

--- Grails/moviestore-rest/grails-app/domain/moviestore/rest/data/Director.groovy
+++ Grails/moviestore-rest/grails-app/domain/moviestore/rest/data/Director.groovy
@@ -27,4 +27,13 @@ import grails.rest.*
 )
 class Director {
 
+    String code
+
+    static constraints = {
+        code size: 8..12, blank: false, unique: true
+    }
+
+    String toString() {
+        code
+    }
 }
