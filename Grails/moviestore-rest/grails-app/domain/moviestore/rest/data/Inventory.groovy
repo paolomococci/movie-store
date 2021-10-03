9f88b485dcfe7212f651dcecf2b7959104cbde46
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-10-04 00:24:07 +0200

    Update Inventory.groovy

--- Grails/moviestore-rest/grails-app/domain/moviestore/rest/data/Inventory.groovy
+++ Grails/moviestore-rest/grails-app/domain/moviestore/rest/data/Inventory.groovy
@@ -42,7 +42,7 @@ class Inventory {
     ]
 
     static constraints = {
-        code size: 8..12, blank: false, unique: true
+        code size: 8..15, blank: false, unique: true, matches: "^[0-9]*"
     }
 
     String toString() {
