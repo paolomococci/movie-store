cfd03bd7d47552a2a6dbf526a201c6853302af0d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-10-03 17:12:28 +0200

    Update Inventory.groovy

--- Grails/moviestore-rest/grails-app/domain/moviestore/rest/data/Inventory.groovy
+++ Grails/moviestore-rest/grails-app/domain/moviestore/rest/data/Inventory.groovy
@@ -20,6 +20,8 @@ package moviestore.rest.data
 
 import grails.rest.*
 
+import java.sql.Timestamp
+
 @Resource(
         readOnly = false,
         formats = ['json', 'xml'],
@@ -28,6 +30,7 @@ import grails.rest.*
 class Inventory {
 
     String code
+    Timestamp updated
 
     static constraints = {
         code size: 8..12, blank: false, unique: true
