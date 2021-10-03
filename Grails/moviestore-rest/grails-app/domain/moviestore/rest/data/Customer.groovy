25b18984cfc7c4b633bcce3d2ed05d21f5bb73b3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-10-03 17:11:16 +0200

    Update Customer.groovy

--- Grails/moviestore-rest/grails-app/domain/moviestore/rest/data/Customer.groovy
+++ Grails/moviestore-rest/grails-app/domain/moviestore/rest/data/Customer.groovy
@@ -20,6 +20,8 @@ package moviestore.rest.data
 
 import grails.rest.*
 
+import java.sql.Timestamp
+
 @Resource(
         readOnly = false,
         formats = ['json', 'xml'],
@@ -28,6 +30,12 @@ import grails.rest.*
 class Customer {
 
     String code
+    String name
+    String nickname
+    String surname
+    String mobile
+    String email
+    Timestamp updated
 
     static constraints = {
         code size: 8..12, blank: false, unique: true
