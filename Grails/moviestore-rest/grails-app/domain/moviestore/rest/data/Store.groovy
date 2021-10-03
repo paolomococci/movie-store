bc60111016e37a664e258b10a7ff7f0a21600f6b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-10-04 00:26:38 +0200

    Update Store.groovy

--- Grails/moviestore-rest/grails-app/domain/moviestore/rest/data/Store.groovy
+++ Grails/moviestore-rest/grails-app/domain/moviestore/rest/data/Store.groovy
@@ -44,7 +44,7 @@ class Store {
     ]
 
     static constraints = {
-        code size: 8..12, blank: false, unique: true
+        code size: 8..15, blank: false, unique: true, matches: "^[0-9]*"
         name size: 1..127, matches: "^[a-zA-Z_-]*"
     }
 
