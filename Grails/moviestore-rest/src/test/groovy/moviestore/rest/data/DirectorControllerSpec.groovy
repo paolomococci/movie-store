88be462a699e25731f5a170bbbdeb2c7a6892294
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-10-05 09:10:33 +0200

    Create DirectorControllerSpec.groovy

---
+++ Grails/moviestore-rest/src/test/groovy/moviestore/rest/data/DirectorControllerSpec.groovy
@@ -0,0 +1,219 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package moviestore.rest.data
+
+import spock.lang.*
+
+import static org.springframework.http.HttpStatus.OK
+import static org.springframework.http.HttpStatus.NOT_FOUND
+import static org.springframework.http.HttpStatus.NO_CONTENT
+import static org.springframework.http.HttpStatus.CREATED
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY
+
+import grails.validation.ValidationException
+import grails.testing.web.controllers.ControllerUnitTest
+import grails.testing.gorm.DomainUnitTest
+import grails.plugin.json.view.JsonViewGrailsPlugin
+
+class DirectorControllerSpec
+        extends Specification
+        implements ControllerUnitTest<DirectorController>, DomainUnitTest<Director> {
+
+    void setupSpec() {
+        defineBeans(new JsonViewGrailsPlugin(applicationContext: applicationContext))
+    }
+
+    def populateValidParams(params) {
+        assert params != null
+
+        // TODO: Populate valid properties like...
+        //params["name"] = 'someValidName'
+        assert false, "TODO: Provide a populateValidParams() implementation for this generated test suite"
+    }
+
+    void "Test the index action returns the correct response"() {
+        given:
+        controller.directorService = Mock(DirectorService) {
+            1 * list(_) >> []
+            1 * count() >> 0
+        }
+
+        when:"The index action is executed"
+            controller.index()
+
+        then:"The response is correct"
+            response.text == '[]'
+    }
+
+
+    void "Test the save action with a null instance"() {
+        when:
+        request.contentType = JSON_CONTENT_TYPE
+        request.method = 'POST'
+        controller.save()
+
+        then:
+        response.status == UNPROCESSABLE_ENTITY.value()
+    }
+
+    void "Test the save action correctly persists"() {
+        given:
+        controller.directorService = Mock(DirectorService) {
+            1 * save(_ as Director)
+        }
+
+        when:
+        response.reset()
+        request.contentType = JSON_CONTENT_TYPE
+        request.method = 'POST'
+        populateValidParams(params)
+        request.json = new Director(params)
+        controller.save()
+
+        then:
+        response.status == CREATED.value()
+        response.json
+    }
+
+    void "Test the save action with an invalid instance"() {
+        given:
+        controller.directorService = Mock(DirectorService) {
+            1 * save(_ as Director) >> { Director director ->
+                throw new ValidationException("Invalid instance", director.errors)
+            }
+        }
+
+        when:
+        request.contentType = JSON_CONTENT_TYPE
+        request.method = 'POST'
+        populateValidParams(params)
+        request.json = new Director(params)
+        controller.save()
+
+        then:
+        response.status == UNPROCESSABLE_ENTITY.value()
+        response.json
+    }
+
+    void "Test the show action with a null id"() {
+        given:
+        controller.directorService = Mock(DirectorService) {
+            1 * get(null) >> null
+        }
+
+        when:"The show action is executed with a null domain"
+        controller.show()
+
+        then:"A 404 error is returned"
+        response.status == NOT_FOUND.value()
+    }
+
+    void "Test the show action with a valid id"() {
+        given:
+        controller.directorService = Mock(DirectorService) {
+            1 * get(2) >> new Director()
+        }
+
+        when:"A domain instance is passed to the show action"
+        params.id = 2
+        controller.show()
+
+        then:"A model is populated containing the domain instance"
+        response.status == OK.value()
+        response.json == [:]
+    }
+
+    void "Test the update action with a null instance"() {
+        when:
+        request.contentType = JSON_CONTENT_TYPE
+        request.method = 'PUT'
+        controller.update()
+
+        then:
+        response.status == UNPROCESSABLE_ENTITY.value()
+    }
+
+    void "Test the update action correctly persists"() {
+        given:
+        controller.directorService = Mock(DirectorService) {
+            1 * save(_ as Director)
+        }
+
+        when:
+        response.reset()
+        request.contentType = JSON_CONTENT_TYPE
+        request.method = 'PUT'
+        populateValidParams(params)
+        def instance = new Director(params)
+        instance.id = 1
+        instance.version = 0
+        controller.update(instance)
+
+        then:
+        response.status == OK.value()
+        response.json
+    }
+
+    void "Test the update action with an invalid instance"() {
+        given:
+        controller.directorService = Mock(DirectorService) {
+            1 * save(_ as Director) >> { Director director ->
+                throw new ValidationException("Invalid instance", director.errors)
+            }
+        }
+
+        when:
+        request.contentType = JSON_CONTENT_TYPE
+        request.method = 'PUT'
+        def instance = new Director(params)
+        instance.id = 1
+        instance.version = 0
+        controller.update(instance)
+
+        then:
+        response.status == UNPROCESSABLE_ENTITY.value()
+        response.json
+    }
+
+    void "Test the delete action with a null instance"() {
+        when:
+        request.contentType = JSON_CONTENT_TYPE
+        request.method = 'DELETE'
+        controller.delete()
+
+        then:
+        response.status == NOT_FOUND.value()
+    }
+
+    void "Test the delete action with an instance"() {
+        given:
+        controller.directorService = Mock(DirectorService) {
+            1 * delete(2) >> new Director(id: 2)
+        }
+
+        when:
+        request.contentType = JSON_CONTENT_TYPE
+        request.method = 'DELETE'
+        params.id = 2
+        controller.delete()
+
+        then:
+        response.status == NO_CONTENT.value()
+    }
+}
