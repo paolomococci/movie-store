4bd281ed352d5bce0a28c81a001d16fee01729de
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-10-06 06:41:26 +0200

    Create PaymentServiceSpec.groovy

---
+++ Grails/moviestore-rest/src/integration-test/groovy/moviestore/rest/data/PaymentServiceSpec.groovy
@@ -0,0 +1,100 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package moviestore.rest.data
+
+import grails.testing.mixin.integration.Integration
+import grails.gorm.transactions.Rollback
+
+import org.grails.datastore.mapping.core.Datastore
+import org.springframework.beans.factory.annotation.Autowired
+
+import spock.lang.Specification
+
+@Integration
+@Rollback
+class PaymentServiceSpec
+        extends Specification {
+
+    PaymentService paymentService
+    @Autowired Datastore datastore
+
+    private Long setupData() {
+        // TODO: Populate valid domain instances and return a valid ID
+        //new Payment(...).save(flush: true, failOnError: true)
+        //new Payment(...).save(flush: true, failOnError: true)
+        //Payment payment = new Payment(...).save(flush: true, failOnError: true)
+        //new Payment(...).save(flush: true, failOnError: true)
+        //new Payment(...).save(flush: true, failOnError: true)
+        assert false, "TODO: Provide a setupData() implementation for this generated test suite"
+        //payment.id
+    }
+
+    void cleanup() {
+        assert false, "TODO: Provide a cleanup implementation if using MongoDB"
+    }
+
+    void "test get"() {
+        setupData()
+
+        expect:
+        paymentService.get(1) != null
+    }
+
+    void "test list"() {
+        setupData()
+
+        when:
+        List<Payment> paymentList = paymentService.list(max: 2, offset: 2)
+
+        then:
+        paymentList.size() == 2
+        assert false, "TODO: Verify the correct instances are returned"
+    }
+
+    void "test count"() {
+        setupData()
+
+        expect:
+        paymentService.count() == 5
+    }
+
+    void "test delete"() {
+        Long paymentId = setupData()
+
+        expect:
+        paymentService.count() == 5
+
+        when:
+        paymentService.delete(paymentId)
+        datastore.currentSession.flush()
+
+        then:
+        paymentService.count() == 4
+    }
+
+    void "test save"() {
+        when:
+        assert false, "TODO: Provide a valid instance to save"
+        Payment payment = new Payment()
+        paymentService.save(payment)
+
+        then:
+        payment.id != null
+    }
+}
