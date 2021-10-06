18950fedd26973e39467963e7bf933d10fce2858
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-10-06 07:23:18 +0200

    Create StaffController.groovy

---
+++ Grails/moviestore-rest/grails-app/controllers/moviestore/rest/data/StaffController.groovy
@@ -0,0 +1,101 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package moviestore.rest.data
+
+import grails.validation.ValidationException
+
+import static org.springframework.http.HttpStatus.CREATED
+import static org.springframework.http.HttpStatus.NOT_FOUND
+import static org.springframework.http.HttpStatus.NO_CONTENT
+import static org.springframework.http.HttpStatus.OK
+
+import grails.gorm.transactions.ReadOnly
+import grails.gorm.transactions.Transactional
+
+@ReadOnly
+class StaffController {
+
+    StaffService staffService
+
+    static responseFormats = ['json', 'xml']
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond staffService.list(params), model:[staffCount: staffService.count()]
+    }
+
+    def show(Long id) {
+        respond staffService.get(id)
+    }
+
+    @Transactional
+    def save(Staff staff) {
+        if (staff == null) {
+            render status: NOT_FOUND
+            return
+        }
+        if (staff.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond staff.errors
+            return
+        }
+
+        try {
+            staffService.save(staff)
+        } catch (ValidationException e) {
+            respond staff.errors
+            return
+        }
+
+        respond staff, [status: CREATED, view:"show"]
+    }
+
+    @Transactional
+    def update(Staff staff) {
+        if (staff == null) {
+            render status: NOT_FOUND
+            return
+        }
+        if (staff.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond staff.errors
+            return
+        }
+
+        try {
+            staffService.save(staff)
+        } catch (ValidationException e) {
+            respond staff.errors
+            return
+        }
+
+        respond staff, [status: OK, view:"show"]
+    }
+
+    @Transactional
+    def delete(Long id) {
+        if (id == null || staffService.delete(id) == null) {
+            render status: NOT_FOUND
+            return
+        }
+
+        render status: NO_CONTENT
+    }
+}
