a679cb2593586812f5e325a8364c7a2c4fdecec1
paolomococci <paolomococci@gmail.com>
2021-10-11 05:00:30 +0200

    I add a controller that performs CRUD operations and the associated views for District domain class

---
+++ Grails/moviestore-web/src/integration-test/groovy/moviestore/web/data/DistrictServiceSpec.groovy
@@ -0,0 +1,95 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package moviestore.web.data
+
+import grails.testing.mixin.integration.Integration
+import grails.gorm.transactions.Rollback
+
+import spock.lang.Specification
+
+import org.hibernate.SessionFactory
+
+@Integration
+@Rollback
+class DistrictServiceSpec
+        extends Specification {
+
+    DistrictService districtService
+    SessionFactory sessionFactory
+
+    private Long setupData() {
+        // TODO: Populate valid domain instances and return a valid ID
+        //new District(...).save(flush: true, failOnError: true)
+        //new District(...).save(flush: true, failOnError: true)
+        //District district = new District(...).save(flush: true, failOnError: true)
+        //new District(...).save(flush: true, failOnError: true)
+        //new District(...).save(flush: true, failOnError: true)
+        assert false, "TODO: Provide a setupData() implementation for this generated test suite"
+        //district.id
+    }
+
+    void "test get"() {
+        setupData()
+
+        expect:
+        districtService.get(1) != null
+    }
+
+    void "test list"() {
+        setupData()
+
+        when:
+        List<District> districtList = districtService.list(max: 2, offset: 2)
+
+        then:
+        districtList.size() == 2
+        assert false, "TODO: Verify the correct instances are returned"
+    }
+
+    void "test count"() {
+        setupData()
+
+        expect:
+        districtService.count() == 5
+    }
+
+    void "test delete"() {
+        Long districtId = setupData()
+
+        expect:
+        districtService.count() == 5
+
+        when:
+        districtService.delete(districtId)
+        sessionFactory.currentSession.flush()
+
+        then:
+        districtService.count() == 4
+    }
+
+    void "test save"() {
+        when:
+        assert false, "TODO: Provide a valid instance to save"
+        District district = new District()
+        districtService.save(district)
+
+        then:
+        district.id != null
+    }
+}
